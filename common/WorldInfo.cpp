238f45c10364ef20731c9350137904438abde851
Al Riddoch <alriddoch@googlemail.com>
2000-08-29 09:25:03 +0000

    *** empty log message ***

---
+++ common/WorldInfo.cpp
@@ -0,0 +1,4 @@
+#include "WorldInfo.h"
+
+int world_info::operation_no=1;
+double world_info::time=0.0;
---
+++ common/WorldInfo.h
@@ -0,0 +1,11 @@
+#ifndef WORLD_INFO_H
+#define WORLD_INFO_H
+
+namespace world_info {
+
+extern int operation_no;
+extern double time;
+
+}
+
+#endif /* WORLD_INFO_H */
