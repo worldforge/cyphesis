ab33ad4ccf4836388be11abbdabec003269a3ef5
Al Riddoch <alriddoch@googlemail.com>
2000-11-20 15:39:29 +0000

    Missing files

---
+++ common/utility.cpp
@@ -0,0 +1,34 @@
+#include <Atlas/Message/Object.h>
+#include <Atlas/Objects/Root.h>
+#include <Atlas/Objects/Entity/RootEntity.h>
+#include <Atlas/Objects/Operation/Login.h>
+
+#include "utility.h"
+
+namespace utility {
+
+Root * Object_asRoot(Atlas::Message::Object & o)
+{
+    Root * obj;
+
+    if (!o.IsMap()) return NULL;
+    if ((o.AsMap().find("objtype") != o.AsMap().end()) &&
+        ((*o.AsMap().find("objtype")).second.IsString())) {
+        if ((*o.AsMap().find("objtype")).second.AsString() == "object") {
+    	    obj = new RootEntity;
+        } else if ((*o.AsMap().find("objtype")).second.AsString() == "op") {
+    	    obj = new RootOperation;
+        } else {
+            obj = new Root;
+        }
+    } else {
+        obj = new Root;
+    }
+    for (Object::MapType::const_iterator I = o.AsMap().begin();
+            I != o.AsMap().end(); I++) {
+        obj->SetAttr(I->first, I->second);
+    }
+    return obj;
+}
+
+}
---
+++ common/utility.h
@@ -0,0 +1,15 @@
+#ifndef COMMON_UTILITY_H
+#define COMMON_UTILITY_H
+
+using Atlas::Message::Object;
+using Atlas::Objects::Root;
+using Atlas::Objects::Entity::RootEntity;
+using Atlas::Objects::Operation::RootOperation;
+
+namespace utility {
+
+Root * Object_asRoot(Atlas::Message::Object &);
+
+}
+
+#endif /* COMMON_UTILITY_H */
