24ad9de58d813afbe2d0f99d4bf4775771e889e8
Al Riddoch <alriddoch@googlemail.com>
2001-10-18 17:23:33 +0000

    Removed obsolete conflicting farmer class

--- rulesets/basic/world/objects/characters/Farmer.py
+++
@@ -1,6 +0,0 @@
-#This file is distributed under the terms of the GNU General Public license.
-#Copyright (C) 1999 Aloril (See the file COPYING for details).
-
-from world.objects.Character import Character
-
-class Farmer(Character): pass
