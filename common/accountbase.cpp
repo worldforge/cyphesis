a00d480c7dc2191b881d67a6fdc0bf66425d952c
Al Riddoch <alriddoch@googlemail.com>
2002-03-04 02:34:01 +0000

    Missing file

---
+++ common/accountbase.cpp
@@ -0,0 +1,7 @@
+// This file may be redistributed and modified only under the terms of
+// the GNU General Public License (See COPYING for details).
+// Copyright (C) 2001 Alistair Riddoch
+
+#include "accountbase.h"
+
+AccountBase * AccountBase::m_instance = NULL;
