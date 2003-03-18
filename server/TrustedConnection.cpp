99f0ef1b45ad7d6d14d93ba5a203b16d92d7f9c9
Al Riddoch <alriddoch@googlemail.com>
2003-03-18 18:21:38 +0000

    New TrustedConnection files

---
+++ server/TrustedConnection.cpp
@@ -0,0 +1,16 @@
+// This file may be redistributed and modified only under the terms of
+// the GNU General Public License (See COPYING for details).
+// Copyright (C) 2004 Alistair Riddoch
+
+#include "TrustedConnection.h"
+
+TrustedConnection::TrustedConnection(const std::string & id, CommClient & client,
+                      ServerRouting & svr) : Connection(id, client, svr)
+{
+}
+
+bool TrustedConnection::verifyCredentials(const Account &,
+                                          const Fragment::MapType &) const
+{
+    return true;
+}
---
+++ server/TrustedConnection.h
@@ -0,0 +1,27 @@
+// This file may be redistributed and modified only under the terms of
+// the GNU General Public License (See COPYING for details).
+// Copyright (C) 2003 Alistair Riddoch
+
+#ifndef SERVER_TRUSTED_CONNECTION_H
+#define SERVER_TRUSTED_CONNECTION_H
+
+#include "Connection.h"
+
+// This class represents a connection from a client that is inherently
+// trusted, and can thus login without a password. This allows us to
+// clients login automatically without needing to store the admin password
+// in plain text anywhere. The client might be trusted because it has
+// connected with a known certificate of some kind, or because it has
+// connected over a unix domain socket.
+
+class TrustedConnection : public Connection {
+  public:
+    TrustedConnection(const std::string & id,
+                      CommClient & client,
+                      ServerRouting & svr);
+
+    virtual bool verifyCredentials(const Account &,
+                              const Atlas::Message::Object::MapType &) const;
+};
+
+#endif // SERVER_TRUSTED_CONNECTION_H
