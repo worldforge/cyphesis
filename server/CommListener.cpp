71e7f1563deec6bb0be033f551e5b61023f71e9b
Al Riddoch <alriddoch@googlemail.com>
2003-04-23 20:38:41 +0000

    2003-04-23  Al Riddoch  <alriddoch@zepler.org>
    	* server/CommListener.cpp: Add IPv6 listen code.

--- server/CommListener.cpp
+++ server/CommListener.cpp
@@ -8,6 +8,7 @@
 #include "CommServer.h"
 
 #include "common/debug.h"
+#include "common/log.h"
 
 #include <iostream>
 
@@ -41,7 +42,28 @@ bool CommListener::setup(int port)
 {
     // Nasty low level socket code to set up listen socket. This should be
     // replaced with a socket class library.
-    struct sockaddr_in sin;
+    listenPort = port;
+    listenFd = ::socket(PF_INET6, SOCK_STREAM, 0);
+    if (listenFd < 0) {
+        log(WARNING, "Could not open 6 socket");
+        perror("socket");
+    } else {
+        int flag = 1;
+        ::setsockopt(listenFd, SOL_SOCKET, SO_REUSEADDR, &flag, sizeof(flag));
+        struct sockaddr_in6 sin;
+        sin.sin6_family = AF_INET6;
+        sin.sin6_port = htons(port);
+        sin.sin6_addr = in6addr_any;
+        if (::bind(listenFd, (struct sockaddr *)&sin, sizeof(sin)) < 0) {
+            log(WARNING, "Could not bind 6 socket");
+            perror("bind");
+            ::close(listenFd);
+        } else {
+            log(NOTICE, "Listening on 6 socket");
+            ::listen(listenFd, 5);
+            return true;
+        }
+    }
 
     listenPort = port;
     listenFd = ::socket(PF_INET, SOCK_STREAM, 0);
@@ -51,6 +73,7 @@ bool CommListener::setup(int port)
     }
     int flag = 1;
     ::setsockopt(listenFd, SOL_SOCKET, SO_REUSEADDR, &flag, sizeof(flag));
+    struct sockaddr_in sin;
     sin.sin_family = AF_INET;
     sin.sin_port = htons(port);
     sin.sin_addr.s_addr = 0L;
@@ -67,15 +90,11 @@ bool CommListener::accept()
 {
     // Low level socket code to accept a new client connection, and create
     // the associated commclient object.
-    struct sockaddr_in sin;
-    unsigned int addr_len = sizeof(sin);
-
-    sin.sin_family = AF_INET;
-    sin.sin_port = htons(listenPort);
-    sin.sin_addr.s_addr = 0L;
+    struct sockaddr_storage sst;
+    unsigned int addr_len = sizeof(sst);
 
     debug(std::cout << "Accepting.." << std::endl << std::flush;);
-    int asockfd = ::accept(listenFd, (struct sockaddr *)&sin, &addr_len);
+    int asockfd = ::accept(listenFd, (struct sockaddr *)&sst, &addr_len);
 
     if (asockfd < 0) {
         return false;
