7d36399b787df8bdfe099ce5b5068cd27211f1c8
Al Riddoch <alriddoch@googlemail.com>
2003-07-19 12:38:39 +0000

    Copyright typo

--- server/Persistor_impl.h
+++ server/Persistor_impl.h
@@ -1,5 +1,5 @@
 // This file may be redistributed and modified only under the terms of
-// the GNU General Public License (See COPyING for details).
+// the GNU General Public License (See COPYING for details).
 // Copyright (C) 2000,2001 Alistair Riddoch
 
 #ifndef SERVER_PERSISTOR_IMPL_H
