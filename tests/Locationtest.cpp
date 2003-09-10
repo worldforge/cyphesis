cb0ce181c36c528cc99a38f0dfd7a1b1a42c84ed
Al Riddoch <alriddoch@googlemail.com>
2003-09-10 00:10:46 +0000

    2003-09-10  Al Riddoch  <alriddoch@zepler.org>
    	* tests/Locationtest.cpp: Quick simple test for Location.

---
+++ tests/Locationtest.cpp
@@ -0,0 +1,87 @@
+// This file may be redistributed and modified only under the terms of
+// the GNU General Public License (See COPYING for details).
+// Copyright (C) 2003 Alistair Riddoch
+
+#include "modules/Location.h"
+
+#include <cassert>
+
+int main()
+{
+    {
+        Location testloc;
+
+        assert(testloc.m_loc == 0);
+        assert(!testloc.m_pos.isValid());
+        assert(!testloc.m_velocity.isValid());
+        assert(!testloc.m_orientation.isValid());
+    }
+
+    {
+        Location testloc(0);
+
+        assert(testloc.m_loc == 0);
+        assert(!testloc.m_pos.isValid());
+        assert(!testloc.m_velocity.isValid());
+        assert(!testloc.m_orientation.isValid());
+    }
+
+    {
+        Vector3D testPos;
+        Location testloc(0, testPos);
+
+        assert(!testPos.isValid());
+
+        assert(testloc.m_loc == 0);
+        assert(!testloc.m_pos.isValid());
+        assert(!testloc.m_velocity.isValid());
+        assert(!testloc.m_orientation.isValid());
+    }
+
+    {
+        Vector3D testPos(0,0,0);
+        Location testloc(0, testPos);
+
+        assert(testPos.isValid());
+
+        assert(testloc.m_loc == 0);
+        assert(testloc.m_pos.isValid());
+        assert(!testloc.m_velocity.isValid());
+        assert(!testloc.m_orientation.isValid());
+    }
+
+    {
+        Vector3D testPos(0,0,0);
+        Vector3D testVel;
+        Location testloc(0, testPos, testVel);
+
+        assert(testPos.isValid());
+        assert(!testVel.isValid());
+
+        assert(testloc.m_loc == 0);
+        assert(testloc.m_pos.isValid());
+        assert(!testloc.m_velocity.isValid());
+        assert(!testloc.m_orientation.isValid());
+    }
+
+    {
+        Vector3D testPos(0,0,0);
+        Vector3D testVel(0,0,0);
+        Location testloc(0, testPos, testVel);
+
+        assert(testPos.isValid());
+        assert(testVel.isValid());
+
+        assert(testloc.m_loc == 0);
+        assert(testloc.m_pos.isValid());
+        assert(testloc.m_velocity.isValid());
+        assert(!testloc.m_orientation.isValid());
+
+        Quaternion testOrientation(1, 0, 0, 0);
+        assert(testOrientation.isValid());
+        testloc.m_orientation = testOrientation;
+        assert(testloc.m_orientation.isValid());
+    }
+
+    return 0;
+}
