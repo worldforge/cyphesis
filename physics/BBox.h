0a4ca52a982144f823f49f706adcb19ffb58a61a
Al Riddoch <alriddoch@googlemail.com>
2003-10-15 18:14:55 +0000

    2003-10-15  Al Riddoch  <alriddoch@zepler.org>
    	* physics/BBox.h, physics/Vector3D.h: Remove historical vector
    	  and bbox classes.

--- physics/BBox.h
+++ physics/BBox.h
@@ -7,111 +7,11 @@
 
 #include <wfmath/axisbox.h>
 
-#if 1
 typedef WFMath::AxisBox<3> BBox;
-#else
-class BBox {
-  private:
-    Vector3D u;
-    Vector3D v;
-  public:
-    BBox() { }
-    explicit BBox(const Vector3D & f) : u(0,0,0), v(f) { }
-    BBox(const Vector3D & n, const Vector3D & f) : u(n), v(f) { }
-    explicit BBox(const Atlas::Message::Element::ListType & l) {
-        if (l.size() == 6) {
-            u = Vector3D(l);
-            v = Vector3D(l[3].asNum(), l[4].AsNum(), l[5].AsNum());
-        } else {
-            u = Vector3D(0,0,0);
-            v = Vector3D(l);
-        }
-    }
 
-    const Vector3D & lowCorner() const { return u; }
-    Vector3D & lowCorner() { return u; }
-    const Vector3D & highCorner() const { return v; }
-    Vector3D & highCorner() { return v; }
+#include "physics/Vector3D.h"
 
-    bool operator ==(const BBox & other) const {
-        return ((u==other.u) && (v==other.v));
-    }
-
-    bool isValid() const {
-        return v.isValid();
-    }
-
-    const BBox operator+(const Vector3D & other) const {
-        return BBox(u + other, v + other);
-    }
-
-    BBox & operator+=(const Vector3D & other) {
-        u += other;
-        v += other;
-        return *this;
-    }
-
-    bool contains(const Vector3D & o) const {
-        // Is point o inside this box
-        return ((o.X() > u.X()) && (o.X() < v.X()) &&
-                (o.Y() > u.Y()) && (o.Y() < v.Y()) &&
-                (o.Z() > u.Z()) && (o.Z() < v.Z()));
-    }
-
-    bool contains(const Vector3D & o, double increase) const {
-        // Is point o inside this box, when this box is increased in each
-        // direction
-        return ((o.X() > (u.X() - increase)) && (o.X() < (v.X() + increase)) &&
-                (o.Y() > (u.Y() - increase)) && (o.Y() < (v.Y() + increase)) &&
-                (o.Z() > (u.Z() - increase)) && (o.Z() < (v.Z() + increase)));
-    }
-
-    const BBox grow(double o) const {
-        return BBox(u - o, v + o);
-    }
-
-    BBox & grow(double o) {
-        u -= o;
-        v += o;
-        return *this;
-    }
-
-    bool hit(const BBox & o) const {
-        return u.hit(v, o.u, o.v);
-    }
-
-    double timeToHit(const Vector3D & vel, const BBox & o, int & axis) const {
-        return u.timeToHit(v, vel, o.u, o.v, axis);
-    }
-
-    double timeToExit(const Vector3D & vel, const BBox & o) const {
-        return u.timeToExit(v, vel, o.u, o.v);
-    }
-
-    const Atlas::Message::Element::ListType asList() const {
-        if (!u.isZero()) {
-            Atlas::Message::Element::ListType box = u.asList();
-            box.push_back(v.X());
-            box.push_back(v.Y());
-            box.push_back(v.Z());
-            return box;
-        } else {
-            Atlas::Message::Element::ListType box = v.asList();
-            return v.asList();
-        }
-    }
-
-    friend std::ostream & operator<<(std::ostream& s, const BBox & v);
-};
-
-inline std::ostream & operator<<(std::ostream& s, const BBox & b)
-{
-    return s << "{" << b.u << b.v << "}";
-}
-#endif // 0
-
-#include <physics/Vector3D.h>
-
+// FIXME This boxContains function is deprecated
 // Is point other inside this box, when this box is increased in each direction
 inline bool boxContains(const BBox & box, const Vector3D & other, double increase)
 {
