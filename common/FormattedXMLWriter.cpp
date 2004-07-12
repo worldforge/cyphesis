84c7873bc34213c982ef30b1949bd5a3be4ff592
Al Riddoch <alriddoch@googlemail.com>
2004-07-12 22:05:50 +0000

    2004-07-12  Al Riddoch  <alriddoch@zepler.org>
    	* common/FormattedXMLWriter.cpp common/FormattedXMLWriter.h:
    	  Utility class to write Atlas to a stream with formatting for
    	  human readability.

---
+++ common/FormattedXMLWriter.cpp
@@ -0,0 +1,122 @@
+// This file may be redistributed and modified only under the terms of
+// the GNU General Public License (See COPYING for details).
+// Copyright (C) 2004 Alistair Riddoch
+
+#include "FormattedXMLWriter.h"
+
+using Atlas::Message::Element;
+typedef Element::MapType MapType;
+typedef Element::ListType ListType;
+
+FormattedXMLWriter::FormattedXMLWriter(std::iostream & stream) :
+                                       m_stream(stream), m_indent(0)
+{
+}
+
+/// Write the atlas header to the stream
+void FormattedXMLWriter::openOutput()
+{
+    m_stream << "<atlas>" << std::endl;
+    m_indent += 2;
+}
+
+/// Write the atlas tail to the stream
+void FormattedXMLWriter::closeOutput()
+{
+    m_indent -= 2;
+    m_stream << "</atlas>" << std::endl;
+}
+
+void FormattedXMLWriter::outputObject(const Element::MapType & o)
+{
+    m_stream << "  <map>" << std::endl;
+    m_indent += 2;
+    outputMap(o);
+    m_indent -= 2;
+    m_stream << "  </map>" << std::endl << std::endl;
+}
+
+const char * FormattedXMLWriter::typeToStr(Element::Type t) const
+{
+    switch (t) {
+        case Element::TYPE_INT:
+            return "int";
+        case Element::TYPE_FLOAT:
+            return "float";
+        case Element::TYPE_STRING:
+            return "string";
+        case Element::TYPE_MAP:
+            return "map";
+        case Element::TYPE_LIST:
+            return "list";
+        case Element::TYPE_NONE:
+        default:
+            return "NONE";
+    };
+}
+
+void FormattedXMLWriter::outputValue(const Element & e)
+{
+    switch (e.getType()) {
+        case Element::TYPE_INT:
+            m_stream << e.asInt();
+            break;
+        case Element::TYPE_FLOAT:
+            m_stream << e.asFloat();
+            break;
+        case Element::TYPE_STRING:
+            m_stream << e.asString();
+            break;
+        case Element::TYPE_MAP:
+            m_stream << std::endl;
+            m_indent += 2;
+            outputMap(e.asMap());
+            m_indent -= 2;
+            for(int i = 0; i < m_indent; ++i) {
+                m_stream << " ";
+            }
+            break;
+        case Element::TYPE_LIST:
+            m_stream << std::endl;
+            m_indent += 2;
+            outputList(e.asList());
+            m_indent -= 2;
+            for(int i = 0; i < m_indent; ++i) {
+                m_stream << " ";
+            }
+            break;
+        case Element::TYPE_NONE:
+        default:
+            m_stream << e.asInt();
+            break;
+    };
+}
+
+void FormattedXMLWriter::outputMap(const MapType & o)
+{
+    for(MapType::const_iterator I = o.begin(); I != o.end(); ++I) {
+        for(int i = 0; i < m_indent; ++i) {
+            m_stream << " ";
+        }
+        const char * type = typeToStr(I->second.getType());
+        m_stream << "<" << type << " name=\""
+                  << I->first << "\">";
+        outputValue(I->second);
+        m_stream << "</" << type << ">";
+        m_stream << std::endl;
+    }
+}
+
+void FormattedXMLWriter::outputList(const ListType & o)
+{
+    for(ListType::const_iterator I = o.begin(); I != o.end(); ++I) {
+        for(int i = 0; i < m_indent; ++i) {
+            m_stream << " ";
+        }
+        const char * type = typeToStr(I->getType());
+        m_stream << "<" << type << ">";
+        outputValue(*I);
+        m_stream << "</" << type << ">";
+        m_stream << std::endl;
+    }
+}
---
+++ common/FormattedXMLWriter.h
@@ -0,0 +1,41 @@
+// This file may be redistributed and modified only under the terms of
+// the GNU General Public License (See COPYING for details).
+// Copyright (C) 2004 Alistair Riddoch
+
+#include <Atlas/Message/Element.h>
+
+#include <string>
+#include <fstream>
+
+/// Class to read old cyphesis rules on standard input, and output in new
+/// standard format.
+class FormattedXMLWriter {
+  private:
+    /// Output file
+    std::iostream & m_stream;
+    /// Counter to keep track of indenting the output
+    int m_indent;
+
+    /// Return a character string corresponding to this atlas element type
+    const char * typeToStr(Atlas::Message::Element::Type t) const;
+
+    /// Write the contents of this element recursively to the stream
+    void outputValue(const Atlas::Message::Element &);
+
+    /// Write the contents of this atlas list recursively to the stream
+    void outputList(const Atlas::Message::Element::ListType &);
+
+    /// Write the contents of this atlas map recursively to the stream
+    void outputMap(const Atlas::Message::Element::MapType &);
+  public:
+    FormattedXMLWriter(std::iostream & stream);
+
+    /// Write the atlas header to the stream
+    void openOutput();
+
+    /// Write the atlas tail to the stream
+    void closeOutput();
+
+    /// Write the contents of this top level object to the stream
+    void outputObject(const Atlas::Message::Element::MapType & o);
+};
