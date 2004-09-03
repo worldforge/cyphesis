ba1bc3dec09fdf65a218e25afbfcb28611c77c03
Al Riddoch <alriddoch@googlemail.com>
2004-09-03 00:38:20 +0000

    2004-09-03  Al Riddoch  <alriddoch@zepler.org>
    	* tests/EntityExerciser.h: Update test harness for new dispatch
    	  mechanism.

--- tests/EntityExerciser.h
+++ tests/EntityExerciser.h
@@ -19,7 +19,8 @@ class EntityExerciser {
     explicit EntityExerciser(EntityType & e) : m_ent(e) { }
 
     virtual void dispatchOp(const RootOperation & op) {
-        OpVector ov1 = m_ent.operation(op);
+        OpVector ov1;
+        m_ent.operation(op, ov1);
         flushOperations(ov1);
     }
 
@@ -84,181 +85,211 @@ inline void EntityExerciser<EntityType>::runOperations()
     {
         Login op;
         dispatchOp(op);
-        OpVector ov = m_ent.LoginOperation(op);
+        OpVector ov;
+        m_ent.LoginOperation(op, ov);
         flushOperations(ov);
     }
     {
         Logout op;
         dispatchOp(op);
-        OpVector ov = m_ent.LogoutOperation(op);
+        OpVector ov;
+        m_ent.LogoutOperation(op, ov);
         flushOperations(ov);
     }
     {
         Action op;
         dispatchOp(op);
-        OpVector ov = m_ent.ActionOperation(op);
+        OpVector ov;
+        m_ent.ActionOperation(op, ov);
         flushOperations(ov);
     }
     {
         Chop op;
         dispatchOp(op);
-        OpVector ov = m_ent.ChopOperation(op);
+        OpVector ov;
+        m_ent.ChopOperation(op, ov);
         flushOperations(ov);
     }
     {
         Combine op;
         dispatchOp(op);
-        OpVector ov = m_ent.CombineOperation(op);
+        OpVector ov;
+        m_ent.CombineOperation(op, ov);
         flushOperations(ov);
     }
     {
         Create op;
         dispatchOp(op);
-        OpVector ov = m_ent.CreateOperation(op);
+        OpVector ov;
+        m_ent.CreateOperation(op, ov);
         flushOperations(ov);
     }
     {
         Cut op;
         dispatchOp(op);
-        OpVector ov = m_ent.CutOperation(op);
+        OpVector ov;
+        m_ent.CutOperation(op, ov);
         flushOperations(ov);
     }
     {
         Delete op;
         dispatchOp(op);
-        OpVector ov = m_ent.DeleteOperation(op);
+        OpVector ov;
+        m_ent.DeleteOperation(op, ov);
         flushOperations(ov);
     }
     {
         Divide op;
         dispatchOp(op);
-        OpVector ov = m_ent.DivideOperation(op);
+        OpVector ov;
+        m_ent.DivideOperation(op, ov);
         flushOperations(ov);
     }
     {
         Eat op;
         dispatchOp(op);
-        OpVector ov = m_ent.EatOperation(op);
+        OpVector ov;
+        m_ent.EatOperation(op, ov);
         flushOperations(ov);
     }
     {
         Burn op;
         dispatchOp(op);
-        OpVector ov = m_ent.BurnOperation(op);
+        OpVector ov;
+        m_ent.BurnOperation(op, ov);
         flushOperations(ov);
     }
     {
         Get op;
         dispatchOp(op);
-        OpVector ov = m_ent.GetOperation(op);
+        OpVector ov;
+        m_ent.GetOperation(op, ov);
         flushOperations(ov);
     }
     {
         Imaginary op;
         dispatchOp(op);
-        OpVector ov = m_ent.ImaginaryOperation(op);
+        OpVector ov;
+        m_ent.ImaginaryOperation(op, ov);
         flushOperations(ov);
     }
     {
         Info op;
         dispatchOp(op);
-        OpVector ov = m_ent.InfoOperation(op);
+        OpVector ov;
+        m_ent.InfoOperation(op, ov);
         flushOperations(ov);
     }
     {
         Move op;
         dispatchOp(op);
-        OpVector ov = m_ent.MoveOperation(op);
+        OpVector ov;
+        m_ent.MoveOperation(op, ov);
         flushOperations(ov);
     }
     {
         Nourish op;
         dispatchOp(op);
-        OpVector ov = m_ent.NourishOperation(op);
+        OpVector ov;
+        m_ent.NourishOperation(op, ov);
         flushOperations(ov);
     }
     {
         Set op;
         dispatchOp(op);
-        OpVector ov = m_ent.SetOperation(op);
+        OpVector ov;
+        m_ent.SetOperation(op, ov);
         flushOperations(ov);
     }
     {
         Sight op;
         dispatchOp(op);
-        OpVector ov = m_ent.SightOperation(op);
+        OpVector ov;
+        m_ent.SightOperation(op, ov);
         flushOperations(ov);
     }
     {
         Sound op;
         dispatchOp(op);
-        OpVector ov = m_ent.SoundOperation(op);
+        OpVector ov;
+        m_ent.SoundOperation(op, ov);
         flushOperations(ov);
     }
     {
         Talk op;
         dispatchOp(op);
-        OpVector ov = m_ent.TalkOperation(op);
+        OpVector ov;
+        m_ent.TalkOperation(op, ov);
         flushOperations(ov);
     }
     {
         Touch op;
         dispatchOp(op);
-        OpVector ov = m_ent.TouchOperation(op);
+        OpVector ov;
+        m_ent.TouchOperation(op, ov);
         flushOperations(ov);
     }
     {
         Tick op;
         dispatchOp(op);
-        OpVector ov = m_ent.TickOperation(op);
+        OpVector ov;
+        m_ent.TickOperation(op, ov);
         flushOperations(ov);
     }
     {
         Look op;
         dispatchOp(op);
-        OpVector ov = m_ent.LookOperation(op);
+        OpVector ov;
+        m_ent.LookOperation(op, ov);
         flushOperations(ov);
     }
     {
         Setup op;
         dispatchOp(op);
-        OpVector ov = m_ent.SetupOperation(op);
+        OpVector ov;
+        m_ent.SetupOperation(op, ov);
         flushOperations(ov);
     }
     {
         Appearance op;
         dispatchOp(op);
-        OpVector ov = m_ent.AppearanceOperation(op);
+        OpVector ov;
+        m_ent.AppearanceOperation(op, ov);
         flushOperations(ov);
     }
     {
         Disappearance op;
         dispatchOp(op);
-        OpVector ov = m_ent.DisappearanceOperation(op);
+        OpVector ov;
+        m_ent.DisappearanceOperation(op, ov);
         flushOperations(ov);
     }
     {
         Use op;
         dispatchOp(op);
-        OpVector ov = m_ent.UseOperation(op);
+        OpVector ov;
+        m_ent.UseOperation(op, ov);
         flushOperations(ov);
     }
     {
         Wield op;
         dispatchOp(op);
-        OpVector ov = m_ent.WieldOperation(op);
+        OpVector ov;
+        m_ent.WieldOperation(op, ov);
         flushOperations(ov);
     }
     {
         Error op;
         dispatchOp(op);
-        OpVector ov = m_ent.ErrorOperation(op);
+        OpVector ov;
+        m_ent.ErrorOperation(op, ov);
         flushOperations(ov);
     }
     {
         RootOperation op;
         dispatchOp(op);
-        OpVector ov = m_ent.OtherOperation(op);
+        OpVector ov;
+        m_ent.OtherOperation(op, ov);
         flushOperations(ov);
     }
 }
