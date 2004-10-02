3d5a17a591fd617814c500abdd8b870e1eeb4090
Al Riddoch <alriddoch@googlemail.com>
2004-10-02 21:58:59 +0000

    *** empty log message ***

--- client/define_world.cpp
+++ client/define_world.cpp
@@ -2,6 +2,8 @@
 // the GNU General Public License (See COPYING for details).
 // Copyright (C) 2000,2001 Alistair Riddoch
 
+#error This file has been removed from the build.
+
 #include "define_world.h"
 
 #include "Py_CreatorClient.h"
