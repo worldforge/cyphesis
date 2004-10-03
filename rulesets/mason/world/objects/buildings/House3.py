729bb3fff998bbda9b5c7113f1ea8bc58ebefc7d
Al Riddoch <alriddoch@googlemail.com>
2004-10-03 03:01:12 +0000

    2004-10-03  Al Riddoch  <alriddoch@zepler.org>
    	* rulesets/mason/world/objects/buildings/House3.py: Fix script
    	  to pass Point to Location constructor for pos.

--- rulesets/mason/world/objects/buildings/House3.py
+++ rulesets/mason/world/objects/buildings/House3.py
@@ -4,6 +4,7 @@
 from world.objects.Thing import Thing
 from atlas import *
 from Vector3D import Vector3D
+from Point3D import Point3D
 
 # bbox = 4,4,2.5
 # bmedian = 3.5,3.5,2.5
@@ -11,25 +12,24 @@ from Vector3D import Vector3D
 
 class House3(Thing):
     def setup_operation(self, op):
-        print "Making house"
         ret = Message()
         # West wall  with door
-	loc = Location(self, Vector3D(0,0,-1))
+	loc = Location(self, Point3D(0,0,-1))
         loc.bbox = Vector3D(0.1,2,4)
         ret.append(Operation("create",Entity(name='wall',parents=['wall'],location=loc),to=self))
-	loc = Location(self, Vector3D(0,4,-1))
+	loc = Location(self, Point3D(0,4,-1))
         loc.bbox = Vector3D(0.1,2,4)
         ret.append(Operation("create",Entity(name='wall',parents=['wall'],location=loc),to=self))
         # South wall
-	loc = Location(self, Vector3D(0,0,-1))
+	loc = Location(self, Point3D(0,0,-1))
         loc.bbox = Vector3D(9,0.1,4)
         ret.append(Operation("create",Entity(name='wall',parents=['wall'],location=loc),to=self))
         # North wall
-	loc = Location(self, Vector3D(0,5.9,-1))
+	loc = Location(self, Point3D(0,5.9,-1))
         loc.bbox = Vector3D(9,0.1,4)
         ret.append(Operation("create",Entity(name='wall',parents=['wall'],location=loc),to=self))
         # East wall
-	loc = Location(self, Vector3D(8.9,0,-1))
+	loc = Location(self, Point3D(8.9,0,-1))
         loc.bbox = Vector3D(0.1,6,4)
         ret.append(Operation("create",Entity(name='wall',parents=['wall'],location=loc),to=self))
         return ret
