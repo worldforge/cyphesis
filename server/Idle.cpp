7d70b0adc49cbdec6b487667ecf7de0ce15604e8
Al Riddoch <alriddoch@googlemail.com>
2004-11-20 22:13:54 +0000

    2004-11-20  Al Riddoch  <alriddoch@zepler.org>
    	* server/Idle.cpp, server/Idle.h: New Idle class missing from
    	  previous commit.

---
+++ server/Idle.cpp
@@ -0,0 +1,19 @@
+// This file may be redistributed and modified only under the terms of
+// the GNU General Public License (See COPYING for details).
+// Copyright (C) 2003 Alistair Riddoch
+
+#include "Idle.h"
+
+#include "CommServer.h"
+
+/// \brief Constructor for socket object.
+///
+/// @param svr Reference to the object that manages all socket communication.
+Idle::Idle(CommServer & svr) : m_commServer(svr)
+{
+}
+
+Idle::~Idle()
+{
+    m_commServer.removeIdle(this);
+}
---
+++ server/Idle.h
@@ -0,0 +1,31 @@
+// This file may be redistributed and modified only under the terms of
+// the GNU General Public License (See COPYING for details).
+// Copyright (C) 2000,2001 Alistair Riddoch
+
+#ifndef SERVER_IDLE_H
+#define SERVER_IDLE_H
+
+#include "CommSocket.h"
+
+#include <time.h>
+
+/// \brief Base class for any object which needs to be polled regularly.
+///
+/// This should be treated as an interface, so can cleanly be used in
+/// multiple inheritance.
+class Idle {
+  protected:
+    explicit Idle(CommServer & svr);
+  public:
+    /// Reference to the object that manages all socket communication.
+    CommServer & m_commServer;
+
+    virtual ~Idle();
+
+    /// \brief Perform idle tasks.
+    ///
+    /// Called from the server's core idle function whenever it is called.
+    virtual void idle(time_t t) = 0;
+};
+
+#endif // SERVER_IDLE_H
