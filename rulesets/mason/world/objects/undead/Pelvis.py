5c53078e1c188b40804bc4137885ab7811590b4a
Al Riddoch <alriddoch@googlemail.com>
2005-01-24 16:37:06 +0000

    2005-01-24  Al Riddoch  <alriddoch@zepler.org>
    	* data/mason.xml: Remove obsolete scripts for some entities
    	  which don't do anything.

--- rulesets/mason/world/objects/undead/Pelvis.py
+++
@@ -1,9 +0,0 @@
-from atlas import *
-
-from world.objects.Thing import Thing
-from misc import set_kw
-
-class Pelvis(Thing):
-    def __init__(self, cppthing, **kw):
-        self.base_init(cppthing, kw)
-        set_kw(self,kw,"mass",1.0)
