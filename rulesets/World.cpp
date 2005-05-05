02b329d1dd040871eac6644bf56cb9f10b172ae8
Al Riddoch <alriddoch@googlemail.com>
2005-05-05 00:22:05 +0000

    2005-05-05  Al Riddoch  <alriddoch@zepler.org>
    	* rulesets/World.cpp: Use a TileShader to handle data about the
    	  material the makes up the terrain surface. When a Delve operation
    	  determine what material the world is at that point.

--- rulesets/World.cpp
+++ rulesets/World.cpp
@@ -12,6 +12,14 @@
 
 #include <Mercator/Terrain.h>
 #include <Mercator/Segment.h>
+#include <Mercator/TileShader.h>
+#include <Mercator/FillShader.h>
+#include <Mercator/ThresholdShader.h>
+#include <Mercator/DepthShader.h>
+#include <Mercator/GrassShader.h>
+#include <Mercator/Surface.h>
+
+#include <wfmath/atlasconv.h>
 
 #include <Atlas/Objects/Operation/Sight.h>
 
@@ -26,15 +34,25 @@ using Atlas::Message::ListType;
 using Atlas::Message::FloatType;
 using Atlas::Objects::Operation::Sight;
 
+typedef enum { ROCK = 0, SAND = 1, GRASS = 2, MUD = 3, SNOW = 4} Surface;
+
 /// \brief Constructor for the World entity
 World::World(const std::string & id) : World_parent(id),
-                                       m_terrain(*new Mercator::Terrain())
+             m_terrain(*new Mercator::Terrain(Mercator::Terrain::SHADED))
 {
     subscribe("set", OP_SET);
     subscribe("delve", OP_OTHER);
 
     m_properties["terrain"] = new TerrainProperty(m_terrain, m_modifiedTerrain,
                                                   m_modifiedTerrain, a_terrain);
+
+    Mercator::TileShader * tileShader = new Mercator::TileShader;
+    tileShader->addShader(new Mercator::FillShader(), ROCK);
+    tileShader->addShader(new Mercator::BandShader(-2.f, 1.5f), SAND);
+    tileShader->addShader(new Mercator::GrassShader(1.f, 80.f, .5f, 1.f),GRASS);
+    tileShader->addShader(new Mercator::DepthShader(0.f, -10.f), MUD);
+    tileShader->addShader(new Mercator::HighShader(110.f), SNOW);
+    m_terrain.addShader(tileShader, 0);
 }
 
 World::~World()
@@ -56,6 +74,63 @@ void World::delveOperation(const Operation & op, OpVector & res)
 {
     // FIXME
     std::cout << "Got delve operation" << std::endl << std::flush;
+
+    if (op.getArgs().empty() || !op.getArgs().front().isMap()) {
+        // FIXME Would it be more useful to send the error back where the
+        // op came from?
+        error(op, "Delve op has no args", res, getId());
+        return;
+    }
+    const MapType & arg = op.getArgs().front().asMap();
+
+    MapType::const_iterator Iend = arg.end();
+    MapType::const_iterator I = arg.find("pos");
+    if (I == Iend) {
+        // FIXME Would it be more useful to send the error back where the
+        // op came from?
+        error(op, "Delve op to world has no POS in args", res, getId());
+        return;
+    }
+    if (!I->second.isList()) {
+        // FIXME Would it be more useful to send the error back where the
+        // op came from?
+        error(op, "Delve op to world has non list POS in args", res, getId());
+        return;
+    }
+    WFMath::Point<3> delve_pos;
+    // FIXME This data is non yet taint checked.
+    delve_pos.fromAtlas(I->second.asList());
+    std::cout << "Got delve on world at " << delve_pos
+              << std::endl << std::flush;
+    float x = delve_pos.x(),
+          y = delve_pos.y();
+    Mercator::Segment * segment = m_terrain.getSegment(x, y);
+    if (segment == 0) {
+        std::cerr << "No terrain at this point" << std::endl << std::flush;
+        return;
+    }
+    if (!segment->isValid()) {
+        segment->populate();
+    }
+    x = x - segment->getResolution() * segment->getXRef();
+    y = y - segment->getResolution() * segment->getYRef();
+    const Mercator::Segment::Surfacestore & surfaces = segment->getSurfaces();
+    WFMath::Vector<3> normal;
+    float height = -23;
+    segment->getHeightAndNormal(x, y, height, normal);
+    std::cout << "At the point " << x << "," << y << " of the segment the height is " << height << std::endl;
+    std::cout << "The segment has " << surfaces.size() << std::endl << std::flush;
+    if (surfaces.size() == 0) {
+        log(ERROR, "The terrain has no surface data");
+        return;
+    }
+    Mercator::Surface & tileSurface = *surfaces.begin()->second;
+    if (!tileSurface.isValid()) {
+        tileSurface.populate();
+    }
+    int material = tileSurface((int)x, (int)y, 0);
+    std::cout << "The material at this point is " << material
+              << std::endl << std::flush;
 }
 
 void World::LookOperation(const Operation & op, OpVector & res)
