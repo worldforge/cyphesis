b9d5a4ca6437be1f28d20f2ed12053764320985f
Al Riddoch <alriddoch@googlemail.com>
2005-05-28 20:09:33 +0000

    2005-05-27  Al Riddoch  <alriddoch@zepler.org>
    	* data/basic.xml, data/mason.xml, rulesets/mason/define_world.py:
    	  Make sure entities with layers all use a different layer ID.
    	  Make sure sty has area and bbox in the default attributes.

--- rulesets/mason/define_world.py
+++ rulesets/mason/define_world.py
@@ -290,7 +290,7 @@ def default(mapeditor):
     merchant=m.make('Dyfed Searae',type='merchant',desc='the pig merchant',
                     xyz=pig_sty_xyz,age=probability.fertility_age,
                     sex='male',orientation=Quaternion(Vector3D([1,0,0]),Vector3D([0,-1,0])).as_list())
-    sty=m.make('sty',type='sty',xyz=pig_sty_xyz,status=1.0,bbox=[5,5,3],area={'points' : [ [0,0], [0,5], [5,5], [5,0] ], 'layer' : 5})
+    sty=m.make('sty',type='sty',xyz=pig_sty_xyz)
     m.know(merchant,mknowledge)
     m.know(merchant,village)
     m.know(merchant,mprices)
