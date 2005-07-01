a2ea886ab0dd23dfe93e06a1755bd4a4725a2fcf
Al Riddoch <alriddoch@googlemail.com>
2005-07-01 00:52:30 +0000

    2005-07-01  Al Riddoch  <alriddoch@zepler.org>
    	* rulesets/mason/world/objects/plants/seeds/Acorn.py:
    	  When an oak is created from seed, make its mass the same as the
    	  acorn's mass, and hack its bbox to the apropriate size.

--- rulesets/mason/world/objects/plants/seeds/Acorn.py
+++ rulesets/mason/world/objects/plants/seeds/Acorn.py
@@ -26,7 +26,7 @@ class Acorn(Seed):
         return Operation("set",ent,to=self)
     def germinate_operation(self, op):
         set_ent=Entity(self.id,status=-1)
-        create_ent=Entity(name='oak',parents=['oak'],location=Location(self.location.parent, self.location.coordinates))
+        create_ent=Entity(name='oak',parents=['oak'],location=Location(self.location.parent, self.location.coordinates),mass=self.mass,bbox=[-0.02, -0.02, 0, 0.02, 0.02, 0.12])
         res = Operation("set",set_ent,to=self)
         res = res + Operation("create",create_ent,to=self)
         return res
