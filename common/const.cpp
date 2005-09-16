23c40329914dda49b73225ec4c7db02df2cdf788
Al Riddoch <alriddoch@googlemail.com>
2005-09-16 22:34:13 +0000

    2005-09-16  Al Riddoch  <alriddoch@zepler.org>
    	* configure.ac: Don't link libdl and libutil explicitly when linking
    	  python dynamically.
    
    	* common/const.cpp: Remove some obsolete comments.

--- common/const.cpp
+++ common/const.cpp
@@ -10,11 +10,9 @@
 
 namespace consts {
 
-  // FIXME
   // Admin password used by admin account
   const char * defaultAdminPasswordHash = "$1$1A67C3C65EECBBCE$A51127573498DCB08992378D07A36A20";
   // Id of root world entity
-  // const char * rootWorldId = "world_0";
   const char * rootWorldId = "0";
   // Version of the software we are running
   const char * version = VERSION;
