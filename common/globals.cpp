da9b258b4bf24eca8e7ad9d886088b64fd439512
Al Riddoch <alriddoch@googlemail.com>
2005-09-23 13:38:48 +0000

    2005-09-23  Al Riddoch  <alriddoch@zepler.org>
    	* common/globals.cpp: Remove debug output.

--- common/globals.cpp
+++ common/globals.cpp
@@ -110,13 +110,10 @@ int loadConfig(int argc, char ** argv, bool server)
 
     if (global_conf->findItem("game", "player_vs_player")) {
         pvp_flag = global_conf->getItem("game", "player_vs_player");
-        std::cout << "Setting pvp to " << pvp_flag << std::endl << std::flush;
     }
 
     if (global_conf->findItem("game", "player_vs_player_offline")) {
         pvp_offl_flag = global_conf->getItem("game", "player_vs_player_offline");
-        std::cout << "Setting pvp_offline to " << pvp_offl_flag << std::endl << std::flush;
-        
     }
 
     // Load up the rulesets. Rulesets are hierarchical, and are read in until
