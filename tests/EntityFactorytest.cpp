fda6345bacf51b0f0be2492b372ebc35c72e6ffd
Al Riddoch <alriddoch@googlemail.com>
2005-11-18 15:09:08 +0000

    2005-11-18  Al Riddoch  <alriddoch@zepler.org>
    	* tests/EntityFactorytest.cpp: Add new tests to verify the Entity
    	  Factory does the right thing with class attributes and inheritance.

--- tests/EntityFactorytest.cpp
+++ tests/EntityFactorytest.cpp
@@ -7,6 +7,7 @@
 #include "rulesets/World.h"
 
 #include "server/EntityFactory.h"
+#include "server/PersistantThingFactory.h"
 
 #include <Atlas/Objects/Anonymous.h>
 
@@ -19,7 +20,7 @@ class ExposedEntityFactory : public EntityFactory {
   public:
     explicit ExposedEntityFactory(BaseWorld & w) : EntityFactory(w) { }
 
-    void getRulesFromFiles(Atlas::Message::MapType & rules) {
+    void getRulesFromFiles(MapType & rules) {
         EntityFactory::getRulesFromFiles(rules);
     }
     void installRules() {
@@ -31,7 +32,7 @@ class ExposedEntityFactory : public EntityFactory {
     }
     void populateFactory(const std::string & className,
                          FactoryBase * factory,
-                         const Atlas::Message::MapType & classDesc) {
+                         const MapType & classDesc) {
         EntityFactory::populateFactory(className, factory, classDesc);
     }
     FactoryBase * getNewFactory(const std::string & clss) {
@@ -39,14 +40,16 @@ class ExposedEntityFactory : public EntityFactory {
     }
     int installEntityClass(const std::string & className,
                            const std::string & parent,
-                           const Atlas::Message::MapType & classDesc) {
+                           const MapType & classDesc) {
         return EntityFactory::installEntityClass(className, parent, classDesc);
     }
     int installOpDefinition(const std::string & opDefName,
                             const std::string & parent,
-                            const Atlas::Message::MapType & opDefDesc) {
+                            const MapType & opDefDesc) {
         return EntityFactory::installOpDefinition(opDefName, parent, opDefDesc);
     }
+
+    const FactoryDict & factoryDict() const { return m_factories; }
 
 };
 
@@ -93,5 +96,134 @@ int main()
 
         EntityFactory::del();
         assert(EntityFactory::instance() == 0);
+    }
+
+    {
+        World e("1", 1);
+        TestWorld test_world(e);
+        Anonymous attributes;
+        Atlas::Message::Element val;
+
+        ExposedEntityFactory entity_factory(test_world);
+
+        Entity * test_ent = entity_factory.newEntity("1", 1, "thing", attributes);
+        assert(test_ent != 0);
+        assert(!test_ent->get("funky", val));
+        assert(val.isNone());
+
+        attributes->setAttr("funky", "true");
+
+        test_ent = entity_factory.newEntity("1", 1, "thing", attributes);
+        assert(test_ent != 0);
+        assert(test_ent->get("funky", val));
+        assert(val.isString());
+        assert(val.String() == "true");
+
+        assert(entity_factory.newEntity("1", 1, "custom_type", attributes) == 0);
+
+        const FactoryDict & factory_dict = entity_factory.factoryDict();
+
+        assert(!factory_dict.empty());
+
+        assert(factory_dict.find("custom_type") == factory_dict.end());
+
+        {
+            MapType custom_type_description;
+            MapType attrs;
+            MapType test_custom_type_attr;
+            test_custom_type_attr["default"] = "test_value";
+            test_custom_type_attr["visibility"] = "public";
+            attrs["test_custom_type_attr"] = test_custom_type_attr;
+            custom_type_description["attributes"] = attrs;
+
+            int ret = entity_factory.installEntityClass("custom_type", "thing", custom_type_description);
+
+            assert(ret == 0);
+        }
+
+        FactoryDict::const_iterator I = factory_dict.find("custom_type");
+        assert(I != factory_dict.end());
+        FactoryBase * custom_type_factory = I->second;
+
+        MapType::const_iterator J = custom_type_factory->m_attributes.find("test_custom_type_attr");
+        assert(J != custom_type_factory->m_attributes.end());
+        assert(J->second.isString());
+        assert(J->second.String() == "test_value");
+
+        test_ent = entity_factory.newEntity("1", 1, "custom_type", attributes);
+        assert(test_ent != 0);
+
+        val = Atlas::Message::Element();
+        assert(val.isNone());
+
+        assert(test_ent->get("funky", val));
+        assert(val.isString());
+        assert(val.String() == "true");
+
+        val = Atlas::Message::Element();
+        assert(val.isNone());
+
+        assert(test_ent->get("test_custom_type_attr", val));
+        assert(val.isString());
+        assert(val.String() == "test_value");
+
+        assert(entity_factory.newEntity("1", 1, "custom_inherited_type", attributes) == 0);
+        assert(factory_dict.find("custom_inherited_type") == factory_dict.end());
+
+        {
+            MapType custom_inherited_type_description;
+            MapType attrs;
+            MapType test_custom_type_attr;
+            test_custom_type_attr["default"] = "test_inherited_value";
+            test_custom_type_attr["visibility"] = "public";
+            attrs["test_custom_inherited_type_attr"] = test_custom_type_attr;
+            custom_inherited_type_description["attributes"] = attrs;
+
+            int ret = entity_factory.installEntityClass("custom_inherited_type", "custom_type", custom_inherited_type_description);
+
+            assert(ret == 0);
+        }
+
+        I = factory_dict.find("custom_inherited_type");
+        assert(I != factory_dict.end());
+        FactoryBase * custom_inherited_type_factory = I->second;
+        assert(custom_inherited_type_factory != 0);
+
+        J = custom_inherited_type_factory->m_attributes.find("test_custom_type_attr");
+        assert(J != custom_inherited_type_factory->m_attributes.end());
+        assert(J->second.isString());
+        assert(J->second.String() == "test_value");
+
+        J = custom_inherited_type_factory->m_attributes.find("test_custom_inherited_type_attr");
+        assert(J != custom_inherited_type_factory->m_attributes.end());
+        assert(J->second.isString());
+        assert(J->second.String() == "test_inherited_value");
+
+        test_ent = entity_factory.newEntity("1", 1, "custom_inherited_type", attributes);
+        assert(test_ent != 0);
+
+        val = Atlas::Message::Element();
+        assert(val.isNone());
+
+        assert(test_ent->get("funky", val));
+        assert(val.isString());
+        assert(val.String() == "true");
+
+        val = Atlas::Message::Element();
+        assert(val.isNone());
+
+        assert(test_ent->get("test_custom_type_attr", val));
+        assert(val.isString());
+        assert(val.String() == "test_value");
+
+        val = Atlas::Message::Element();
+        assert(val.isNone());
+
+        assert(test_ent->get("test_custom_inherited_type_attr", val));
+        assert(val.isString());
+        assert(val.String() == "test_inherited_value");
+
+        // FIXME TODO Modify a type, and ensure attribute propagate to inherited types.
+
     }
 }
