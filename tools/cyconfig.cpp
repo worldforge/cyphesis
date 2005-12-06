bae9050c2af29d538eedaf67e10a12f99d4460e1
Al Riddoch <alriddoch@googlemail.com>
2005-12-06 01:48:43 +0000

    2005-12-05  Al Riddoch  <alriddoch@zepler.org>
    	* tools/cyconfig.cpp: Start adding a system for processing commands,
    	  so cyconfig can be used to perform more server configuration
    	  tasks.

--- tools/cyconfig.cpp
+++ tools/cyconfig.cpp
@@ -4,6 +4,39 @@
 
 #include <varconf/Config.h>
 
+#include <cassert>
+
+static int install(int argc, char ** argv)
+{
+    if (argc < 2) {
+        std::cerr << "usage: " << argv[0] << " <ruleset> [ <directory> | <zipfile> ]" << std::endl << std::flush;
+        return 1;
+    }
+    return 0;
+}
+
+struct command {
+    const char * cmd_string;
+    int (*cmd_function)(int argc, char ** argv);
+    const char * cmd_descrption;
+};
+
+static struct command commands[] = {
+    { "install", &install, "Install a ruleset", },
+    { 0, }
+};
+
+static int runCommand(int argc, char ** argv)
+{
+    assert(argc > 0);
+    for (struct command * i = &commands[0]; i->cmd_string != 0; ++i) {
+        if (strcmp(argv[0], i->cmd_string) == 0) {
+            return i->cmd_function(argc, argv);
+        }
+    }
+    return 1;
+}
+
 int main(int argc, char ** argv)
 {
     char * home;
@@ -14,7 +47,12 @@ int main(int argc, char ** argv)
     std::string homeDirConfig = std::string(home) + "/.cyphesis.vconf";
     varconf::Config * global_conf = varconf::Config::inst();
     global_conf->readFromFile(homeDirConfig);
-    global_conf->getCmdline(argc, argv);
+    int optind = global_conf->getCmdline(argc, argv);
+
+    if (optind > 0 && optind < argc) {
+        runCommand(argc - optind, &argv[optind]);
+    }
+
     global_conf->writeToFile(homeDirConfig);
     return 0;
 }
