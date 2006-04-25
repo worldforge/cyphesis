53879c8ca6405be97fca31faf157b229e177bcca
Al Riddoch <alriddoch@googlemail.com>
2006-04-25 15:00:00 +0000

    2006-04-25  Al Riddoch  <alriddoch@zepler.org>
    	* rulesets/basic/test_python_api.py: More tests of the Python API
    	  for Atlas Operations.

--- rulesets/basic/test_python_api.py
+++ rulesets/basic/test_python_api.py
@@ -34,6 +34,29 @@ def default(mapeditor):
     assert(operation.to=="8")
     assert(operation.from_=="9")
 
+    assert(operation.getSerialno() != 452)
+    assert(operation.getRefno() != 567)
+    assert(operation.getFrom() != "10")
+    assert(operation.getTo() != "11")
+    assert(operation.getSeconds() != 577)
+    assert(operation.getFutureSeconds() != 345)
+
+    operation.setSerialno(452)
+    operation.setRefno(567)
+    operation.setFrom("10")
+    operation.setTo("11")
+    operation.setSeconds(577.5)
+    operation.setFutureSeconds(345.5)
+    operation.setArgs([atlas.Operation("set")])
+
+    assert(operation.getSerialno() == 452)
+    assert(operation.getRefno() == 567)
+    assert(operation.getFrom() == "10")
+    assert(operation.getTo() == "11")
+    assert(operation.getSeconds() == 577.5)
+    assert(operation.getFutureSeconds() == 345.5)
+    assert(len(operation.getArgs()) == 1)
+
     print "Test location"
     a=1
     if atlas.isLocation(a):
