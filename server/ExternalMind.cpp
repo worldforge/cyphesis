d738a3908f0e6e3e3e91de0828d51c23376702e9
Al Riddoch <alriddoch@googlemail.com>
2007-01-19 14:20:50 +0000

    2007-01-18  Al Riddoch  <alriddoch@zepler.org>
    	* NEWS: Update NEWS announcement for release.

--- server/ExternalMind.cpp
+++ server/ExternalMind.cpp
@@ -15,11 +15,23 @@
 // along with this program; if not, write to the Free Software Foundation,
 // Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-// $Id: ExternalMind.cpp,v 1.18 2006-10-26 00:48:14 alriddoch Exp $
+// $Id: ExternalMind.cpp,v 1.19 2007-01-19 14:20:50 alriddoch Exp $
 
 #include "ExternalMind.h"
 
 #include "Connection_methods.h"
+
+#include <Atlas/Objects/SmartPtr.h>
+#include <Atlas/Objects/Operation.h>
+#include <Atlas/Objects/Anonymous.h>
+
+using Atlas::Message::Element;
+using Atlas::Objects::Root;
+using Atlas::Objects::smart_dynamic_cast;
+using Atlas::Objects::Entity::RootEntity;
+using Atlas::Objects::Entity::Anonymous;
+using Atlas::Objects::Operation::Sight;
+using Atlas::Objects::Operation::Imaginary;
 
 ExternalMind::ExternalMind(Connection & connection,
                            const std::string & id, long intId) :
@@ -37,4 +49,43 @@ ExternalMind::~ExternalMind()
 void ExternalMind::operation(const Operation & op, OpVector &)
 {
     m_connection.send(op);
+
+    // Here we see if there is anything we should be sending the user
+    // extra info about. The initial demo implementation checks for
+    // Set ops which make the characters status less than 0.1, and sends
+    // emotes that the character is hungry.
+    const std::vector<Root> & args = op->getArgs();
+    if (op->getClassNo() == OP_SIGHT && !args.empty()) {
+        Operation sub_op = smart_dynamic_cast<Operation>(args.front());
+        if (sub_op.isValid()) {
+            const std::vector<Root> & sub_args = sub_op->getArgs();
+            if (sub_op->getClassNo() == OP_SET && !sub_args.empty()) {
+                const Root & arg = sub_args.front();
+                Element status_value;
+                if (arg->copyAttr("status", status_value) == 0 and
+                    status_value.isFloat() and status_value.Float() < 0.1) {
+
+                    Anonymous imaginary_arg;
+                    imaginary_arg->setId(getId());
+                    if (status_value.Float() < 0.01) {
+                        imaginary_arg->setAttr("description", "is starving.");
+                    } else {
+                        imaginary_arg->setAttr("description", "is hungry.");
+                    }
+
+                    Imaginary imaginary;
+                    imaginary->setTo(getId());
+                    imaginary->setFrom(getId());
+                    imaginary->setArgs1(imaginary_arg);
+
+                    Sight sight;
+                    sight->setTo(getId());
+                    sight->setFrom(getId());
+                    sight->setArgs1(imaginary);
+
+                    m_connection.send(sight);
+                }
+            }
+        }
+    }
 }
