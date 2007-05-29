b554571606685cbf3adcb89d083a8e9383aba43e
Al Riddoch <alriddoch@googlemail.com>
2007-05-29 00:36:53 +0000

    2007-05-28  Al Riddoch  <alriddoch@zepler.org>
    	* scripts/Info.plist: Add package list spec.
    
    	* scripts/gen_mac_app.sh: Use pre-created package list rather than
    	  trying to generate it on the fly.

--- scripts/gen_mac_app.sh
+++ scripts/gen_mac_app.sh
@@ -8,19 +8,20 @@ PLIST_FILE=${CONTENT_DIRECTORY}/Info.plist
 mkdir -p ${CONTENT_DIRECTORY}
 
 # Add a minimal package list
-echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" > ${PLIST_FILE}
-echo "<!DOCTYPE plist PUBLIC \"-//Apple Computer//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">" >> ${PLIST_FILE}
-echo "<plist version=\"1.0\">" >> ${PLIST_FILE}
-echo "<dict>" >> ${PLIST_FILE}
-echo "  <key>CFBundleExecutable</key>" >> ${PLIST_FILE}
-echo "  <string>cyphesis</string>" >> ${PLIST_FILE}
-echo "  <key>CFBundleIdentifier</key>" >> ${PLIST_FILE}
-echo "  <string>org.worldforge.cyphesis</string>" >> ${PLIST_FILE}
-echo "  <key>CFBundleName</key>" >> ${PLIST_FILE}
-echo "  <string>BARRG</string>" >> ${PLIST_FILE}
-echo "  <key>LSBackgroundOnly</key>" >> ${PLIST_FILE}
-echo "  <boolean>1</boolean>" >> ${PLIST_FILE}
-echo "</dict>" >> ${PLIST_FILE}
-echo "</plist>" >> ${PLIST_FILE}
+# echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" > ${PLIST_FILE}
+# echo "<!DOCTYPE plist PUBLIC \"-//Apple Computer//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">" >> ${PLIST_FILE}
+# echo "<plist version=\"1.0\">" >> ${PLIST_FILE}
+# echo "<dict>" >> ${PLIST_FILE}
+# echo "  <key>CFBundleExecutable</key>" >> ${PLIST_FILE}
+# echo "  <string>cyphesis</string>" >> ${PLIST_FILE}
+# echo "  <key>CFBundleIdentifier</key>" >> ${PLIST_FILE}
+# echo "  <string>org.worldforge.cyphesis</string>" >> ${PLIST_FILE}
+# echo "  <key>CFBundleName</key>" >> ${PLIST_FILE}
+# echo "  <string>BARRG</string>" >> ${PLIST_FILE}
+# echo "  <key>LSBackgroundOnly</key>" >> ${PLIST_FILE}
+# echo "  <boolean>1</boolean>" >> ${PLIST_FILE}
+# echo "</dict>" >> ${PLIST_FILE}
+# echo "</plist>" >> ${PLIST_FILE}
+cp scripts/Info.plist ${PLIST_FILE}
 
-./configure --bindir=`pwd`/${CONTENT_DIRECTORY}/MacOS --datadir=`pwd`/${CONTENT_DIRECTORY}/Resources --sysconfdir=`pwd`/${CONTENT_DIRECTORY}/Resources/etc --localstatedir=/var
+# ./configure --bindir=`pwd`/${CONTENT_DIRECTORY}/MacOS --datadir=`pwd`/${CONTENT_DIRECTORY}/Resources --sysconfdir=`pwd`/${CONTENT_DIRECTORY}/Resources/etc --localstatedir=/var
