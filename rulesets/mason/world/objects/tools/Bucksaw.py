c1e3c255193dc1db8b353cbdf7f5f84fc3a90f1c
Al Riddoch <alriddoch@googlemail.com>
2007-06-12 18:57:53 +0000

    2007-06-12  Al Riddoch  <alriddoch@zepler.org>
    	* rulesets/mason/world/objects/tools/Bucksaw.py: Add an error message
    	  if this code gets called, as it should no longer be.

--- rulesets/mason/world/objects/tools/Bucksaw.py
+++ rulesets/mason/world/objects/tools/Bucksaw.py
@@ -8,6 +8,7 @@ from world.objects.Thing import Thing
 class Bucksaw(Thing):
     """This is a large saw for cutting logs"""
     def cut_operation(self, op):
+        print "Bucksaw.cur_operation() Don't call this."
         #to_ = self.world.get_object(op[1].id)
         to_ = op[0].id
         if not to_:
