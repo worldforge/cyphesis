a8a57f2c3044b6f4f407dd7f89314cb25c63d729
Al Riddoch <alriddoch@googlemail.com>
2007-07-04 20:38:22 +0000

    2007-07-04  Al Riddoch  <alriddoch@zepler.org>
    	* data/mason.xml: New task class for pickaxe. Remove useless script
    	  from rope.
    
    	* rulesets/mason/world/tasks/Delve.py: Add new task for using a
    	  pickaxe to get rock from the terrain.

---
+++ rulesets/mason/world/tasks/Delve.py
@@ -0,0 +1,77 @@
+#This file is distributed under the terms of the GNU General Public license.
+#Copyright (C) 2006 Al Riddoch (See the file COPYING for details).
+
+from atlas import *
+from physics import *
+from Quaternion import Quaternion
+from Vector3D import Vector3D
+
+from cyphesis.Thing import Thing
+
+print "Importing Delve"
+
+class Delve(Thing):
+    """ A task for cutting a log into boards."""
+    def cut_operation(self, op):
+        """ Op handler for cut op which activates this task """
+        # print "Delve.cut"
+
+        if len(op) < 1:
+            sys.stderr.write("Delve task has no target in cut op")
+
+        # FIXME Use weak references, once we have them
+        self.target = op[0].id
+        self.tool = op.to
+
+        self.pos = Vector3D(op[0].pos)
+
+    def tick_operation(self, op):
+        """ Op handler for regular tick op """
+        # print "Delve.tick"
+
+        target=self.character.world.get_object(self.target)
+        if not target:
+            # print "Target is no more"
+            self.irrelevant()
+            return
+
+        old_rate = self.rate
+
+        self.rate = 0.1 / 1.75
+        self.progress += 0.1
+
+        if old_rate < 0.01:
+            self.progress = 0
+        else:
+            self.progress += 0.1
+
+        # print "%s" % self.pos
+
+        if self.progress < 1:
+            # print "Not done yet"
+            return self.next_tick(1.75)
+
+        self.progress = 0
+
+        surface = target.terrain.get_surface(self.character.location.coordinates)
+        # print "SURFACE %d at %s" % (surface, self.pos)
+        if surface is not 0:
+            # print "Not rock"
+            return self.next_tick(1.75)
+
+        res=Message()
+
+        chunk_loc = Location(self.character.location.parent)
+        chunk_loc.velocity = Vector3D()
+
+        chunk_loc.coordinates = self.pos
+
+        create=Operation("create",
+                         Entity(name = "boulder",
+                                type = "boulder",
+                                location = chunk_loc), to = target)
+        res.append(create)
+
+        res.append(self.next_tick(1.75))
+
+        return res
