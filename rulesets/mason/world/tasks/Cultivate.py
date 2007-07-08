e85db448c2ae930bdb2975e0d9d113b7a59d254b
Al Riddoch <alriddoch@googlemail.com>
2007-07-08 19:38:02 +0000

    2007-07-08  Al Riddoch  <alriddoch@zepler.org>
    	* rulesets/mason/world/tasks/Cultivate.py: Add new task script for
    	  cultivating plants from seed.
    
    	* data/mason.xml: Add task class for cultivate. Add GERMINATES
    	  property to all seed classes. Remove germinate operation definition
    	  and trowel script.

---
+++ rulesets/mason/world/tasks/Cultivate.py
@@ -0,0 +1,68 @@
+#This file is distributed under the terms of the GNU General Public license.
+#Copyright (C) 2006 Al Riddoch (See the file COPYING for details).
+
+from atlas import *
+from physics import *
+from Quaternion import Quaternion
+from Vector3D import Vector3D
+
+try:
+  from random import *
+except ImportError:
+  from whrandom import *
+
+from cyphesis.Thing import Thing
+
+class Cultivate(Thing):
+    """ A proof of concept task for sharpening a log into a stake."""
+    def sow_operation(self, op):
+        """ Op handler for sow op which activates this task """
+        # print "Cultivate.sow"
+
+        if len(op) < 1:
+            sys.stderr.write("Cultivate task has no target in sow op")
+
+        # FIXME Use weak references, once we have them
+        self.target = op[0].id
+        self.tool = op.to
+
+    def tick_operation(self, op):
+        """ Op handler for regular tick op """
+        # print "Cultivate.tick"
+        res=Message()
+
+        target=self.character.world.get_object(self.target)
+        if not target:
+            # print "Target is no more"
+            self.irrelevant()
+            return
+
+        if square_distance(self.character.location, target.location) > \
+           self.character.location.bbox.square_bounding_radius():
+            self.progress = 0
+            self.rate = 0
+            return self.next_tick(1.75)
+
+        if self.rate < 0.01:
+            self.progress = 0
+            self.rate = 1 / 1.75
+            return self.next_tick(1.75)
+
+        new_loc = target.location.copy()
+        new_loc.orientation = target.location.orientation
+        create=Operation("create", Entity(type = target.germinates,
+                                          mass = target.mass,
+                                          bbox = [-0.02, -0.02, 0,
+                                                  0.02, 0.02, 0.12],
+                                          location = new_loc), to = target)
+        res.append(create)
+
+        set=Operation("set", Entity(self.target, status=-1), to=target)
+        res.append(set)
+
+        self.progress = 1
+        self.rate = 0
+        
+        res.append(self.next_tick(1.75))
+
+        return res
