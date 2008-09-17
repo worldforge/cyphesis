027f8fd20590bd0ff110190db1bb850be42acece
Al Riddoch <alriddoch@googlemail.com>
2008-09-17 17:31:47 +0000

    2008-09-17  Al Riddoch  <alriddoch@googlemail.com>
    	* dox/cyphesis/Thing.py, dox/cyphesis/__init__.py: Add a tree of
    	  stub python files used to document the API exposed by the C++ core.
    
    	* Doxyfile-python: Trim the directories down to the top level, and
    	  turn on recursion.

---
+++ dox/cyphesis/Thing.py
@@ -0,0 +1,6 @@
+## \brief Interface to server core objects.
+##
+## This is the interface class for all entity, task and mind scripts.
+## It provides the interface to the underlying object in the server core.
+class Thing:
+  pass
