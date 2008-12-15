e1e947d358bf6c3f5ca2260df43c8e66ace83c00
Al Riddoch <alriddoch@googlemail.com>
2008-12-15 18:49:44 +0000

    2008-12-15  Al Riddoch  <alriddoch@googlemail.com>
    	* rulesets/mason/benchmarks.py: Add a client script which exercises
    	  entity creation, destruction and all connected subsystems.

---
+++ rulesets/mason/benchmarks.py
@@ -0,0 +1,21 @@
+#This file is distributed under the terms of the GNU General Public license.
+#Copyright (C) 2008 Alistair Riddoch (See the file COPYING for details).
+
+from atlas import *
+from random import *
+from cyphesis.editor import editor
+from Quaternion import Quaternion
+from Vector3D import Vector3D
+from math import *
+
+import time
+
+def default(mapeditor):
+    # Spray the world with rubbish
+
+    m=editor(mapeditor)
+
+    for i in range(10000):
+        m.make('oak', pos=(uniform(-500, 300), uniform(-500, 300), 0))
+        m.delete(m.look_for(type='oak').id)
+        time.sleep(0.1)
