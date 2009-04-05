f435a49658d0df43e3bdcac9af797898cd32a3df
Alistair Riddoch <alriddoch@googlemail.com>
2009-04-05 04:50:44 +0100

    Implement logging in using urllib and raw httplib

--- scripts/happypenguin_announce.py
+++ scripts/happypenguin_announce.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 
+import cookielib
 import httplib
+import re
+import sys
+import urllib2
 
 # <form action="login.php" method="post" target="_top">
 # <input type="text" class="post" name="username" size="25" maxlength="40" value="" />
@@ -11,20 +15,51 @@ import httplib
 
 # username=wef&password=wef&autologin=on&redirect=&login=Log+in
 
-_USERNAME='alriddoch'
-_PASSWORD=''
+_USERNAME='bar'
 
-def Main():
+def get_using_urllib(password):
+    cj = cookielib.CookieJar()
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+    body = "username=%s&password=%s&autologin=on&redirect=&login=Log+in" % (_USERNAME, password)
+    res = opener.open('http://happypenguin.org/forums/login.php', body)
+    content = res.read()
+
+    if re.search('incorrect or inactive username', content):
+        print 'login failed'
+    elif re.search('You are not allowed to login', content):
+        print 'login banned'
+    else:
+        print 'dunno'
+        print content
+
+    res = opener.open('http://happypenguin.org/')
+    content = res.read()
+
+def get_using_httplib(password):
     con = httplib.HTTPConnection('happypenguin.org')
-    # con = httplib.HTTPConnection('localhost', 8080)
-    # con = httplib.HTTPConnection('http://happypenguin.org/forums/login.php')
-    body = "username=%s&password=%s&autologin=on&redirect=&login=Log+in" % (_USERNAME, _PASSWORD)
-    con.request('POST', '/forums/login.php', body)
+
+    body = "username=%s&password=%s&autologin=on&redirect=&login=Log+in" % (_USERNAME, password)
+    headers = {}
+    headers['Content-Type'] = 'application/x-www-form-urlencoded'
+    con.request('POST', '/forums/login.php', body, headers)
     res = con.getresponse()
     content = res.read()
+    headers = res.getheaders()
     print res.status
     print content
-    print 'foo'
+    print headers
+
+    for h in headers:
+      if len(h) < 2:
+        continue
+      if h[0] == 'set-cookie':
+        print "COOKIE", h[1]
+
+def Main():
+    if len(sys.argv) != 2:
+        print "usage: announce <password>"
+        exit(1)
+    get_using_urllib(sys.argv[1])
 
 if __name__ == '__main__':
     Main()
