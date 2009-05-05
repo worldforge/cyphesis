3520bf12a2b6ba669cfe21f4fdf3aa56edd9fff4
Alistair Riddoch <alriddoch@googlemail.com>
2009-05-05 22:28:18 +0100

    Assert that position and velocity are valid when resolving collisions

--- rulesets/Motion.cpp
+++ rulesets/Motion.cpp
@@ -65,6 +65,8 @@ float Motion::checkCollisions()
 {
     assert(m_entity.m_location.m_loc != 0);
     assert(m_entity.m_location.m_loc->m_contains != 0);
+    assert(m_entity.m_location.m_pos.isValid());
+    assert(m_entity.m_location.m_velocity.isValid());
     // Check to see whether a collision is going to occur from now until the
     // the next tick in consts::move_tick seconds
     float coll_time = consts::move_tick;
