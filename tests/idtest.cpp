c0342922b88034f2b8571a8aeb408bb64db28540
Alistair Riddoch <alriddoch@googlemail.com>
2009-05-05 21:13:53 +0100

    Complete test coverage for id.cpp

--- tests/idtest.cpp
+++ tests/idtest.cpp
@@ -19,7 +19,21 @@
 
 #include "common/id.h"
 
+#include <iostream>
+
+#include <cstdlib>
+
+#include <setjmp.h>
+#include <signal.h>
+
 #include <cassert>
+
+jmp_buf env;
+
+static void handle_abort(int)
+{
+    longjmp(env, 1);
+}
 
 int main()
 {
@@ -86,8 +100,31 @@ int main()
     {
         std::string text("text");
 
+        assert(integerIdCheck(text) == -1);
+    }
+
+    {
+        std::string text("text");
+
         assert(integerId(text) == -1);
     }
+
+    if (setjmp(env) == 0) {
+        signal(SIGABRT, handle_abort);
+
+        // This should abort due to hitting an abort call
+        {
+            std::string text("text");
+
+            forceIntegerId(text);
+        }
+
+        // The abort causes control to go back to setjmp
+        return 1;
+    }
+
+    std::cout << "It worked" << std::endl << std::flush;
+
 
     return 0;
 }
