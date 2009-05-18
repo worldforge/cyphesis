4e8a8b020bafa431c3d192cb8b77fabf0d28148a
Alistair Riddoch <alriddoch@googlemail.com>
2009-05-18 17:39:22 +0100

    Cover all the Python_API.cpp code available from the interpreter

--- tests/Python_APItest.cpp
+++ tests/Python_APItest.cpp
@@ -27,6 +27,65 @@ int main()
 {
     init_python_api();
 
+    assert(PyRun_SimpleString("print 'hello'") == 0);
+    assert(PyRun_SimpleString("import sys") == 0);
+    assert(PyRun_SimpleString("sys.stdout.write('hello')") == 0);
+    assert(PyRun_SimpleString("sys.stdout.write(1)") == -1);
+    assert(PyRun_SimpleString("sys.stderr.write('hello')") == 0);
+    assert(PyRun_SimpleString("sys.stderr.write(1)") == -1);
+
+    assert(PyRun_SimpleString("from common import log") == 0);
+    assert(PyRun_SimpleString("log.debug('foo')") == 0);
+    assert(PyRun_SimpleString("log.thinking('foo')") == 0);
+
+    assert(PyRun_SimpleString("import atlas") == 0);
+
+    assert(PyRun_SimpleString("l=atlas.Location()") == 0);
+    assert(PyRun_SimpleString("atlas.isLocation(l)") == 0);
+    assert(PyRun_SimpleString("atlas.isLocation(1)") == 0);
+    assert(PyRun_SimpleString("l1=atlas.Location()") == 0);
+    assert(PyRun_SimpleString("l2=atlas.Location()") == 0);
+
+    assert(PyRun_SimpleString("import physics") == 0);
+    assert(PyRun_SimpleString("physics.distance_to()") == -1);
+    assert(PyRun_SimpleString("physics.square_distance()") == -1);
+    assert(PyRun_SimpleString("physics.square_horizontal_distance()") == -1);
+    assert(PyRun_SimpleString("physics.distance_to(l1, l2)") == 0);
+    assert(PyRun_SimpleString("physics.square_distance(l1, l2)") == 0);
+    assert(PyRun_SimpleString("physics.square_horizontal_distance(l1, l2)") == 0);
+    assert(PyRun_SimpleString("physics.distance_to('1', l2)") == -1);
+    assert(PyRun_SimpleString("physics.square_distance('1', l2)") == -1);
+    assert(PyRun_SimpleString("physics.square_horizontal_distance('1', l2)") == -1);
+
+    assert(PyRun_SimpleString("atlas.Entity()") == 0);
+    assert(PyRun_SimpleString("e=atlas.Entity('1')") == 0);
+    assert(PyRun_SimpleString("e=atlas.Entity(1)") == -1);
+    assert(PyRun_SimpleString("e=atlas.Entity('1', location='loc')") == -1);
+    assert(PyRun_SimpleString("e=atlas.Entity('1', location=l)") == 0);
+    assert(PyRun_SimpleString("e=atlas.Entity('1', pos=())") == 0);
+    assert(PyRun_SimpleString("e=atlas.Entity('1', pos=[])") == 0);
+    assert(PyRun_SimpleString("e=atlas.Entity('1', pos=(1,1.0,'1'))") == -1);
+    assert(PyRun_SimpleString("e=atlas.Entity('1', pos=[1,1.0,'1'])") == -1);
+    assert(PyRun_SimpleString("e=atlas.Entity('1', pos=1)") == -1);
+    assert(PyRun_SimpleString("e=atlas.Entity('1', parent=1)") == -1);
+    assert(PyRun_SimpleString("e=atlas.Entity('1', parent='0')") == 0);
+    assert(PyRun_SimpleString("e=atlas.Entity('1', type=1)") == -1);
+    assert(PyRun_SimpleString("e=atlas.Entity('1', type='pig')") == 0);
+    assert(PyRun_SimpleString("e=atlas.Entity('1', other=1)") == 0);
+
+    assert(PyRun_SimpleString("o=atlas.Operation()") == -1);
+    assert(PyRun_SimpleString("o=atlas.Operation('get')") == 0);
+    assert(PyRun_SimpleString("o=atlas.Operation('thought')") == 0);
+    assert(PyRun_SimpleString("o=atlas.Operation('not valid')") == -1);
+    assert(PyRun_SimpleString("o=atlas.Operation('get', to='1', from_='1')") == 0);
+    assert(PyRun_SimpleString("o=atlas.Operation('get', to=e, from_=e)") == 0);
+    assert(PyRun_SimpleString("o=atlas.Operation('get', atlas.Entity(), to='1', from_='1')") == 0);
+    assert(PyRun_SimpleString("o=atlas.Operation('get', atlas.Operation('set'), to='1', from_='1')") == 0);
+    assert(PyRun_SimpleString("o=atlas.Operation('get', atlas.Location(), to='1', from_='1')") == -1);
+    assert(PyRun_SimpleString("o=atlas.Operation('get', atlas.Entity(), atlas.Entity(), atlas.Entity(), to='1', from_='1')") == 0);
+
+    assert(PyRun_SimpleString("atlas.Message(atlas.Operation('get'))") == 0);
+
     assert(PyRun_SimpleString("import BBox") == 0);
     assert(PyRun_SimpleString("b=BBox.BBox()") == 0);
     assert(PyRun_SimpleString("b=BBox.BBox([1])") == -1);
