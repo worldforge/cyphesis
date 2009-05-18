b471454ee30c4c82be038ceaeef0ff19172c03e2
Alistair Riddoch <alriddoch@googlemail.com>
2009-05-19 00:00:18 +0100

    Extend the coverage of Py_BBox.cpp

--- tests/Py_BBoxtest.cpp
+++ tests/Py_BBoxtest.cpp
@@ -29,7 +29,19 @@ int main()
 
     assert(PyRun_SimpleString("import BBox") == 0);
     assert(PyRun_SimpleString("b=BBox.BBox()") == 0);
-    assert(PyRun_SimpleString("import Point3D") == 0);
+    assert(PyRun_SimpleString("print b.near_point") == 0);
+    assert(PyRun_SimpleString("print b.far_point") == 0);
+    assert(PyRun_SimpleString("print b.square_bounding_radius()") == 0);
+    assert(PyRun_SimpleString("import Vector3D") == 0);
+    assert(PyRun_SimpleString("b.near_point=Vector3D.Vector3D()") == -1);
+    assert(PyRun_SimpleString("b.far_point=Vector3D.Vector3D()") == -1);
+    assert(PyRun_SimpleString("b.near_point=Vector3D.Vector3D(0,0,0)") == 0);
+    assert(PyRun_SimpleString("b.far_point=Vector3D.Vector3D(1,1,1)") == 0);
+    assert(PyRun_SimpleString("b.other=Vector3D.Vector3D()") == -1);
+    assert(PyRun_SimpleString("print b.square_bounding_radius()") == 0);
+    assert(PyRun_SimpleString("b2=BBox.BBox()") == 0);
+    assert(PyRun_SimpleString("print b == b2") == 0);
+    
 
     shutdown_python_api();
     return 0;
