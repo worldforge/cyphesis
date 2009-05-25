eae3d69885247a2a811f27c73355bc75405f4db2
Alistair Riddoch <alriddoch@googlemail.com>
2009-05-25 16:35:25 +0100

    Add initial coverage test for PyMap, PyMind and PyTask

---
+++ tests/Py_Maptest.cpp
@@ -0,0 +1,50 @@
+// Cyphesis Online RPG Server and AI Engine
+// Copyright (C) 2009 Alistair Riddoch
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+// $Id$
+
+#include <Python.h>
+
+#include "rulesets/Python_API.h"
+
+#include <cassert>
+
+int main()
+{
+    init_python_api();
+
+    assert(PyRun_SimpleString("from server import Map") == 0);
+    assert(PyRun_SimpleString("from atlas import Location") == 0);
+    assert(PyRun_SimpleString("m=Map()") == 0);
+    assert(PyRun_SimpleString("m.find_by_location()") == -1);
+    assert(PyRun_SimpleString("l=Location()") == 0);
+    assert(PyRun_SimpleString("m.find_by_location(l)") == -1);
+    assert(PyRun_SimpleString("m.find_by_location(l, 5.0, 'foo')") == -1);
+    assert(PyRun_SimpleString("m.find_by_type()") == -1);
+    assert(PyRun_SimpleString("m.find_by_type('foo')") == 0);
+    assert(PyRun_SimpleString("m.add()") == -1);
+    assert(PyRun_SimpleString("m.delete()") == -1);
+    assert(PyRun_SimpleString("m.get()") == -1);
+    assert(PyRun_SimpleString("m.get_add()") == -1);
+    assert(PyRun_SimpleString("m.update()") == -1);
+    assert(PyRun_SimpleString("m.add_hooks_append()") == -1);
+    assert(PyRun_SimpleString("m.update_hooks_append()") == -1);
+    assert(PyRun_SimpleString("m.delete_hooks_append()") == -1);
+
+    shutdown_python_api();
+    return 0;
+}
