3a0c0bd1c0b1d8c76799854f23e6798fa0590e91
Alistair Riddoch <alriddoch@googlemail.com>
2009-05-28 02:51:55 +0100

    Remove pointless compare function.

--- rulesets/Py_WorldTime.cpp
+++ rulesets/Py_WorldTime.cpp
@@ -75,18 +75,6 @@ static PyObject * WorldTime_getattr(PyWorldTime *self, char *name)
     return Py_FindMethod(WorldTime_methods, (PyObject *)self, name);
 }
 
-static int WorldTime_cmp(PyWorldTime *self, PyObject *other)
-{
-    if (PyString_Check(other)) {
-        printf("Python compare of worldtime to string\n");
-        bool eq = (*self->time == std::string(PyString_AsString(other)));
-        return eq ? 0 : -1;
-    } else {
-        printf("Python compare of worldtime to ?\n");
-        return -1;
-    }
-}
-
 static PyObject * WorldTime_new(PyTypeObject * type, PyObject *, PyObject *)
 {
     // This looks allot like the default implementation, except we set some
@@ -122,7 +110,7 @@ PyTypeObject PyWorldTime_Type = {
         0,                              // tp_print
         (getattrfunc)WorldTime_getattr, // tp_getattr
         0,                              // tp_setattr
-        (cmpfunc)WorldTime_cmp,         // tp_compare
+        0,                              // tp_compare
         0,                              // tp_repr
         0,                              // tp_as_number
         0,                              // tp_as_sequence
