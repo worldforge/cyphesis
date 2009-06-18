247499f34dc4a16f6e031129064a10661f6eab0b
Alistair Riddoch <alriddoch@googlemail.com>
2009-06-18 01:15:53 +0100

    Ensure that a TasksProperty is installed whenever tasks are updated.

--- rulesets/Character.cpp
+++ rulesets/Character.cpp
@@ -26,6 +26,7 @@
 #include "EntityProperty.h"
 #include "OutfitProperty.h"
 #include "StatusProperty.h"
+#include "TasksProperty.h"
 
 #include "common/op_switch.h"
 #include "common/const.h"
@@ -289,15 +290,14 @@ void Character::updateTask()
         log(ERROR, "Character::updateTask called when no task running");
     }
 
-    Anonymous set_arg;
-    m_task->addToEntity(set_arg);
-    set_arg->setId(getId());
+    TasksProperty * tp = requirePropertyClass<TasksProperty>("tasks");
 
-    Set set;
-    set->setArgs1(set_arg);
-    set->setTo(getId());
-    
-    sendWorld(set);
+    tp->setFlags(flag_unsent);
+
+    Update update;
+    update->setTo(getId());
+
+    sendWorld(update);
 }
 
 /// \brief Clean up and remove the task currently being executed
