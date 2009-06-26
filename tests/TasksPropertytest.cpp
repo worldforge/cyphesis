803b266e213273fb21644f47a86a728111b545a2
Alexey Torkhov <atorkhov@gmail.com>
2009-06-27 02:12:07 +0400

    Add almost 100% coverage for TasksProperty.

--- tests/TasksPropertytest.cpp
+++ tests/TasksPropertytest.cpp
@@ -17,21 +17,64 @@
 
 // $Id$
 
-#include "PropertyCoverage.h"
+//#include "PropertyCoverage.h"
+#include "TestWorld.h"
 
+#include "rulesets/Entity.h"
+#include "rulesets/Character.h"
 #include "rulesets/TasksProperty.h"
+#include "rulesets/TaskScript.h"
+
+#include <Atlas/Message/Element.h>
 
 using Atlas::Message::ListType;
+using Atlas::Message::MapType;
+using Atlas::Message::Element;
 
 int main()
 {
     TasksProperty * ap = new TasksProperty;
 
-    PropertyCoverage pc(ap);
-
-    // Coverage is complete, but it wouldn't hurt to add some bad data here.
-
+//    PropertyCoverage pc(ap);
 //    pc.basicCoverage();
+
+    Entity * const tlve = new Entity("0", 0);
+    BaseWorld * const wrld = new TestWorld(*tlve);
+    Character * const ent = new Character("1", 1);
+    Task * const task = new TaskScript(*ent);
+
+    ent->m_location.m_loc = tlve;
+    ent->m_location.m_pos = Point3D(1,0,0);
+
+    tlve->m_contains = new LocatedEntitySet;
+    tlve->m_contains->insert(ent);
+
+    task->progress() = .1;
+    task->rate() = .1;
+    ent->setTask(task);
+
+    Element val;
+
+    MapType map;
+    map["one"] = 23;
+    map["two"] = 23.;
+    map["three"] = "twenty_three";
+    map["four"] = ListType(1, 23);
+    map["five"] = ListType(1, 23.);
+    map["six"] = ListType(1, "twenty_three");
+
+    ap->get(val);
+
+    ap->set(ListType(1, map));
+
+    ap->install(ent);
+
+    ap->get(val);
+
+    ap->set(ListType(1, map));
+
+    ap->set(23);
+    ap->set(ListType(1, 23));
 
     // The is no code in operations.cpp to execute, but we need coverage.
     return 0;
