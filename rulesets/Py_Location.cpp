240b1ecaf6e9c0ea69b2f564c025a097f09430b5
Alistair Riddoch <alriddoch@googlemail.com>
2009-07-01 02:14:58 +0100

    Extend the checks so Location take any Entity subtype

--- rulesets/Py_Location.cpp
+++ rulesets/Py_Location.cpp
@@ -211,7 +211,8 @@ static int Location_init(PyLocation * self, PyObject * args, PyObject * kwds)
                 refO = PyObject_GetAttrString(refO, "cppthing");
                 decrefO = true;
             }
-            if (!PyEntity_Check(refO) && !PyMind_Check(refO)) {
+            if (!PyEntity_Check(refO) && !PyMind_Check(refO) &&
+                !PyObject_IsInstance(refO, (PyObject *)&PyEntity_Type)) {
                 PyErr_SetString(PyExc_TypeError, "Arg ref required");
                 if (decrefO) { Py_DECREF(refO); }
                 return -1;
