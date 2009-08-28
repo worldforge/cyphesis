be6cd758fd35266452e8f2f0232beb20abaa9356
Alistair Riddoch <alriddoch@googlemail.com>
2009-08-28 01:24:46 +0100

    Write script to automate maintenance of the regression tests.

---
+++ scripts/update_regression.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+REGRESSION="rulesets/mason/regression.py"
+
+CLASSES=$(grep --exclude=data/tasks.xml --exclude=data/operations.xml \
+            --exclude=data/werewolf.xml --exclude=data/acorn.xml \
+            "^ \+<string name=\"id\">\([a-z_]\+\)<\/string>" data/*.xml |
+          sed "s/^.*<string name=\"id\">\([a-z_]\+\)<\/string>/\"\1\", /" |
+          sort)
+
+# Strip off the trailing ,
+CLASSES=$(echo ${CLASSES} | sed "s/, \?$//")
+
+sed -i -e "s/^ALL_CLASSES=\[.*\]$/ALL_CLASSES=[${CLASSES}]/" ${REGRESSION}
