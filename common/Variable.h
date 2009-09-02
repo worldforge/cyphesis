ec34feaecc4d1120a60781f6e6ae609b8775c17d
Alistair Riddoch <alriddoch@googlemail.com>
2009-09-03 00:27:26 +0100

    Add new files with Monitor class tree code copied from Monitors.*.

---
+++ common/Variable.h
@@ -0,0 +1,50 @@
+// Cyphesis Online RPG Server and AI Engine
+// Copyright (C) 2008 Alistair Riddoch
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+// $Id$
+
+#ifndef COMMON_VARIABLE_H
+#define COMMON_VARIABLE_H
+
+#include <iosfwd>
+
+/// \brief Abstract class for dynamic variable monitors
+///
+/// Variables which should be represented in the monitors output are
+/// referenced by subclasses this interface
+class MonitorBase {
+  public:
+    virtual ~MonitorBase() = 0;
+    virtual void send(std::ostream &) = 0;
+};
+
+/// \brief Concrete class template for dynamic variable monitors
+///
+/// Objects of this type expose the variable they reference to the
+/// monitoring subsystem.
+template<typename T>
+class Monitor : public MonitorBase {
+  protected:
+    const T & m_variable;
+  public:
+    Monitor(const T & variable);
+
+    virtual ~Monitor();
+    virtual void send(std::ostream &);
+};
+
+#endif // COMMON_VARIABLE_H
