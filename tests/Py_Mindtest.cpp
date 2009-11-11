d8b600dcdd631acacac25aad9e1f977040005b3a
Alistair Riddoch <alriddoch@googlemail.com>
2009-11-11 07:40:30 +0000

    Add code to cover the assert checks in bindings.

--- tests/Py_Mindtest.cpp
+++ tests/Py_Mindtest.cpp
@@ -20,12 +20,42 @@
 #include <Python.h>
 
 #include "rulesets/Python_API.h"
+#include "rulesets/Py_Mind.h"
 
 #include <cassert>
+
+static PyObject * null_wrapper(PyObject * self, PyMind * o)
+{
+    if (!PyMind_Check(o)) {
+        PyErr_SetString(PyExc_TypeError, "Unknown Object type");
+        return Py_True;
+    }
+#ifndef NDEBUG
+    o->m_mind = NULL;
+#endif // NDEBUG
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+static PyMethodDef sabotage_methods[] = {
+    {"null", (PyCFunction)null_wrapper,                 METH_O},
+    {NULL,          NULL}                       /* Sentinel */
+};
+
+static void setup_test_functions()
+{
+    PyObject * sabotage = Py_InitModule("sabotage", sabotage_methods);
+    assert(sabotage != 0);
+}
 
 int main()
 {
     init_python_api();
+
+    setup_test_functions();
+
+    PyMind * mind = newPyMind();
+    assert(mind != 0);
 
     assert(PyRun_SimpleString("from server import Mind") == 0);
     assert(PyRun_SimpleString("Mind()") == -1);
@@ -48,6 +78,24 @@ int main()
     assert(PyRun_SimpleString("print m.foo") == 0);
     assert(PyRun_SimpleString("m.bar=[1]") == 0);
     assert(PyRun_SimpleString("m.baz={'foo': 1}") == 0);
+    assert(PyRun_SimpleString("print m.qux") == -1);
+    assert(PyRun_SimpleString("print m.baz") == 0);
+    assert(PyRun_SimpleString("m==m") == 0);
+    assert(PyRun_SimpleString("m2=Mind('2')") == 0);
+    assert(PyRun_SimpleString("m2.mind=1") == -1);
+    assert(PyRun_SimpleString("m2.mind=m") == 0);
+    
+
+#ifndef NDEBUG
+    assert(PyRun_SimpleString("import sabotage") == 0);
+    // Hit the assert checks.
+    assert(PyRun_SimpleString("as_entity_methd=m.as_entity") == 0);
+    assert(PyRun_SimpleString("sabotage.null(m)") == 0);
+    assert(PyRun_SimpleString("as_entity_methd()") == -1);
+    assert(PyRun_SimpleString("m==m") == -1);
+    assert(PyRun_SimpleString("print m.id") == -1);
+    assert(PyRun_SimpleString("m.foo=1") == -1);
+#endif // NDEBUG
 
     shutdown_python_api();
     return 0;
