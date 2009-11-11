1294ed1c63c63fd0fc0a12283fcfdc1da35ecf58
Alistair Riddoch <alriddoch@googlemail.com>
2009-11-11 01:28:27 +0000

    Add code to sabotage the internal structure of an object, so asserts are checked

--- tests/Py_WorldTimetest.cpp
+++ tests/Py_WorldTimetest.cpp
@@ -20,12 +20,42 @@
 #include <Python.h>
 
 #include "rulesets/Python_API.h"
+#include "rulesets/Py_WorldTime.h"
 
 #include <cassert>
+
+static PyObject * null_wrapper(PyObject * self, PyWorldTime * o)
+{
+    if (!PyWorldTime_Check(o)) {
+        PyErr_SetString(PyExc_TypeError, "Unknown Object type");
+        return Py_True;
+    }
+#ifndef NDEBUG
+    o->time = NULL;
+#endif // NDEBUG
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+static PyMethodDef sabotage_methods[] = {
+    {"null", (PyCFunction)null_wrapper,                 METH_O},
+    {NULL,          NULL}                       /* Sentinel */
+};
+
+static void setup_test_functions()
+{
+    PyObject * sabotage = Py_InitModule("sabotage", sabotage_methods);
+    assert(sabotage != 0);
+}
 
 int main()
 {
     init_python_api();
+
+    setup_test_functions();
+
+    PyWorldTime * world_time = newPyWorldTime();
+    assert(world_time != 0);
 
     assert(PyRun_SimpleString("from server import WorldTime") == 0);
     assert(PyRun_SimpleString("WorldTime()") == -1);
@@ -39,6 +69,13 @@ int main()
     assert(PyRun_SimpleString("w.is_now(1)") == -1);
     assert(PyRun_SimpleString("w.seconds()") == 0);
 
+#ifndef NDEBUG
+    assert(PyRun_SimpleString("import sabotage") == 0);
+    assert(PyRun_SimpleString("sabotage.null(w)") == 0);
+    // Hit the assert checks.
+    assert(PyRun_SimpleString("w.is_now('morning')") == -1);
+    assert(PyRun_SimpleString("w.seconds()") == -1);
+#endif // NDEBUG
 
     shutdown_python_api();
     return 0;
