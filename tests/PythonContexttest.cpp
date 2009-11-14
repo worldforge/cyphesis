ec1be5236517d96f8ca2fb34f8ebe8f3e461ca45
Alistair Riddoch <alriddoch@googlemail.com>
2009-11-14 18:10:24 +0000

    Add coverage test for PythonContext

---
+++ tests/PythonContexttest.cpp
@@ -0,0 +1,38 @@
+// Cyphesis Online RPG Server and AI Engine
+// Copyright (C) 2009 Alistair Riddoch
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+// $Id$
+
+#include <Python.h>
+
+#include "rulesets/Python_API.h"
+#include "rulesets/PythonContext.h"
+
+#include <cassert>
+
+int main()
+{
+    init_python_api();
+
+    PythonContext pc;
+
+    pc.runCommand("1");
+    pc.runCommand("foo(");
+
+    shutdown_python_api();
+    return 0;
+}
