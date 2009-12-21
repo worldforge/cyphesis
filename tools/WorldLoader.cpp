ab2f1d4a10ca7278172c442267d65015199dcf77
Alistair Riddoch <alriddoch@googlemail.com>
2009-12-21 19:37:28 +0000

    Stub class for loading data into the world

---
+++ tools/WorldLoader.cpp
@@ -0,0 +1,69 @@
+// Cyphesis Online RPG Server and AI Engine
+// Copyright (C) 2009 Alistair Riddoch
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+// $Id$
+
+#include "WorldLoader.h"
+
+#include "tools/MultiLineListFormatter.h"
+
+#include "common/AtlasFileLoader.h"
+
+#include <Atlas/Codecs/XML.h>
+#include <Atlas/Message/QueuedDecoder.h>
+#include <Atlas/Objects/Anonymous.h>
+#include <Atlas/Objects/Encoder.h>
+#include <Atlas/Objects/Operation.h>
+
+#include <iostream>
+
+using Atlas::Objects::Root;
+using Atlas::Objects::smart_dynamic_cast;
+using Atlas::Objects::Entity::Anonymous;
+using Atlas::Objects::Entity::RootEntity;
+using Atlas::Objects::Operation::Get;
+
+void WorldLoader::infoArrived(const Operation & op, OpVector & res)
+{
+}
+
+WorldLoader::WorldLoader(const std::string & accountId) : m_account(accountId),
+                                                          m_lastSerialNo(-1),
+                                                          m_count(0)
+{
+}
+
+WorldLoader::~WorldLoader()
+{
+}
+
+void WorldLoader::setup(const std::string & arg, OpVector & res)
+{
+    std::string filename("world.xml");
+    if (!arg.empty()) {
+        filename = arg;
+    }
+
+    // Send initiating op.
+}
+
+void WorldLoader::operation(const Operation & op, OpVector & res)
+{
+    if (op->getClassNo() == Atlas::Objects::Operation::INFO_NO) {
+        infoArrived(op, res);
+    }
+}
