1edcf786c60b5bb9a1c562b90d7bdbcb2e02c233
Alistair Riddoch <alriddoch@googlemail.com>
2009-12-30 01:32:31 +0000

    Add concept classes for handling spawn points.

---
+++ server/Spawn.h
@@ -0,0 +1,32 @@
+// Cyphesis Online RPG Server and AI Engine
+// Copyright (C) 2009 Alistair Riddoch
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+// $Id$
+
+#ifndef SERVER_SPAWN_H
+#define SERVER_SPAWN_H
+
+#include <Atlas/Objects/ObjectsFwd.h>
+
+class Spawn {
+  public:
+    virtual ~Spawn() = 0;
+
+    virtual int spawnEntity(const Atlas::Objects::Entity::RootEntity & dsc) = 0;
+};
+
+#endif // SERVER_SPAWN_H
