5ab956b7ed895623e62d02d670fe921bec41ebd8
Alistair Riddoch <alriddoch@googlemail.com>
2010-03-18 01:42:03 +0000

    Add a nominal Mastertest, as this file is not yet in the build.

---
+++ tests/Mastertest.cpp
@@ -0,0 +1,44 @@
+// Cyphesis Online RPG Server and AI Engine
+// Copyright (C) 2009 Alistair Riddoch
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+// $Id$
+
+#include "server/Master.h"
+
+#include <Atlas/Objects/RootOperation.h>
+#include <Atlas/Objects/SmartPtr.h>
+
+#include <cassert>
+
+int main()
+{
+    {
+        Master * p = new Master(*(CommClient*)0, *(ServerRouting*)0, "1");
+        delete p;
+    }
+
+    {
+        Master * p = new Master(*(CommClient*)0, *(ServerRouting*)0, "1");
+
+        Atlas::Objects::Operation::RootOperation op;
+        OpVector res;
+        p->operation(op, res);
+
+        delete p;
+    }
+    return 0;
+}
