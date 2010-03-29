1fbee409c587476b8c4218560d6b9ce00ccf65e4
Alistair Riddoch <alriddoch@googlemail.com>
2010-03-29 08:52:31 +0100

    Isolate tests for distance and emergence testing

--- tests/distancetest.cpp
+++ tests/distancetest.cpp
@@ -21,6 +21,8 @@
 
 #include "modules/Location.h"
 
+#include "common/log.h"
+
 // RelPos to self: (0,0,0)
 // RelPos ent -> tlve: (-1,-1,0)
 // RelPos tlve -> ent: (1,1,0)
@@ -226,4 +228,204 @@ int main()
         ent2.m_location.m_loc = 0;
     }
     return ret;
+}
+
+// stubs
+
+Entity::Entity(const std::string & id, long intId) :
+        LocatedEntity(id, intId), m_motion(0), m_flags(0)
+{
+}
+
+Entity::~Entity()
+{
+}
+
+void Entity::ActuateOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::AppearanceOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::AttackOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::CombineOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::CreateOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::DeleteOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::DisappearanceOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::DivideOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::EatOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::ImaginaryOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::LookOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::MoveOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::NourishOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::SetOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::SightOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::SoundOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::TalkOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::TickOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::TouchOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::UpdateOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::UseOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::WieldOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::externalOperation(const Operation & op)
+{
+}
+
+void Entity::operation(const Operation & op, OpVector & res)
+{
+}
+
+void Entity::addToMessage(Atlas::Message::MapType & omap) const
+{
+}
+
+void Entity::addToEntity(const Atlas::Objects::Entity::RootEntity & ent) const
+{
+}
+
+void Entity::setAttr(const std::string & name,
+                     const Atlas::Message::Element & attr)
+{
+}
+
+const PropertyBase * Entity::getProperty(const std::string & name) const
+{
+    return 0;
+}
+
+void Entity::onContainered()
+{
+}
+
+void Entity::onUpdated()
+{
+}
+
+LocatedEntity::LocatedEntity(const std::string & id, long intId) :
+               Router(id, intId),
+               m_refCount(0), m_seq(0),
+               m_script(0), m_type(0), m_contains(0)
+{
+}
+
+LocatedEntity::~LocatedEntity()
+{
+}
+
+bool LocatedEntity::hasAttr(const std::string & name) const
+{
+    return false;
+}
+
+bool LocatedEntity::getAttr(const std::string & name, Atlas::Message::Element & attr) const
+{
+    return false;
+}
+
+bool LocatedEntity::getAttrType(const std::string & name,
+                                Atlas::Message::Element & attr,
+                                int type) const
+{
+    return false;
+}
+
+void LocatedEntity::setAttr(const std::string & name, const Atlas::Message::Element & attr)
+{
+    return;
+}
+
+const PropertyBase * LocatedEntity::getProperty(const std::string & name) const
+{
+    return 0;
+}
+
+void LocatedEntity::onContainered()
+{
+}
+
+void LocatedEntity::onUpdated()
+{
+}
+
+Router::Router(const std::string & id, long intId) : m_id(id),
+                                                             m_intId(intId)
+{
+}
+
+Router::~Router()
+{
+}
+
+void Router::addToMessage(Atlas::Message::MapType & omap) const
+{
+}
+
+void Router::addToEntity(const Atlas::Objects::Entity::RootEntity & ent) const
+{
+}
+
+void log(LogLevel lvl, const std::string & msg)
+{
 }
--- tests/emergencetest.cpp
+++ tests/emergencetest.cpp
@@ -21,6 +21,8 @@
 
 #include "modules/Location.h"
 
+#include "common/log.h"
+
 #include <iostream>
 
 #include <cassert>
@@ -75,4 +77,10 @@ int main()
     }
 
     return ret;
+}
+
+// stubs
+
+void log(LogLevel lvl, const std::string & msg)
+{
 }
