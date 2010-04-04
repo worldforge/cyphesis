de90c4dc39a3e036fe747696cda8bdc99f84e054
Alistair Riddoch <alriddoch@googlemail.com>
2010-04-04 22:49:39 +0100

    Isolate Character and Creator unit tests.

--- tests/Creatortest.cpp
+++ tests/Creatortest.cpp
@@ -23,8 +23,16 @@
 
 #include "rulesets/Creator.h"
 #include "rulesets/Python_API.h"
+#include "rulesets/Script.h"
 
+#include "modules/EntityRef.h"
+
+#include "common/id.h"
+#include "common/log.h"
+#include "common/Property_impl.h"
 #include "common/TypeNode.h"
+
+#include <cstdlib>
 
 #include <cassert>
 
@@ -33,10 +41,6 @@ using Atlas::Message::ListType;
 
 int main(int argc, char ** argv)
 {
-    loadConfig(argc, argv);
-
-    init_python_api();
-
     Creator e("1", 1);
     TypeNode type;
     type.name() = "creator";
@@ -54,52 +58,666 @@ int main(int argc, char ** argv)
     // Throw an op of every type at the entity again now it is subscribed
     ee.runOperations();
 
-    std::set<std::string> attrNames;
-    attrNames.insert("id");
-
-    // Make sure we have all the default attributes
-    assert(ee.checkAttributes(attrNames));
-
-    attrNames.insert("test_int");
-    attrNames.insert("test_float");
-    attrNames.insert("test_list_string");
-    attrNames.insert("test_list_int");
-    attrNames.insert("test_list_float");
-    attrNames.insert("test_map_string");
-    attrNames.insert("test_map_int");
-    attrNames.insert("test_map_float");
-
-    // Make sure we don't have the test attributes yet
-    assert(!ee.checkAttributes(attrNames));
-
-    // Add the test attributes
-    e.setAttr("test_int", 1);
-    e.setAttr("test_float", 1.f);
-    e.setAttr("test_list_string", "test_value");
-    e.setAttr("test_list_int", ListType(1, 1));
-    e.setAttr("test_list_float", ListType(1, 1.f));
-    e.setAttr("test_map_string", ListType(1, "test_value"));
-    MapType test_map;
-    test_map["test_key"] = 1;
-    e.setAttr("test_map_int", test_map);
-    test_map["test_key"] = 1.f;
-    e.setAttr("test_map_float", test_map);
-    test_map["test_key"] = "test_value";
-    e.setAttr("test_map_string", test_map);
-    
-    // Make sure we have the test attributes now
-    assert(ee.checkAttributes(attrNames));
-
-    MapType entityAsAtlas;
-
-    // Dump a representation of the entity into an Atlas Message
-    e.addToMessage(entityAsAtlas);
-
-    // Make sure we got at least some of it
-    assert(entityAsAtlas.size() > 14);
-
-    // Read the contents of the Atlas Message back in
-    e.merge(entityAsAtlas);
-
     return 0;
+}
+
+// stubs
+
+Script noScript;
+
+namespace Atlas { namespace Objects { namespace Operation {
+int ACTUATE_NO = -1;
+int ATTACK_NO = -1;
+int EAT_NO = -1;
+int GOAL_INFO_NO = -1;
+int NOURISH_NO = -1;
+int SETUP_NO = -1;
+int TICK_NO = -1;
+int THOUGHT_NO = -1;
+int UNSEEN_NO = -1;
+int UPDATE_NO = -1;
+} } }
+
+Character::Character(const std::string & id, long intId) :
+           Character_parent(id, intId),
+               m_movement(*(Movement*)0),
+               m_task(0), m_mind(0), m_externalMind(0)
+{
+}
+
+Character::~Character()
+{
+}
+
+void Character::operation(const Operation & op, OpVector &)
+{
+}
+
+void Character::externalOperation(const Operation & op)
+{
+}
+
+
+void Character::ImaginaryOperation(const Operation & op, OpVector &)
+{
+}
+
+void Character::TickOperation(const Operation & op, OpVector &)
+{
+}
+
+void Character::TalkOperation(const Operation & op, OpVector &)
+{
+}
+
+void Character::NourishOperation(const Operation & op, OpVector &)
+{
+}
+
+void Character::UseOperation(const Operation & op, OpVector &)
+{
+}
+
+void Character::WieldOperation(const Operation & op, OpVector &)
+{
+}
+
+void Character::AttackOperation(const Operation & op, OpVector &)
+{
+}
+
+
+void Character::mindActuateOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindAttackOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindCombineOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindCreateOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindDeleteOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindDivideOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindEatOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindGoalInfoOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindImaginaryOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindLookOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindMoveOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindSetOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindSetupOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindTalkOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindThoughtOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindTickOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindTouchOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindUpdateOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindUseOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindWieldOperation(const Operation &, OpVector &)
+{
+}
+
+
+void Character::mindOtherOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::sendMind(const Operation & op, OpVector & res)
+{
+}
+
+Thing::Thing(const std::string & id, long intId) :
+       Thing_parent(id, intId)
+{
+}
+
+Thing::~Thing()
+{
+}
+
+void Thing::DeleteOperation(const Operation & op, OpVector & res)
+{
+}
+
+void Thing::MoveOperation(const Operation & op, OpVector & res)
+{
+}
+
+void Thing::SetOperation(const Operation & op, OpVector & res)
+{
+}
+
+void Thing::LookOperation(const Operation & op, OpVector & res)
+{
+}
+
+void Thing::CreateOperation(const Operation & op, OpVector & res)
+{
+}
+
+void Thing::UpdateOperation(const Operation & op, OpVector & res)
+{
+}
+
+Entity::Entity(const std::string & id, long intId) :
+        LocatedEntity(id, intId), m_motion(0), m_flags(0)
+{
+}
+
+Entity::~Entity()
+{
+}
+
+void Entity::destroy()
+{
+    destroyed.emit();
+}
+
+void Entity::ActuateOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::AppearanceOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::AttackOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::CombineOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::CreateOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::DeleteOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::DisappearanceOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::DivideOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::EatOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::ImaginaryOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::LookOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::MoveOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::NourishOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::SetOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::SightOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::SoundOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::TalkOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::TickOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::TouchOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::UpdateOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::UseOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::WieldOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::externalOperation(const Operation & op)
+{
+}
+
+void Entity::operation(const Operation & op, OpVector & res)
+{
+}
+
+void Entity::addToMessage(Atlas::Message::MapType & omap) const
+{
+}
+
+void Entity::addToEntity(const Atlas::Objects::Entity::RootEntity & ent) const
+{
+}
+
+void Entity::setAttr(const std::string & name,
+                     const Atlas::Message::Element & attr)
+{
+}
+
+const PropertyBase * Entity::getProperty(const std::string & name) const
+{
+    return 0;
+}
+
+PropertyBase * Entity::modProperty(const std::string & name)
+{
+    return 0;
+}
+
+void Entity::onContainered()
+{
+}
+
+void Entity::onUpdated()
+{
+}
+
+void Entity::callOperation(const Operation & op, OpVector & res)
+{
+}
+
+LocatedEntity::LocatedEntity(const std::string & id, long intId) :
+               Router(id, intId),
+               m_refCount(0), m_seq(0),
+               m_script(&noScript), m_type(0), m_contains(0)
+{
+}
+
+LocatedEntity::~LocatedEntity()
+{
+}
+
+bool LocatedEntity::hasAttr(const std::string & name) const
+{
+    return false;
+}
+
+bool LocatedEntity::getAttr(const std::string & name, Atlas::Message::Element & attr) const
+{
+    return false;
+}
+
+bool LocatedEntity::getAttrType(const std::string & name,
+                                Atlas::Message::Element & attr,
+                                int type) const
+{
+    return false;
+}
+
+void LocatedEntity::setAttr(const std::string & name, const Atlas::Message::Element & attr)
+{
+    return;
+}
+
+const PropertyBase * LocatedEntity::getProperty(const std::string & name) const
+{
+    return 0;
+}
+
+void LocatedEntity::onContainered()
+{
+}
+
+void LocatedEntity::onUpdated()
+{
+}
+
+void LocatedEntity::makeContainer()
+{
+    if (m_contains == 0) {
+        m_contains = new LocatedEntitySet;
+    }
+}
+
+void LocatedEntity::merge(const MapType & ent)
+{
+}
+
+Router::Router(const std::string & id, long intId) : m_id(id),
+                                                             m_intId(intId)
+{
+}
+
+Router::~Router()
+{
+}
+
+void Router::addToMessage(Atlas::Message::MapType & omap) const
+{
+}
+
+void Router::addToEntity(const Atlas::Objects::Entity::RootEntity & ent) const
+{
+}
+
+void Router::error(const Operation & op,
+                   const std::string & errstring,
+                   OpVector & res,
+                   const std::string & to) const
+{
+}
+
+BaseWorld * BaseWorld::m_instance = 0;
+
+BaseWorld::BaseWorld(Entity & gw) : m_gameWorld(gw)
+{
+    m_instance = this;
+}
+
+BaseWorld::~BaseWorld()
+{
+    m_instance = 0;
+}
+
+Entity * BaseWorld::getEntity(const std::string & id) const
+{
+    long intId = integerId(id);
+
+    EntityDict::const_iterator I = m_eobjects.find(intId);
+    if (I != m_eobjects.end()) {
+        assert(I->second != 0);
+        return I->second;
+    } else {
+        return 0;
+    }
+}
+
+Entity * BaseWorld::getEntity(long id) const
+{
+    EntityDict::const_iterator I = m_eobjects.find(id);
+    if (I != m_eobjects.end()) {
+        assert(I->second != 0);
+        return I->second;
+    } else {
+        return 0;
+    }
+}
+
+Script::Script()
+{
+}
+
+/// \brief Script destructor
+Script::~Script()
+{
+}
+
+bool Script::operation(const std::string & opname,
+                       const Atlas::Objects::Operation::RootOperation & op,
+                       OpVector & res)
+{
+   return false;
+}
+
+void Script::hook(const std::string & function, LocatedEntity * entity)
+{
+}
+
+void Location::addToMessage(MapType & omap) const
+{
+}
+
+Location::Location()
+{
+}
+
+Location::Location(LocatedEntity * rf, const Point3D & pos)
+{
+}
+
+void Location::addToEntity(const Atlas::Objects::Entity::RootEntity & ent) const
+{
+}
+
+TypeNode::TypeNode() : m_parent(0)
+{
+}
+
+TypeNode::~TypeNode()
+{
+}
+
+PropertyBase::PropertyBase(unsigned int flags) : m_flags(flags)
+{
+}
+
+PropertyBase::~PropertyBase()
+{
+}
+
+void PropertyBase::install(Entity *)
+{
+}
+
+void PropertyBase::apply(Entity *)
+{
+}
+
+void PropertyBase::add(const std::string & s,
+                       Atlas::Message::MapType & ent) const
+{
+    get(ent[s]);
+}
+
+void PropertyBase::add(const std::string & s,
+                       const Atlas::Objects::Entity::RootEntity & ent) const
+{
+}
+
+template<>
+void Property<int>::set(const Atlas::Message::Element & e)
+{
+    if (e.isInt()) {
+        this->m_data = e.asInt();
+    }
+}
+
+template<>
+void Property<double>::set(const Atlas::Message::Element & e)
+{
+    if (e.isNum()) {
+        this->m_data = e.asNum();
+    }
+}
+
+template<>
+void Property<std::string>::set(const Atlas::Message::Element & e)
+{
+    if (e.isString()) {
+        this->m_data = e.String();
+    }
+}
+
+template class Property<int>;
+template class Property<double>;
+template class Property<std::string>;
+
+SoftProperty::SoftProperty()
+{
+}
+
+SoftProperty::SoftProperty(const Atlas::Message::Element & data) :
+              PropertyBase(0), m_data(data)
+{
+}
+
+bool SoftProperty::get(Atlas::Message::Element & val) const
+{
+    val = m_data;
+    return true;
+}
+
+void SoftProperty::set(const Atlas::Message::Element & val)
+{
+}
+
+PropertyManager * PropertyManager::m_instance = 0;
+
+PropertyManager::PropertyManager()
+{
+    assert(m_instance == 0);
+    m_instance = this;
+}
+
+PropertyManager::~PropertyManager()
+{
+   m_instance = 0;
+}
+
+void log(LogLevel lvl, const std::string & msg)
+{
+}
+
+long integerId(const std::string & id)
+{
+    long intId = strtol(id.c_str(), 0, 10);
+    if (intId == 0 && id != "0") {
+        intId = -1L;
+    }
+
+    return intId;
+}
+
+template <typename FloatT>
+int fromStdVector(Point3D & p, const std::vector<FloatT> & vf)
+{
+    if (vf.size() != 3) {
+        return -1;
+    }
+    p[0] = vf[0];
+    p[1] = vf[1];
+    p[2] = vf[2];
+    p.setValid();
+    return 0;
+}
+
+template <typename FloatT>
+int fromStdVector(Vector3D & v, const std::vector<FloatT> & vf)
+{
+    if (vf.size() != 3) {
+        return -1;
+    }
+    v[0] = vf[0];
+    v[1] = vf[1];
+    v[2] = vf[2];
+    v.setValid();
+    return 0;
+}
+
+template int fromStdVector<double>(Point3D & p, const std::vector<double> & vf);
+template int fromStdVector<double>(Vector3D & v, const std::vector<double> & vf);
+
+EntityRef::EntityRef(Entity* e) : m_inner(e)
+{
+}
+
+EntityRef::EntityRef(const EntityRef& ref) : m_inner(ref.m_inner)
+{
+}
+
+EntityRef& EntityRef::operator=(const EntityRef& ref)
+{
+    m_inner = ref.m_inner;
+
+    return *this;
+}
+
+void EntityRef::onEntityDeleted()
+{
+}
+
+const Vector3D distanceTo(const Location & self, const Location & other)
+{
+    return Vector3D(1,0,0);
+}
+
+template<class V>
+const Quaternion quaternionFromTo(const V & from, const V & to)
+{
+    return Quaternion(1.f, 0.f, 0.f, 0.f);
+}
+
+template
+const Quaternion quaternionFromTo<Vector3D>(const Vector3D &, const Vector3D&);
+
+void addToEntity(const Point3D & p, std::vector<double> & vd)
+{
 }
