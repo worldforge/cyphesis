c0466373ea2d68da0762f3a953beec1ca109f90e
Alistair Riddoch <alriddoch@googlemail.com>
2010-07-29 17:20:06 +0100

    Add stubbing require to have none-NULL world entity

--- tests/StorageManagertest.cpp
+++ tests/StorageManagertest.cpp
@@ -46,10 +46,12 @@ int main()
 #include "server/EntityBuilder.h"
 
 #include "rulesets/Entity.h"
+#include "rulesets/Script.h"
 
 #include "modules/EntityRef.h"
 #include "modules/Location.h"
 
+#include "common/const.h"
 #include "common/Database.h"
 #include "common/globals.h"
 #include "common/log.h"
@@ -99,7 +101,8 @@ OpQueEntry::~OpQueEntry()
     from.decRef();
 }
 
-WorldRouter::WorldRouter() : BaseWorld(*(Entity*)0),
+WorldRouter::WorldRouter() : BaseWorld(*new Entity(consts::rootWorldId,
+                                                   consts::rootWorldIntId)),
                              m_entityCount(1)
           
 {
@@ -196,6 +199,136 @@ Entity * EntityBuilder::newEntity(const std::string & id, long intId,
     return 0;
 }
 
+Script noScript;
+
+Entity::Entity(const std::string & id, long intId) :
+        LocatedEntity(id, intId), m_motion(0), m_flags(0)
+{
+}
+
+Entity::~Entity()
+{
+}
+
+void Entity::destroy()
+{
+    destroyed.emit();
+}
+
+void Entity::ActuateOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::AppearanceOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::AttackOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::CombineOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::CreateOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::DeleteOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::DisappearanceOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::DivideOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::EatOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::ImaginaryOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::LookOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::MoveOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::NourishOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::SetOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::SightOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::SoundOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::TalkOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::TickOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::TouchOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::UpdateOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::UseOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::WieldOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::externalOperation(const Operation & op)
+{
+}
+
+void Entity::operation(const Operation & op, OpVector & res)
+{
+}
+
+void Entity::addToMessage(Atlas::Message::MapType & omap) const
+{
+}
+
+void Entity::addToEntity(const Atlas::Objects::Entity::RootEntity & ent) const
+{
+}
+
+void Entity::setAttr(const std::string & name,
+                     const Atlas::Message::Element & attr)
+{
+}
+
+const PropertyBase * Entity::getProperty(const std::string & name) const
+{
+    return 0;
+}
+
 PropertyBase * Entity::modProperty(const std::string & name)
 {
     return 0;
@@ -205,6 +338,95 @@ PropertyBase * Entity::setProperty(const std::string & name,
                                    PropertyBase * prop)
 {
     return 0;
+}
+
+void Entity::onContainered()
+{
+}
+
+void Entity::onUpdated()
+{
+}
+
+LocatedEntity::LocatedEntity(const std::string & id, long intId) :
+               Router(id, intId),
+               m_refCount(0), m_seq(0),
+               m_script(&noScript), m_type(0), m_contains(0)
+{
+}
+
+LocatedEntity::~LocatedEntity()
+{
+}
+
+bool LocatedEntity::hasAttr(const std::string & name) const
+{
+    return false;
+}
+
+bool LocatedEntity::getAttr(const std::string & name, Atlas::Message::Element & attr) const
+{
+    return false;
+}
+
+bool LocatedEntity::getAttrType(const std::string & name,
+                                Atlas::Message::Element & attr,
+                                int type) const
+{
+    return false;
+}
+
+void LocatedEntity::setAttr(const std::string & name, const Atlas::Message::Element & attr)
+{
+    return;
+}
+
+const PropertyBase * LocatedEntity::getProperty(const std::string & name) const
+{
+    return 0;
+}
+
+void LocatedEntity::onContainered()
+{
+}
+
+void LocatedEntity::onUpdated()
+{
+}
+
+void LocatedEntity::makeContainer()
+{
+    if (m_contains == 0) {
+        m_contains = new LocatedEntitySet;
+    }
+}
+
+void LocatedEntity::merge(const MapType & ent)
+{
+}
+
+Router::Router(const std::string & id, long intId) : m_id(id),
+                                                             m_intId(intId)
+{
+}
+
+Router::~Router()
+{
+}
+
+void Router::addToMessage(Atlas::Message::MapType & omap) const
+{
+}
+
+void Router::addToEntity(const Atlas::Objects::Entity::RootEntity & ent) const
+{
+}
+
+void Router::error(const Operation & op,
+                   const std::string & errstring,
+                   OpVector & res,
+                   const std::string & to) const
+{
 }
 
 PropertyManager * PropertyManager::m_instance = 0;
@@ -218,6 +440,42 @@ PropertyManager::PropertyManager()
 PropertyManager::~PropertyManager()
 {
    m_instance = 0;
+}
+
+void Location::addToMessage(MapType & omap) const
+{
+}
+
+Location::Location()
+{
+}
+
+Location::Location(LocatedEntity * rf, const Point3D & pos)
+{
+}
+
+void Location::addToEntity(const Atlas::Objects::Entity::RootEntity & ent) const
+{
+}
+
+Script::Script()
+{
+}
+
+/// \brief Script destructor
+Script::~Script()
+{
+}
+
+bool Script::operation(const std::string & opname,
+                       const Atlas::Objects::Operation::RootOperation & op,
+                       OpVector & res)
+{
+   return false;
+}
+
+void Script::hook(const std::string & function, LocatedEntity * entity)
+{
 }
 
 EntityRef::EntityRef(Entity* e) : m_inner(e)
@@ -398,3 +656,12 @@ void log(LogLevel lvl, const std::string & msg)
 }
 
 bool database_flag = true;
+
+namespace consts {
+
+  // Id of root world entity
+  const char * rootWorldId = "0";
+  // Integer id of root world entity
+  const long rootWorldIntId = 0L;
+
+}
