c288c5ae0c520d3a48ac177b388fb30eb5ace773
Alistair Riddoch <alriddoch@googlemail.com>
2010-07-29 13:53:14 +0100

    Add full coverage test for Idle

---
+++ tests/Idletest.cpp
@@ -0,0 +1,54 @@
+// Cyphesis Online RPG Server and AI Engine
+// Copyright (C) 2010 Alistair Riddoch
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+// $Id$
+
+#include "server/Idle.h"
+
+#include "server/CommServer.h"
+
+#include <cassert>
+
+class TestIdler : public Idle
+{
+  public:
+    TestIdler(CommServer & svr) : Idle(svr) { }
+    ~TestIdler() { }
+
+    virtual void idle(time_t) { }
+};
+
+int main()
+{
+    {
+        CommServer cs(*(ServerRouting*)0);
+
+        TestIdler ti(cs);
+    }
+
+    return 0;
+}
+
+// stubs
+
+CommServer::CommServer(ServerRouting&sr) : m_server(sr)
+{
+}
+
+CommServer::~CommServer()
+{
+}
