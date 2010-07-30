32941d10f2ee8b9db3d4c574ee5b1cfaa35272ce
Alistair Riddoch <alriddoch@googlemail.com>
2010-07-30 02:04:44 +0100

    Add start of a metaserver query routine in python

--- rulesets/basic/metaserver.py
+++ rulesets/basic/metaserver.py
@@ -1,11 +1,40 @@
 #!/usr/bin/python
 
 import socket
+import struct
 
-def metaquery()
+SKEEP_ALIVE = socket.htonl(1)
+CKEEP_ALIVE = socket.htonl(2)
+HANDSHAKE = socket.htonl(3)
+SERVERSHAKE = socket.htonl(4)
+CLIENTSHAKE = socket.htonl(5)
+TERMINATE = socket.htonl(6)
+LIST_REQ = socket.htonl(7)
+LIST_RESP = socket.htonl(8)
+PROTO_ERANGE = socket.htonl(9)
+
+def metaquery():
     ip = "80.68.90.68"
     port = 8453
 
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    s.send...
+
+    s.sendto(struct.pack('I', CKEEP_ALIVE), (ip, port))
+
+    data,addr = s.recvfrom(8)
+
+    res = struct.unpack('II', data)
+    if res[0] != HANDSHAKE:
+        print socket.ntohl(res[0]), HANDSHAKE
+        print "no handshake"
+        return
+
+    shake = res[1]
+
+    s.sendto(struct.pack('II', CLIENTSHAKE, shake), (ip, port))
+
+    s.sendto(struct.pack('II', LIST_REQ, socket.htonl(0)), (ip, port))
+
+
+metaquery()
