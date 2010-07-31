8152bd55620bdd8afae70c44308245d39c9a3817
Alistair Riddoch <alriddoch@googlemail.com>
2010-07-31 12:29:26 +0100

    metaserver search kinda works now.

--- rulesets/basic/metaserver.py
+++ rulesets/basic/metaserver.py
@@ -30,11 +30,35 @@ def metaquery():
         print "no handshake"
         return
 
+    print "handshook"
+
     shake = res[1]
 
     s.sendto(struct.pack('II', CLIENTSHAKE, shake), (ip, port))
 
-    s.sendto(struct.pack('II', LIST_REQ, socket.htonl(0)), (ip, port))
+    print "listing"
+    segment=0
+    while True:
+        s.sendto(struct.pack('II', LIST_REQ, socket.htonl(segment)), (ip, port))
+
+        data,addr = s.recvfrom(4096)
+        print len(data)
+        res = struct.unpack('I', data[:4])
+        print socket.ntohl(res[0])
+        if res[0] == LIST_RESP:
+            payload = struct.unpack('II', data[4:12])
+            count = socket.ntohl(payload[1])
+            if len(data) != (12 + count * 4):
+                print "Error: ", len(data), count * 4
+                return
+            for i in range(0, count):
+                entry = struct.unpack('BBBB', data[12+i*4:16+i*4])
+                print "%d.%d.%d.%d" % entry
+            segment += count
+            print segment, " got."
+            if segment == socket.ntohl(payload[0]):
+                print "Done."
+                return
 
 
 metaquery()
