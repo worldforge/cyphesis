e6e0929b15db7f7ff72d53f8e8b01053696ffe60
Manohar Vanga <manohar.vanga@gmail.com>
2010-08-20 22:14:31 +0530

    Fixed bug in Peer::peerTeleportResponse

--- server/Peer.cpp
+++ server/Peer.cpp
@@ -258,7 +258,7 @@ void Peer::peerTeleportResponse(const Operation &op, OpVector &res)
     }
 
     TeleportMap::iterator I = m_teleports.find(id);
-    if (I != m_teleports.end()) {
+    if (I == m_teleports.end()) {
         log(ERROR, "Info op for unknown create");
         return;
     }
