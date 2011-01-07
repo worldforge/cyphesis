005f10f7ff6eaf9c0eefc2e79771ba40d54d6952
Alistair Riddoch <alriddoch@googlemail.com>
2011-01-07 22:39:30 +0000

    Add VisibilityProperty to override Entity visibilty

---
+++ rulesets/VisibilityProperty.h
@@ -0,0 +1,34 @@
+// Cyphesis Online RPG Server and AI Engine
+// Copyright (C) 2010 Alistair Riddoch
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+// $Id$
+
+#ifndef RULESETS_VISIBILITY_PROPERTY_H
+#define RULESETS_VISIBILITY_PROPERTY_H
+
+#include "common/Property.h"
+
+/// \brief Class to handle assigning a mind object to an entity
+/// \ingroup PropertyClasses
+class VisibilityProperty : public Property<float> {
+  public:
+    VisibilityProperty();
+
+    virtual void apply(Entity *);
+};
+
+#endif // RULESETS_VISIBILITY_PROPERTY_H
