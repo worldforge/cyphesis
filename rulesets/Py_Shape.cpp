6a8330d8e3ed0ca72aa6e75a30570c8811704167
Alistair Riddoch <alriddoch@googlemail.com>
2011-02-20 16:06:33 +0000

    Add stub inplace multiply for shapes to experiement

--- rulesets/Py_Shape.cpp
+++ rulesets/Py_Shape.cpp
@@ -93,42 +93,49 @@ Py_ssize_t Shape_sq_length(PyShape * self)
     return self->shape->size();
 }
 
+static PyObject * Shape_inplace_multiply(PyShape * self, PyObject * other)
+{
+    std::cout << "inplace mult" << std::endl << std::flush;
+    Py_INCREF(self);
+    return (PyObject*)self;
+}
+
 static PyNumberMethods Shape_number = {
-    0,                   // nb_add;
-    0,                   // nb_subtract;
-    0,                   // nb_multiply;
-    0,                   // nb_divide;
-    0,                   // nb_remainder;
-    0,                   // nb_divmod;
-    0,                   // nb_power;
-    0,                   // nb_negative;
-    0,                   // nb_positive;
-    0,                   // nb_absolute;
-    0,                   // nb_nonzero;
-    0,                   // nb_invert;
-    0,                   // nb_lshift;
-    0,                   // nb_rshift;
-    0,                   // nb_and;
-    0,                   // nb_xor;
-    0,                   // nb_or;
-    0,                   // nb_coerce;
-    0,                   // nb_int;
-    0,                   // nb_long;
-    0,                   // nb_float;
-    0,                   // nb_oct;
-    0,                   // nb_hex;
+    0,                                        // nb_add;
+    0,                                        // nb_subtract;
+    0,                                        // nb_multiply;
+    0,                                        // nb_divide;
+    0,                                        // nb_remainder;
+    0,                                        // nb_divmod;
+    0,                                        // nb_power;
+    0,                                        // nb_negative;
+    0,                                        // nb_positive;
+    0,                                        // nb_absolute;
+    0,                                        // nb_nonzero;
+    0,                                        // nb_invert;
+    0,                                        // nb_lshift;
+    0,                                        // nb_rshift;
+    0,                                        // nb_and;
+    0,                                        // nb_xor;
+    0,                                        // nb_or;
+    0,                                        // nb_coerce;
+    0,                                        // nb_int;
+    0,                                        // nb_long;
+    0,                                        // nb_float;
+    0,                                        // nb_oct;
+    0,                                        // nb_hex;
     /* Added in release 2.0 */
-    0,                   // nb_inplace_add;
-    0,                   // nb_inplace_subtract;
-    0,                   // nb_inplace_multiply;
-    0,                   // nb_inplace_divide;
-    0,                   // nb_inplace_remainder;
-    0,                   // nb_inplace_power;
-    0,                   // nb_inplace_lshift;
-    0,                   // nb_inplace_rshift;
-    0,                   // nb_inplace_and;
-    0,                   // nb_inplace_xor;
-    0,                   // nb_inplace_or;
+    0,                                        // nb_inplace_add;
+    0,                                        // nb_inplace_subtract;
+    (binaryfunc)Shape_inplace_multiply,       // nb_inplace_multiply;
+    0,                                        // nb_inplace_divide;
+    0,                                        // nb_inplace_remainder;
+    0,                                        // nb_inplace_power;
+    0,                                        // nb_inplace_lshift;
+    0,                                        // nb_inplace_rshift;
+    0,                                        // nb_inplace_and;
+    0,                                        // nb_inplace_xor;
+    0,                                        // nb_inplace_or;
 
 };
 
