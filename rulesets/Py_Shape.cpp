75f88d44a182d7a60919cc7d938a52ecfb0453d3
Alistair Riddoch <alriddoch@googlemail.com>
2011-02-20 15:25:28 +0000

    Add protocol for numeric operations to Shape Python wrapper

--- rulesets/Py_Shape.cpp
+++ rulesets/Py_Shape.cpp
@@ -93,6 +93,45 @@ Py_ssize_t Shape_sq_length(PyShape * self)
     return self->shape->size();
 }
 
+static PyNumberMethods Shape_number = {
+    0,                   // nb_add;
+    0,                   // nb_subtract;
+    0,                   // nb_multiply;
+    0,                   // nb_divide;
+    0,                   // nb_remainder;
+    0,                   // nb_divmod;
+    0,                   // nb_power;
+    0,                   // nb_negative;
+    0,                   // nb_positive;
+    0,                   // nb_absolute;
+    0,                   // nb_nonzero;
+    0,                   // nb_invert;
+    0,                   // nb_lshift;
+    0,                   // nb_rshift;
+    0,                   // nb_and;
+    0,                   // nb_xor;
+    0,                   // nb_or;
+    0,                   // nb_coerce;
+    0,                   // nb_int;
+    0,                   // nb_long;
+    0,                   // nb_float;
+    0,                   // nb_oct;
+    0,                   // nb_hex;
+    /* Added in release 2.0 */
+    0,                   // nb_inplace_add;
+    0,                   // nb_inplace_subtract;
+    0,                   // nb_inplace_multiply;
+    0,                   // nb_inplace_divide;
+    0,                   // nb_inplace_remainder;
+    0,                   // nb_inplace_power;
+    0,                   // nb_inplace_lshift;
+    0,                   // nb_inplace_rshift;
+    0,                   // nb_inplace_and;
+    0,                   // nb_inplace_xor;
+    0,                   // nb_inplace_or;
+
+};
+
 static PySequenceMethods Shape_sequence = {
         (lenfunc)Shape_sq_length, // sq_length;
         0, // sq_concat;
@@ -120,7 +159,7 @@ PyTypeObject PyShape_Type = {
         (setattrfunc)Shape_setattr,     /*tp_setattr*/
         0,                              /*tp_compare*/
         (reprfunc)Shape_repr,           /*tp_repr*/
-        0,                              /*tp_as_number*/
+        &Shape_number,                  /*tp_as_number*/
         &Shape_sequence,                /*tp_as_sequence*/
         0,                              /*tp_as_mapping*/
         0,                              /*tp_hash*/
