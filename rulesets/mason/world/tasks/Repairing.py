8cea1a0aeac36e6734036adbae036af3dda5de7a
Jekin Trivedi <jekintrivedi@gmail.com>
2011-08-21 20:48:23 +0530

    Added basic task Repairing & Destroying

---
+++ rulesets/mason/world/tasks/Repairing.py
@@ -0,0 +1,64 @@
+#This file is distributed under the terms of the GNU General Public license.
+#Copyright (C) 2011 Jekin Trivedi <jekintrivedi@gmail.com> (See the file COPYING for details).
+
+from atlas import *
+from physics import *
+from physics import Quaternion
+from physics import Vector3D
+
+import server
+
+class Repairing(server.Task):
+    """A very simple Repair system for Repairing structures."""
+    def repair_operation(self, op):
+        """ The repair op is FROM the the character,
+            TO the structure that is getting Repaired which we
+            term the target. """
+
+        if len(op) < 1:
+            sys.stderr.write("Repair task has no target in repair op")
+
+        # FIXME Use weak references, once we have them
+        self.target = op[0].id
+        self.tool = op.to
+
+    def tick_operation(self, op):
+        """ This method is called repeatedly, each time a Repair turn occurs.
+            In this example the interval is fixed, but it can be varied. """
+        # print "Repair.tick" 
+
+        target=server.world.get_object(self.target)
+        if not target:
+            # print "Target is no more"
+            self.irrelevant()
+            return
+
+        if self.character.stamina <= 0:
+            # print "I am exhausted"
+            self.irrelevant()
+            return
+
+        current_status = target.status
+
+        if square_distance(self.character.location, target.location) > target.location.bbox.square_bounding_radius():
+            self.progress = current_status
+            self.rate = 0
+            return self.next_tick(1.75)
+
+        res=Oplist()
+
+        if current_status < 0.9:
+            set=Operation("set", Entity(self.target, status=current_status+0.1), to=self.target)
+            res.append(set)
+        else:
+            set = Operation("set", Entity(self.target, status = 1),
+                            to = self.target)
+            res.append(set)
+
+        self.progress = current_status
+        self.rate = 0.1 / 1.75
+        
+        res.append(self.next_tick(1.75))
+
+        return res
+
