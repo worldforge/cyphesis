27cc00e38274e1a1b9001ab23fc44730bd898f93
Alistair Riddoch <alriddoch@googlemail.com>
2011-09-16 10:43:01 +0100

    Relocate code for adding properties to TypeNode

--- common/TypeNode.cpp
+++ common/TypeNode.cpp
@@ -42,6 +42,21 @@ TypeNode::~TypeNode()
     }
 }
 
+void TypeNode::addProperties(const MapType & attributes)
+{
+    MapType::const_iterator J = attributes.begin();
+    MapType::const_iterator Jend = attributes.end();
+    PropertyBase * p;
+    for (; J != Jend; ++J) {
+        p = PropertyManager::instance()->addProperty(J->first,
+                                                     J->second.getType());
+        assert(p != 0);
+        p->set(J->second);
+        p->setFlags(flag_class);
+        m_defaults[J->first] = p;
+    }
+}
+
 void TypeNode::updateProperties(const MapType & attributes)
 {
     // Discover the default attributes which are no longer
