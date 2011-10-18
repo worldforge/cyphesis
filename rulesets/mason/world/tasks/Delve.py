90f06e495887c90f3b2e6c7271a6d247bf645cdf
Alistair Riddoch <alriddoch@googlemail.com>
2011-10-19 00:44:37 +0100

    Get the correct kind of weak reference

--- rulesets/mason/world/tasks/Delve.py
+++ rulesets/mason/world/tasks/Delve.py
@@ -21,7 +21,7 @@ class Delve(server.Task):
             sys.stderr.write("Delve task has no target in cut op")
 
         # FIXME Use weak references, once we have them
-        self.target = server.world.get_object(op[0].id)
+        self.target = server.world.get_object_ref(op[0].id)
         self.tool = op.to
 
         self.pos = Point3D(op[0].pos)
