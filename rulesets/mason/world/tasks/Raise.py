0a89d75f2e5284c783ac2204f136b0ac796113c6
Alistair Riddoch <alriddoch@googlemail.com>
2011-10-20 11:14:16 +0100

    Migrate to weak references

--- rulesets/mason/world/tasks/Raise.py
+++ rulesets/mason/world/tasks/Raise.py
@@ -22,31 +22,31 @@ class Raise(server.Task):
             std.stderr.write("Raise task hsa no target in lever op")
 
         # FIXME Use weak references, once we have them
-        self.target = op[0].id
+        self.target = server.world.get_object_ref(op[0].id)
         self.tool = op.to
 
     def tick_operation(self, op):
         """ Op handler for regular tick op """
         # print "Raise.tick"
 
-        target=server.world.get_object(self.target)
-        if not target:
+        if self.target() is None:
             # print "Target is no more"
             self.irrelevant()
             return
-        if not target.location.parent:
+        if not self.target().location.parent:
             # Not withstanding famous quotes to the contrary, in these
             # system we can not move the world no matter how large a lever
             # or firm a place to stand we have.
             self.irrelevant()
             return
 
-        distance=distance_to(self.character.location, target.location)
+        distance=distance_to(self.character.location, self.target().location)
         # Check we are not too far away from the object to interact with it
         # This calculation is imprecise as it sums the square radii, but
         # its usually close enough.
         d = distance.square_mag()
-        r = self.character.location.bbox.square_bounding_radius() + target.location.bbox.square_bounding_radius()
+        r = self.character.location.bbox.square_bounding_radius() + \
+            self.target().location.bbox.square_bounding_radius()
         if d > r:
             return self.next_tick(1);
         if d < const.epsilon:
@@ -59,14 +59,16 @@ class Raise(server.Task):
         # If axis is zero, the quaternion contains NaNs.
         rotation=Quaternion(axis, -0.05)
         # print "ROT ", rotation, rotation.is_valid()
-        if target.location.orientation.is_valid():
+        if self.target().location.orientation.is_valid():
             # print "VALID"
-            rotation = target.location.orientation * rotation
+            rotation = self.target().location.orientation * rotation
 
         # print "NEW_ROT", rotation, rotation.is_valid()
-        target_location=Location(target.location.parent, target.location.coordinates)
+        target_location=Location(self.target().location.parent,
+                                 self.target().location.coordinates)
         target_location.orientation=rotation
-        move=Operation("move", Entity(self.target, location=target_location), to=self.target)
+        move=Operation("move", Entity(self.target().id,
+                                      location=target_location), to=self.target())
         res=Oplist()
         res.append(move)
 
