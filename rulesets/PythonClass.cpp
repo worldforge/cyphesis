4a0b3a508df11903882bfdefba8d36b1da9b2a68
Alistair Riddoch <alriddoch@googlemail.com>
2011-11-08 18:54:10 +0000

    Add a new class to encapsulate a Python class on disk

---
+++ rulesets/PythonClass.cpp
@@ -0,0 +1,92 @@
+// Cyphesis Online RPG Server and AI Engine
+// Copyright (C) 2011 Alistair Riddoch
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+// $Id$
+
+#include <Python.h>
+
+#include "PythonClass.h"
+
+#include "rulesets/Python_Script_Utils.h"
+
+#include "common/log.h"
+#include "common/compose.hpp"
+
+/// \brief ScriptKit constructor
+/// 
+/// @param package name of the script package scripts are to be created from
+/// @param type name of the type instanced to create scripts
+PythonClass::PythonClass(const std::string & package,
+                         const std::string & type) : m_package(package),
+                                                     m_type(type),
+                                                     m_module(0),
+                                                     m_class(0)
+{
+    // FIXME #4 This sort of code should not be in the constructor
+    m_module = Get_PyModule(m_package);
+    if (m_module == NULL) {
+        return;
+    }
+    getClass();
+}
+
+PythonClass::~PythonClass()
+{
+    if (m_class != 0) {
+        Py_DECREF(m_class);
+    }
+    if (m_module != 0) {
+        Py_DECREF(m_module);
+    }
+}
+
+/// \brief Retrieve the pythonclass object from the module which has
+/// already been loaded.
+int PythonClass::getClass()
+{
+    m_class = Get_PyClass(m_module, m_package, m_type);
+    if (m_class == 0) {
+        return -1;
+    }
+    if (check() != 0) {
+        log(ERROR, String::compose("Python class does not inherit from "
+                                   "a core server type. \"%1.%2\"",
+                                   m_package, m_type));
+        Py_DECREF(m_class);
+        m_class = 0;
+        return -1;
+    }
+
+    return 0;
+}
+
+int PythonClass::refresh()
+{
+    if (m_module == 0) {
+        return -1;
+    }
+    PyObject * new_module = PyImport_ReloadModule(m_module);
+    if (new_module == 0) {
+        log(ERROR, String::compose("Error reloading python module \"%1\"",
+                                   m_package));
+        PyErr_Clear();
+        return -1;
+    }
+    Py_DECREF(m_module);
+    m_module = new_module;
+    return getClass();
+}
---
+++ rulesets/PythonClass.h
@@ -0,0 +1,54 @@
+// Cyphesis Online RPG Server and AI Engine
+// Copyright (C) 2011 Alistair Riddoch
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+// $Id$
+
+#ifndef RULESETS_PYTHON_CLASS_H
+#define RULESETS_PYTHON_CLASS_H
+
+#include <string>
+
+/// \brief Factory interface for creating scripts to attach to in game
+/// entity objects.
+class PythonClass {
+  protected:
+    /// \brief Name of the script package where the script type is
+    std::string m_package;
+    /// \brief Name of the script type instanced by this factory
+    std::string m_type;
+
+    /// \brief Module object which contains the script class
+    struct _object * m_module;
+    /// \brief Class object to be instanced when creating scripts
+    struct _object * m_class;
+
+    PythonClass(const std::string & package, const std::string & type);
+
+    int getClass();
+    int refresh();
+
+    virtual int check() = 0;
+
+  public:
+    virtual ~PythonClass();
+
+    bool isOkay() const {
+        return m_class != 0;
+    }
+};
+
+#endif // RULESETS_PYTHON_CLASS_H
