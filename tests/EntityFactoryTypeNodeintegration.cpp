49a17c7af61a9d6742e3ea6367492fe9dc9e303b
Alistair Riddoch <alriddoch@googlemail.com>
2011-12-13 00:32:52 +0000

    Add a new integration test
    This one ensures attributes and properties are handled correctly
    between EntityFactory and TypeNode

---
+++ tests/EntityFactoryTypeNodeintegration.cpp
@@ -0,0 +1,556 @@
+// Cyphesis Online RPG Server and AI Engine
+// Copyright (C) 2011 Alistair Riddoch
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+// $Id$
+
+#ifdef NDEBUG
+#undef NDEBUG
+#endif
+#ifndef DEBUG
+#define DEBUG
+#endif
+
+#include "server/EntityFactory.h"
+
+#include "TestPropertyManager.h"
+
+#include "rulesets/Thing.h"
+
+#include "common/TypeNode.h"
+
+#include <cassert>
+
+int main()
+{
+    TestPropertyManager tpm;
+
+    EntityKit * ek;
+    ek = new EntityFactory<Thing>;
+
+    ek->m_type = new TypeNode("thing");
+
+    assert(ek->m_type->defaults().empty());
+
+    ek->m_attributes["test1"] = Atlas::Message::StringType("foo");
+    ek->m_classAttributes["test1"] = Atlas::Message::StringType("foo");
+
+    assert(ek->m_type->defaults().empty());
+
+    ek->updateChildren();
+    ek->updateChildrenProperties();
+
+    assert(ek->m_type->defaults().size() == 1);
+
+    EntityKit * subclass_ek = new EntityFactory<Thing>;
+
+    subclass_ek->m_type = new TypeNode("subclass");
+    subclass_ek->m_type->setParent(ek->m_type);
+    ek->m_children.insert(subclass_ek);
+
+    assert(subclass_ek->m_type->defaults().empty());
+
+    subclass_ek->m_attributes["test2"] = Atlas::Message::StringType("bar");
+    subclass_ek->m_classAttributes["test2"] = Atlas::Message::StringType("bar");
+
+    assert(subclass_ek->m_type->defaults().empty());
+
+    subclass_ek->updateChildren();
+    subclass_ek->updateChildrenProperties();
+
+    assert(subclass_ek->m_type->defaults().size() == 1);
+
+    ek->updateChildren();
+    ek->updateChildrenProperties();
+
+    assert(ek->m_type->defaults().size() == 1);
+    assert(subclass_ek->m_type->defaults().size() == 2);
+
+    subclass_ek->m_attributes["test1"] = Atlas::Message::StringType("bar");
+    subclass_ek->m_classAttributes["test1"] = Atlas::Message::StringType("bar");
+
+    ek->updateChildren();
+    ek->updateChildrenProperties();
+
+    assert(ek->m_type->defaults().size() == 1);
+    assert(subclass_ek->m_type->defaults().size() == 2);
+
+    return 0;
+}
+
+// stubs
+
+#include "server/ScriptFactory.h"
+
+#include "rulesets/Creator.h"
+#include "rulesets/Plant.h"
+#include "rulesets/Stackable.h"
+
+ScriptKit::~ScriptKit()
+{
+}
+
+Stackable::Stackable(const std::string & id, long intId) :
+           Stackable_parent(id, intId), m_num(1)
+{
+    // m_properties["num"] = new Property<int>(m_num, 0);
+}
+
+Stackable::~Stackable()
+{
+}
+
+void Stackable::CombineOperation(const Operation & op, OpVector & res)
+{
+}
+
+void Stackable::DivideOperation(const Operation & op, OpVector & res)
+{
+}
+
+Plant::Plant(const std::string & id, long intId) :
+       Plant_parent(id, intId), m_nourishment(0)
+{
+}
+
+Plant::~Plant()
+{
+}
+
+void Plant::NourishOperation(const Operation & op, OpVector & res)
+{
+}
+
+void Plant::TickOperation(const Operation & op, OpVector & res)
+{
+}
+
+void Plant::TouchOperation(const Operation & op, OpVector & res)
+{
+}
+
+Creator::Creator(const std::string & id, long intId) :
+         Creator_parent(id, intId)
+{
+}
+
+void Creator::operation(const Operation & op, OpVector & res)
+{
+}
+
+void Creator::externalOperation(const Operation & op)
+{
+}
+
+void Creator::mindLookOperation(const Operation & op, OpVector & res)
+{
+}
+
+Character::Character(const std::string & id, long intId) :
+           Character_parent(id, intId),
+               m_movement(*(Movement*)0),
+               m_mind(0), m_externalMind(0)
+{
+}
+
+Character::~Character()
+{
+}
+
+void Character::operation(const Operation & op, OpVector &)
+{
+}
+
+void Character::externalOperation(const Operation & op)
+{
+}
+
+
+void Character::ImaginaryOperation(const Operation & op, OpVector &)
+{
+}
+
+void Character::TickOperation(const Operation & op, OpVector &)
+{
+}
+
+void Character::TalkOperation(const Operation & op, OpVector &)
+{
+}
+
+void Character::NourishOperation(const Operation & op, OpVector &)
+{
+}
+
+void Character::UseOperation(const Operation & op, OpVector &)
+{
+}
+
+void Character::WieldOperation(const Operation & op, OpVector &)
+{
+}
+
+void Character::AttackOperation(const Operation & op, OpVector &)
+{
+}
+
+void Character::ActuateOperation(const Operation & op, OpVector &)
+{
+}
+
+void Character::mindActuateOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindAttackOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindCombineOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindCreateOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindDeleteOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindDivideOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindEatOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindGoalInfoOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindImaginaryOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindLookOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindMoveOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindSetOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindSetupOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindTalkOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindThoughtOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindTickOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindTouchOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindUpdateOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindUseOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindWieldOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindOtherOperation(const Operation &, OpVector &)
+{
+}
+
+
+Thing::Thing(const std::string & id, long intId) :
+       Thing_parent(id, intId)
+{
+}
+
+Thing::~Thing()
+{
+}
+
+void Thing::DeleteOperation(const Operation & op, OpVector & res)
+{
+}
+
+void Thing::MoveOperation(const Operation & op, OpVector & res)
+{
+}
+
+void Thing::SetOperation(const Operation & op, OpVector & res)
+{
+}
+
+void Thing::LookOperation(const Operation & op, OpVector & res)
+{
+}
+
+void Thing::CreateOperation(const Operation & op, OpVector & res)
+{
+}
+
+void Thing::UpdateOperation(const Operation & op, OpVector & res)
+{
+}
+
+Entity::Entity(const std::string & id, long intId) :
+        LocatedEntity(id, intId), m_motion(0), m_flags(0)
+{
+}
+
+Entity::~Entity()
+{
+}
+
+void Entity::destroy()
+{
+    destroyed.emit();
+}
+
+void Entity::ActuateOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::AppearanceOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::AttackOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::CombineOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::CreateOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::DeleteOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::DisappearanceOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::DivideOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::EatOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::ImaginaryOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::LookOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::MoveOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::NourishOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::SetOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::SightOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::SoundOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::TalkOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::TickOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::TouchOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::UpdateOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::UseOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::WieldOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::externalOperation(const Operation & op)
+{
+}
+
+void Entity::operation(const Operation & op, OpVector & res)
+{
+}
+
+void Entity::addToMessage(Atlas::Message::MapType & omap) const
+{
+}
+
+void Entity::addToEntity(const Atlas::Objects::Entity::RootEntity & ent) const
+{
+}
+
+PropertyBase * Entity::setAttr(const std::string & name,
+                               const Atlas::Message::Element & attr)
+{
+    return 0;
+}
+
+const PropertyBase * Entity::getProperty(const std::string & name) const
+{
+    return 0;
+}
+
+PropertyBase * Entity::setProperty(const std::string & name,
+                                   PropertyBase * prop)
+{
+    return m_properties[name] = prop;
+}
+
+void Entity::onContainered()
+{
+}
+
+void Entity::onUpdated()
+{
+}
+
+LocatedEntity::LocatedEntity(const std::string & id, long intId) :
+               Router(id, intId),
+               m_refCount(0), m_seq(0),
+               m_script(0), m_type(0), m_contains(0)
+{
+}
+
+LocatedEntity::~LocatedEntity()
+{
+}
+
+bool LocatedEntity::hasAttr(const std::string & name) const
+{
+    return false;
+}
+
+int LocatedEntity::getAttr(const std::string & name,
+                           Atlas::Message::Element & attr) const
+{
+    return -1;
+}
+
+int LocatedEntity::getAttrType(const std::string & name,
+                               Atlas::Message::Element & attr,
+                               int type) const
+{
+    return -1;
+}
+
+PropertyBase * LocatedEntity::setAttr(const std::string & name,
+                                      const Atlas::Message::Element & attr)
+{
+    return 0;
+}
+
+const PropertyBase * LocatedEntity::getProperty(const std::string & name) const
+{
+    return 0;
+}
+
+void LocatedEntity::onContainered()
+{
+}
+
+void LocatedEntity::onUpdated()
+{
+}
+
+Router::Router(const std::string & id, long intId) : m_id(id),
+                                                             m_intId(intId)
+{
+}
+
+Router::~Router()
+{
+}
+
+void Router::addToMessage(Atlas::Message::MapType & omap) const
+{
+}
+
+void Router::addToEntity(const Atlas::Objects::Entity::RootEntity & ent) const
+{
+}
+
+void Router::error(const Operation & op,
+                   const std::string & errstring,
+                   OpVector & res,
+                   const std::string & to) const
+{
+}
+
+void Router::clientError(const Operation & op,
+                         const std::string & errstring,
+                         OpVector & res,
+                         const std::string & to) const
+{
+}
+
+Location::Location() : m_loc(0)
+{
+}
