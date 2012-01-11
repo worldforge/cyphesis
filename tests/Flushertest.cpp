0c239803abc9bfaf99ab0fa9829c8d0f580b1c22
Alistair Riddoch <alriddoch@googlemail.com>
2012-01-11 13:16:36 +0000

    Add stub test for Flusher

---
+++ tests/Flushertest.cpp
@@ -0,0 +1,48 @@
+// Cyphesis Online RPG Server and AI Engine
+// Copyright (C) 2011 Alistair Riddoch
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+// $Id$
+
+#ifdef NDEBUG
+#undef NDEBUG
+#endif
+#ifndef DEBUG
+#define DEBUG
+#endif
+
+#include "tools/Flusher.h"
+
+#include "common/compose.hpp"
+
+#include <cassert>
+
+int main()
+{
+}
+
+// stubs
+
+#include "common/log.h"
+#include "common/Tick.h"
+
+namespace Atlas { namespace Objects { namespace Operation {
+int TICK_NO = -1;
+} } }
+
+void log(LogLevel lvl, const std::string & msg)
+{
+}
