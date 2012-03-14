3030f36ca4928b4651c177f9c813e6bbc0f2dd3e
Alistair Riddoch <alriddoch@googlemail.com>
2012-03-14 20:30:55 +0000

    Add test for ClientPropertyManager

---
+++ tests/ClientPropertyManagertest.cpp
@@ -0,0 +1,45 @@
+// Cyphesis Online RPG Server and AI Engine
+// Copyright (C) 2012 Alistair Riddoch
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+// $Id$
+
+#ifdef NDEBUG
+#undef NDEBUG
+#endif
+#ifndef DEBUG
+#define DEBUG
+#endif
+
+#include "client/ClientPropertyManager.h"
+
+#include <cassert>
+
+int main()
+{
+    {
+        ClientPropertyManager * cpm = new ClientPropertyManager;
+        delete cpm;
+    }
+
+    {
+        ClientPropertyManager * cpm = new ClientPropertyManager;
+        assert(cpm->addProperty("foo", 0) == 0);
+        delete cpm;
+    }
+
+    return 0;
+}
