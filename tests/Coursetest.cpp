dcf00c8d836fe926509745bd83bcabbc336e2df2
Alistair Riddoch <alriddoch@googlemail.com>
2012-04-29 09:05:17 +0100

    Add missing test source

---
+++ tests/Coursetest.cpp
@@ -0,0 +1,42 @@
+// Cyphesis Online RPG Server and AI Engine
+// Copyright (C) 2012 Alistair Riddoch
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+// $Id$
+
+#ifdef NDEBUG
+#undef NDEBUG
+#endif
+#ifndef DEBUG
+#define DEBUG
+#endif
+
+#include "physics/Course.h"
+
+#include <wfmath/line.h>
+
+#include <cassert>
+
+int main()
+{
+    {
+        Course<2, WFMath::Line> * linear_course = new Course<2, WFMath::Line>;
+        assert(linear_course != 0);
+        delete linear_course;
+    }
+
+    return 0;
+}
