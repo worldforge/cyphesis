ff070f403d14ebe77ca6fa2c37bd39ba43fba7e6
Alistair Riddoch <alriddoch@googlemail.com>
2012-06-06 09:46:15 +0100

    Add new class for cycmd avatar context

---
+++ tools/AvatarContext.cpp
@@ -0,0 +1,53 @@
+// Cyphesis Online RPG Server and AI Engine
+// Copyright (C) 2012 Alistair Riddoch
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+// $Id$
+
+#include "AvatarContext.h"
+
+#include <Atlas/Objects/RootOperation.h>
+#include <Atlas/Objects/SmartPtr.h>
+
+#include <iostream>
+
+using Atlas::Objects::Operation::RootOperation;
+
+AvatarContext::AvatarContext() : m_refNo(0L)
+{
+}
+
+bool AvatarContext::accept(const RootOperation& op) const
+{
+    std::cout << "Checking avatar context to see if it matches"
+              << std::endl << std::flush;
+    return false;
+}
+
+int AvatarContext::dispatch(const RootOperation & op)
+{
+    return 0;
+}
+
+std::string AvatarContext::repr() const
+{
+    return "avatar";
+}
+
+bool AvatarContext::checkContextCommand(const struct command *)
+{
+    return false;
+}
