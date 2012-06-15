07b0cccc9e8a928e590ddc6a9c21c38a7d861b03
Anthony Pesce <timetopat@gmail.com>
2012-06-15 17:50:24 -0400

    Updated task to use move function instead of create.

--- rulesets/mason/world/tasks/Pioneeringconstruction.py
+++ rulesets/mason/world/tasks/Pioneeringconstruction.py
@@ -10,8 +10,8 @@ from physics import Vector3D
 import server
 
 class Pioneeringconstruction(server.Task):
-    """A task for creating a Wooden structures such as A Frames with lumber and rope but for now a hammer"""
-    
+    """A task for creating a Wooden structures such as A Frames with lumber and rope but for now a hammer""" 
+
     materials = "lumber"
     def constructions_operation(self, op):
         """ Op handler for Pioneeringconstruction op which activates this task """
@@ -41,7 +41,7 @@ class Pioneeringconstruction(server.Task):
             self.irrelevant()
             return
 
-        res=Oplist()
+        
 
         if self.progress < 1:
             # print "Not done yet"
@@ -50,7 +50,6 @@ class Pioneeringconstruction(server.Task):
         self.progress = 0
         lcount = 0
         count = 0
-        # List which stores the to be consumed entity  
         raw_materials = []
 
         # Make sure only 1 part of each attribute is being consumed as per the recipe. 
@@ -68,32 +67,18 @@ class Pioneeringconstruction(server.Task):
         chunk_loc = Location(self.character.location.parent)
         chunk_loc.velocity = Vector3D()
         chunk_loc.coordinates = self.pos
-        chunk_loc.orientation=Quaternion([1,0.5,0,1])
+        chunk_loc.orientation=Quaternion([0,0,0,0])
         count = lcount 
-
-        # consume the materials stores in the list raw_materials
+        res=Oplist()
+        
+        #loops through raw_materials and attempts to place 3 lumber in inventory infront of user
         while (count > 0) : 
             tar = raw_materials.pop()
-            set = Operation("set", Entity(tar.id, status = -1), to = tar)
-            res.append(set)
+            move=Operation("move", Entity(tar.id,location=chunk_loc), to=self.target())
+            print " ID "+tar.id+"!"
+            res.append(move)
             count = count - 1
-        #right part of a frame
-        offset=Vector3D(0.5,0,0.5)
-        chunk_loc.orientation=Quaternion([1,0.5,0,1])
-        create1=Operation("create", Entity(name = "lumber1", type = "lumber", location = chunk_loc), to = target)      
-        res.append(create1)
-        #left part of a frame
-        chunk_loc.orientation=Quaternion([1,-0.5,0,1])
-        chunk_loc.coordinates=chunk_loc.coordinates+offset
-        create2=Operation("create", Entity(name = "lumber2", type = "lumber", location = chunk_loc), to = target)      
-        res.append(create2)
-        #bottom part of a frame
-        offset1=Vector3D(-0.4,0,0.5)
-        chunk_loc.orientation=Quaternion([1,2,0,2])
-        chunk_loc.coordinates=chunk_loc.coordinates+offset1
-        create3=Operation("create", Entity(name = "lumber3", type = "lumber", location = chunk_loc), to = target)      
-        res.append(create3)
-        
+            
         
         self.progress = 1
         self.irrelevant()
