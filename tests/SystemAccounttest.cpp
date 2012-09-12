8edb61e75208575acaa85789baeeb5c7cf6e8d80
Alistair Riddoch <alriddoch@googlemail.com>
2012-09-12 13:58:33 +0100

    Add a stub SystemAccount test

---
+++ tests/SystemAccounttest.cpp
@@ -0,0 +1,930 @@
+// Cyphesis Online RPG Server and AI Engine
+// Copyright (C) 2009 Alistair Riddoch
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+// $Id$
+
+#ifdef NDEBUG
+#undef NDEBUG
+#endif
+#ifndef DEBUG
+#define DEBUG
+#endif
+
+#include "TestBase.h"
+#include "TestWorld.h"
+
+#include "server/SystemAccount.h"
+
+#include "server/CommSocket.h"
+#include "server/Connection.h"
+#include "server/ServerRouting.h"
+
+#include "rulesets/Entity.h"
+
+#include "common/compose.hpp"
+
+#include <Atlas/Objects/RootEntity.h>
+#include <Atlas/Objects/SmartPtr.h>
+
+#include <cassert>
+
+using Atlas::Message::MapType;
+using Atlas::Objects::Root;
+using Atlas::Objects::Entity::RootEntity;
+
+using String::compose;
+
+class SystemAccounttest : public Cyphesis::TestBase
+{
+  protected:
+    long m_id_counter;
+
+    ServerRouting * m_server;
+    Connection * m_connection;
+    Account * m_account;
+  public:
+    SystemAccounttest();
+
+    void setup();
+    void teardown();
+
+    void test_null();
+};
+
+class TestSystemAccount : public SystemAccount {
+  public:
+    TestSystemAccount(Connection * conn,
+                      const std::string & username,
+                      const std::string & passwd,
+                      const std::string & id, long intId);
+
+    ~TestSystemAccount();
+
+    virtual int characterError(const Operation & op,
+                               const Atlas::Objects::Root & ent,
+                               OpVector & res) const;
+
+    Entity * testAddNewCharacter(const std::string & typestr,
+                                 const RootEntity & ent,
+                                 const RootEntity & arg);
+};
+
+SystemAccounttest::SystemAccounttest() : m_id_counter(0L),
+                                         m_server(0),
+                                         m_connection(0),
+                                         m_account(0)
+{
+    ADD_TEST(SystemAccounttest::test_null);
+}
+
+void SystemAccounttest::setup()
+{
+    Entity * gw = new Entity(compose("%1", m_id_counter),
+                             m_id_counter++);
+    m_server = new ServerRouting(*new TestWorld(*gw),
+                                 "5529d7a4-0158-4dc1-b4a5-b5f260cac635",
+                                 "bad621d4-616d-4faf-b9e6-471d12b139a9",
+                                 compose("%1", m_id_counter), m_id_counter++,
+                                 compose("%1", m_id_counter), m_id_counter++);
+    m_connection = new Connection(*(CommSocket*)0, *m_server,
+                                  "8d18a4e8-f14f-4a46-997e-ada120d5438f",
+                                  compose("%1", m_id_counter), m_id_counter++);
+    m_account = new TestSystemAccount(m_connection,
+                                "6c9f3236-5de7-4ba4-8b7a-b0222df0af38",
+                                "fa1a03a2-a745-4033-85cb-bb694e921e62",
+                                compose("%1", m_id_counter), m_id_counter++);
+}
+
+void SystemAccounttest::teardown()
+{
+    delete m_server;
+}
+
+void SystemAccounttest::test_null()
+{
+    ASSERT_TRUE(m_account != 0);
+}
+
+TestSystemAccount::TestSystemAccount(Connection * conn,
+                                     const std::string & username,
+                                     const std::string & passwd,
+                                     const std::string & id, long intId) :
+      SystemAccount(conn, username, passwd, id, intId)
+{
+}
+
+TestSystemAccount::~TestSystemAccount()
+{
+    delete m_connection;
+}
+
+int TestSystemAccount::characterError(const Operation & op,
+                                      const Atlas::Objects::Root & ent,
+                                      OpVector & res) const
+{
+    return 0;
+}
+
+Entity * TestSystemAccount::testAddNewCharacter(const std::string & typestr,
+                                                const RootEntity & ent,
+                                                const RootEntity & arg)
+{
+    return addNewCharacter(typestr, ent, arg);
+}
+
+void TestWorld::message(const Operation & op, Entity & ent)
+{
+}
+
+Entity * TestWorld::addNewEntity(const std::string &,
+                                 const Atlas::Objects::Entity::RootEntity &)
+{
+    return 0;
+}
+
+int main()
+{
+    SystemAccounttest t;
+
+    return t.run();
+}
+
+// stubs
+
+#include "server/Connection.h"
+#include "server/Persistence.h"
+#include "server/TeleportAuthenticator.h"
+
+#include "rulesets/Character.h"
+
+#include "common/globals.h"
+#include "common/id.h"
+#include "common/log.h"
+
+#include <cstdlib>
+
+Account::Account(Connection * conn,
+                 const std::string & uname,
+                 const std::string & passwd,
+                 const std::string & id,
+                 long intId) :
+         ConnectedRouter(id, intId, conn),
+         m_username(uname), m_password(passwd)
+{
+}
+
+Account::~Account()
+{
+}
+
+Entity * Account::addNewCharacter(const std::string & typestr,
+                                  const RootEntity & ent,
+                                  const Root & arg)
+{
+    return 0;
+}
+
+int Account::connectCharacter(Entity *chr)
+{
+    return 0;
+}
+
+const char * Account::getType() const
+{
+    return "account";
+}
+
+void Account::store() const
+{
+}
+
+void Account::createObject(const std::string & type_str,
+                           const Root & arg,
+                           const Operation & op,
+                           OpVector & res)
+{
+}
+
+void Account::addToMessage(MapType & omap) const
+{
+}
+
+void Account::addToEntity(const RootEntity & ent) const
+{
+}
+
+void Account::operation(const Operation & op, OpVector & res)
+{
+}
+
+void Account::LogoutOperation(const Operation &, OpVector &)
+{
+}
+
+void Account::CreateOperation(const Operation &, OpVector &)
+{
+}
+
+void Account::SetOperation(const Operation &, OpVector &)
+{
+}
+
+void Account::ImaginaryOperation(const Operation &, OpVector &)
+{
+}
+
+void Account::TalkOperation(const Operation &, OpVector &)
+{
+}
+
+void Account::LookOperation(const Operation &, OpVector &)
+{
+}
+
+void Account::GetOperation(const Operation &, OpVector &)
+{
+}
+
+void Account::OtherOperation(const Operation &, OpVector &)
+{
+}
+
+Admin::Admin(Connection * conn,
+             const std::string & username,
+             const std::string & passwd,
+             const std::string & id,
+             long intId) :
+       Account(conn, username, passwd, id, intId)
+{
+}
+
+Admin::~Admin()
+{
+    if (m_monitorConnection.connected()) {
+        m_monitorConnection.disconnect();
+    }
+}
+
+const char * Admin::getType() const
+{
+    return "admin";
+}
+
+void Admin::addToMessage(MapType & omap) const
+{
+}
+
+void Admin::addToEntity(const Atlas::Objects::Entity::RootEntity & ent) const
+{
+}
+
+int Admin::characterError(const Operation & op,
+                          const Root & ent, OpVector & res) const
+{
+    return 0;
+}
+
+void Admin::createObject(const std::string & type_str,
+                           const Root & arg,
+                           const Operation & op,
+                           OpVector & res)
+{
+}
+
+void Admin::LogoutOperation(const Operation & op, OpVector & res)
+{
+}
+
+void Admin::GetOperation(const Operation & op, OpVector & res)
+{
+}
+
+void Admin::SetOperation(const Operation & op, OpVector & res)
+{
+}
+
+void Admin::OtherOperation(const Operation & op, OpVector & res)
+{
+}
+
+Connection::Connection(CommSocket & client,
+                       ServerRouting & svr,
+                       const std::string & addr,
+                       const std::string & id, long iid) :
+            Link(client, id, iid), m_obsolete(false),
+                                                m_server(svr)
+{
+}
+
+Account * Connection::newAccount(const std::string & type,
+                                 const std::string & username,
+                                 const std::string & passwd,
+                                 const std::string & id, long intId)
+{
+    return 0;
+}
+
+int Connection::verifyCredentials(const Account &,
+                                  const Atlas::Objects::Root &) const
+{
+    return 0;
+}
+
+Connection::~Connection()
+{
+}
+
+void Connection::operation(const Operation &, OpVector &)
+{
+}
+
+void Connection::LoginOperation(const Operation &, OpVector &)
+{
+}
+
+void Connection::LogoutOperation(const Operation &, OpVector &)
+{
+}
+
+void Connection::CreateOperation(const Operation &, OpVector &)
+{
+}
+
+void Connection::GetOperation(const Operation &, OpVector &)
+{
+}
+
+void Connection::connectAvatar(Character * chr)
+{
+}
+
+void Connection::addEntity(Entity * ent)
+{
+}
+
+ConnectedRouter::ConnectedRouter(const std::string & id,
+                                 long iid,
+                                 Connection *c) :
+                 Router(id, iid),
+                 m_connection(c)
+{
+}
+
+ConnectedRouter::~ConnectedRouter()
+{
+}
+
+ServerRouting::ServerRouting(BaseWorld & wrld,
+                             const std::string & ruleset,
+                             const std::string & name,
+                             const std::string & id, long intId,
+                             const std::string & lId, long lIntId) :
+        Router(id, intId),
+        m_svrRuleset(ruleset), m_svrName(name),
+        m_numClients(0), m_world(wrld), m_lobby(*(Lobby*)0)
+{
+}
+
+ServerRouting::~ServerRouting()
+{
+}
+
+void ServerRouting::addToMessage(Atlas::Message::MapType & omap) const
+{
+}
+
+void ServerRouting::addToEntity(const Atlas::Objects::Entity::RootEntity & ent) const
+{
+}
+
+Account * ServerRouting::getAccountByName(const std::string & username)
+{
+    return 0;
+}
+
+void ServerRouting::addAccount(Account * a)
+{
+}
+
+TeleportAuthenticator * TeleportAuthenticator::m_instance = NULL;
+
+int TeleportAuthenticator::removeTeleport(const std::string &entity_id)
+{
+    return 0;
+}
+
+Entity *TeleportAuthenticator::authenticateTeleport(const std::string &entity_id,
+                                            const std::string &possess_key)
+{
+    return 0;
+}
+
+Persistence * Persistence::m_instance = NULL;
+
+Persistence::Persistence() : m_db(*(Database*)0)
+{
+}
+
+Persistence * Persistence::instance()
+{
+    if (m_instance == NULL) {
+        m_instance = new Persistence();
+    }
+    return m_instance;
+}
+
+void Persistence::putAccount(const Account & ac)
+{
+}
+
+Character::Character(const std::string & id, long intId) :
+           Character_parent(id, intId),
+               m_movement(*(Movement*)0),
+               m_mind(0), m_externalMind(0)
+{
+}
+
+Character::~Character()
+{
+}
+
+void Character::operation(const Operation & op, OpVector &)
+{
+}
+
+void Character::externalOperation(const Operation & op)
+{
+}
+
+
+void Character::ImaginaryOperation(const Operation & op, OpVector &)
+{
+}
+
+void Character::InfoOperation(const Operation & op, OpVector &)
+{
+}
+
+void Character::TickOperation(const Operation & op, OpVector &)
+{
+}
+
+void Character::TalkOperation(const Operation & op, OpVector &)
+{
+}
+
+void Character::NourishOperation(const Operation & op, OpVector &)
+{
+}
+
+void Character::UseOperation(const Operation & op, OpVector &)
+{
+}
+
+void Character::WieldOperation(const Operation & op, OpVector &)
+{
+}
+
+void Character::AttackOperation(const Operation & op, OpVector &)
+{
+}
+
+void Character::ActuateOperation(const Operation & op, OpVector &)
+{
+}
+
+void Character::mindActuateOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindAttackOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindCombineOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindCreateOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindDeleteOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindDivideOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindEatOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindGoalInfoOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindImaginaryOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindLookOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindMoveOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindSetOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindSetupOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindTalkOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindThoughtOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindTickOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindTouchOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindUpdateOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindUseOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindWieldOperation(const Operation &, OpVector &)
+{
+}
+
+
+void Character::mindOtherOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::sendMind(const Operation & op, OpVector & res)
+{
+}
+
+Thing::Thing(const std::string & id, long intId) :
+       Thing_parent(id, intId)
+{
+}
+
+Thing::~Thing()
+{
+}
+
+void Thing::DeleteOperation(const Operation & op, OpVector & res)
+{
+}
+
+void Thing::MoveOperation(const Operation & op, OpVector & res)
+{
+}
+
+void Thing::SetOperation(const Operation & op, OpVector & res)
+{
+}
+
+void Thing::LookOperation(const Operation & op, OpVector & res)
+{
+}
+
+void Thing::CreateOperation(const Operation & op, OpVector & res)
+{
+}
+
+void Thing::UpdateOperation(const Operation & op, OpVector & res)
+{
+}
+
+Entity::Entity(const std::string & id, long intId) :
+        LocatedEntity(id, intId), m_motion(0), m_flags(0)
+{
+}
+
+Entity::~Entity()
+{
+}
+
+void Entity::destroy()
+{
+    destroyed.emit();
+}
+
+void Entity::ActuateOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::AppearanceOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::AttackOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::CombineOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::CreateOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::DeleteOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::DisappearanceOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::DivideOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::EatOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::GetOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::InfoOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::ImaginaryOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::LookOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::MoveOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::NourishOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::SetOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::SightOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::SoundOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::TalkOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::TickOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::TouchOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::UpdateOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::UseOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::WieldOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::externalOperation(const Operation & op)
+{
+}
+
+void Entity::operation(const Operation & op, OpVector & res)
+{
+}
+
+void Entity::addToMessage(Atlas::Message::MapType & omap) const
+{
+}
+
+void Entity::addToEntity(const Atlas::Objects::Entity::RootEntity & ent) const
+{
+}
+
+PropertyBase * Entity::setAttr(const std::string & name,
+                               const Atlas::Message::Element & attr)
+{
+    return 0;
+}
+
+const PropertyBase * Entity::getProperty(const std::string & name) const
+{
+    return 0;
+}
+
+PropertyBase * Entity::modProperty(const std::string & name)
+{
+    return 0;
+}
+
+void Entity::onContainered()
+{
+}
+
+void Entity::onUpdated()
+{
+}
+
+void Entity::callOperation(const Operation & op, OpVector & res)
+{
+}
+
+LocatedEntity::LocatedEntity(const std::string & id, long intId) :
+               Router(id, intId),
+               m_refCount(0), m_seq(0),
+               m_script(0), m_type(0), m_contains(0)
+{
+}
+
+LocatedEntity::~LocatedEntity()
+{
+}
+
+bool LocatedEntity::hasAttr(const std::string & name) const
+{
+    return false;
+}
+
+int LocatedEntity::getAttr(const std::string & name,
+                           Atlas::Message::Element & attr) const
+{
+    return -1;
+}
+
+int LocatedEntity::getAttrType(const std::string & name,
+                               Atlas::Message::Element & attr,
+                               int type) const
+{
+    return -1;
+}
+
+PropertyBase * LocatedEntity::setAttr(const std::string & name,
+                                      const Atlas::Message::Element & attr)
+{
+    return 0;
+}
+
+const PropertyBase * LocatedEntity::getProperty(const std::string & name) const
+{
+    return 0;
+}
+
+void LocatedEntity::onContainered()
+{
+}
+
+void LocatedEntity::onUpdated()
+{
+}
+
+void LocatedEntity::makeContainer()
+{
+    if (m_contains == 0) {
+        m_contains = new LocatedEntitySet;
+    }
+}
+
+void LocatedEntity::merge(const MapType & ent)
+{
+}
+
+Link::Link(CommSocket & socket, const std::string & id, long iid) :
+            Router(id, iid), m_encoder(0), m_commSocket(socket)
+{
+}
+
+Link::~Link()
+{
+}
+
+void Link::send(const Operation & op) const
+{
+}
+
+void Link::disconnect()
+{
+}
+
+BaseWorld * BaseWorld::m_instance = 0;
+
+BaseWorld::BaseWorld(Entity & gw) : m_gameWorld(gw)
+{
+    m_instance = this;
+}
+
+BaseWorld::~BaseWorld()
+{
+    m_instance = 0;
+}
+
+Entity * BaseWorld::getEntity(const std::string & id) const
+{
+    return 0;
+}
+
+Entity * BaseWorld::getEntity(long id) const
+{
+    return 0;
+}
+
+Router::Router(const std::string & id, long intId) : m_id(id),
+                                                             m_intId(intId)
+{
+}
+
+Router::~Router()
+{
+}
+
+void Router::addToMessage(Atlas::Message::MapType & omap) const
+{
+}
+
+void Router::addToEntity(const Atlas::Objects::Entity::RootEntity & ent) const
+{
+}
+
+void Router::error(const Operation & op,
+                   const std::string & errstring,
+                   OpVector & res,
+                   const std::string & to) const
+{
+}
+
+Location::Location() : m_loc(0)
+{
+}
+
+long integerId(const std::string & id)
+{
+    long intId = strtol(id.c_str(), 0, 10);
+    if (intId == 0 && id != "0") {
+        intId = -1L;
+    }
+
+    return intId;
+}
+
+void logEvent(LogEvent lev, const std::string & msg)
+{
+}
+
+void log(LogLevel lvl, const std::string & msg)
+{
+}
+
+bool database_flag = false;
