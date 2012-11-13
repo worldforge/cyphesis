2d15e104100cce8d1da294f0edfe507f6fe046ac
anthonypesce <timetopat@gmail.com>
2012-11-12 20:25:19 -0500

    Added test for Shaker and Connection.

---
+++ tests/ConnectionShakerintegration.cpp
@@ -0,0 +1,822 @@
+// Cyphesis Online RPG Server and AI Engine
+// Copyright (C) 2012 Anthony Pesce
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+// $Id$
+
+#ifdef NDEBUG
+#undef NDEBUG
+#endif
+#ifndef DEBUG
+#define DEBUG
+#endif
+
+#include "TestBase.h"
+
+#include "TestWorld.h"
+#include "null_stream.h"
+#include "common/Shaker.h"
+#include "server/CommServer.h"
+#include "server/Connection.h"
+#include "server/CommClient.h"
+
+#include "server/ServerAccount.h"
+#include "server/ServerRouting.h"
+
+#include <Atlas/Objects/Anonymous.h>
+#include <Atlas/Objects/Operation.h>
+
+#include "common/Monitors.h"
+#include "common/PropertyManager.h"
+#include "common/Variable.h"
+
+#include "server/ExternalMind.h"
+#include "common/const.h"
+#include "common/globals.h"
+#include "common/log.h"
+#include "rulesets/Character.h"
+#include "common/TypeNode.h"
+
+#include "server/ExternalProperty.h"
+#include "server/Persistence.h"
+#include "server/Ruleset.h"
+#include "server/TeleportAuthenticator.h"
+#include "server/Juncture.h"
+#include <cstdio>
+
+#include "common/const.h"
+
+using Atlas::Message::Element;
+using Atlas::Message::ListType;
+using Atlas::Message::MapType;
+using Atlas::Objects::Root;
+using Atlas::Objects::Entity::RootEntity;
+using Atlas::Objects::Entity::Anonymous;
+using Atlas::Objects::Operation::Create;
+using Atlas::Objects::Operation::Get;
+using Atlas::Objects::Operation::Imaginary;
+using Atlas::Objects::Operation::Logout;
+using Atlas::Objects::Operation::Look;
+using Atlas::Objects::Operation::Set;
+using Atlas::Objects::Operation::Talk;
+using Atlas::Objects::Operation::Move;
+using Atlas::Objects::smart_dynamic_cast;
+
+
+static long idGenerator = 500;
+
+bool database_flag = false;
+
+namespace consts {
+
+  const char * buildTime = __TIME__;
+  const char * buildDate = __DATE__;
+  const int buildId = -1;
+  const char * version = "test_build";
+}
+
+const char * CYPHESIS = "cyphesisAccountConnectionintegration";
+int timeoffset = 0;
+std::string instance(CYPHESIS);
+Ruleset * Ruleset::m_instance = NULL;
+
+int Ruleset::installRule(const std::string & class_name,
+                         const std::string & section,
+                         const Root & class_desc)
+{
+    return 0;
+}
+
+int Ruleset::modifyRule(const std::string & class_name,
+                        const Root & class_desc)
+{
+    return 0;
+}
+
+
+long newId(std::string & id)
+{
+    static char buf[32];
+    long new_id = ++idGenerator;
+    sprintf(buf, "%ld", new_id);
+    id = buf;
+    assert(!id.empty());
+    return new_id;
+}
+
+void log(LogLevel lvl, const std::string & msg)
+{
+    std::cout << msg << std::endl;
+}
+
+void logEvent(LogEvent lev, const std::string & msg)
+{
+}
+void Persistence::putAccount(const Account & ac)
+{
+}
+
+Juncture::Juncture(Connection * c,
+                   const std::string & id, long iid) :
+          ConnectedRouter(id, iid, c),
+                                                       m_socket(0),
+                                                       m_peer(0)
+{
+}
+
+Juncture::~Juncture()
+{
+}
+
+void Juncture::operation(const Operation & op, OpVector & res)
+{
+}
+
+void Juncture::addToMessage(MapType & omap) const
+{
+}
+
+void Juncture::addToEntity(const RootEntity & ent) const
+{
+}
+
+class ConnectionShakerintegration : public Cyphesis::TestBase
+{
+  private:
+    Entity * m_tlve;
+    BaseWorld * m_world;
+    CommServer * m_cs;
+    Connection * m_connection;
+    ServerRouting * m_server;
+  public:
+    ConnectionShakerintegration();
+    void testAccount();
+    void setup();
+    void teardown();
+};
+
+class TestCommClient : public CommClient<null_stream> {
+  public:
+    TestCommClient(CommServer & cs) : CommClient<null_stream>(cs, "") { }
+};
+
+class SpawningTestWorld : public TestWorld {
+  public:
+    SpawningTestWorld(Entity & gw) : TestWorld(gw) { }
+
+    virtual Entity * addEntity(Entity * ent) { 
+        ent->m_location.m_loc = &m_gameWorld;
+        ent->m_location.m_pos = WFMath::Point<3>(0,0,0);
+        m_eobjects[ent->getIntId()] = ent;
+        return ent;
+    }
+
+    virtual Entity * addNewEntity(const std::string & t,
+                                  const Atlas::Objects::Entity::RootEntity &) {
+        std::string id;
+        long intId = newId(id);
+
+        Entity * e = new Character(id, intId);
+
+        e->setType(new TypeNode(t));
+        return addEntity(e);
+    }
+};
+
+ConnectionShakerintegration::ConnectionShakerintegration()
+{
+    ADD_TEST(ConnectionShakerintegration::testAccount);
+}
+
+void ConnectionShakerintegration::testAccount()
+{
+	//wanted to test adding the new account that calls shaker
+	//ASSERT_NOT_NULL(m_connection->addNewAccount("player","testuser","testpassword"));
+	ASSERT_NOT_NULL(m_connection);
+}
+
+void ConnectionShakerintegration::setup()
+{
+		    m_tlve = new Entity("0", 0);
+            m_world = new SpawningTestWorld(*m_tlve);
+            m_cs = new CommServer;
+	        m_connection = new Connection(*new TestCommClient(*m_cs),
+                                      *m_server,
+                                      "test_addr",
+                                      "3", 3);
+}
+
+void ConnectionShakerintegration::teardown()
+{
+	delete m_tlve;
+	delete m_world;
+	//delete m_cs;
+	delete m_connection;
+}
+
+int main()
+{
+   ConnectionShakerintegration t;
+   return t.run();
+}
+
+//stubs
+
+void hash_password(const std::string & pwd, const std::string & salt,
+                   std::string & hash)
+{
+}
+ExternalProperty::ExternalProperty(Router * & data) : m_data(data)
+{
+}
+
+int ExternalProperty::get(Atlas::Message::Element & val) const
+{
+    return 0;
+}
+
+void ExternalProperty::set(const Atlas::Message::Element & val)
+{
+}
+
+void ExternalProperty::add(const std::string & s,
+                         Atlas::Message::MapType & map) const
+{
+}
+
+void ExternalProperty::add(const std::string & s,
+                         const Atlas::Objects::Entity::RootEntity & ent) const
+{
+}
+TeleportAuthenticator * TeleportAuthenticator::m_instance = NULL;
+
+int TeleportAuthenticator::addTeleport(const std::string &entity_id,
+                                        const std::string &possess_key)
+{
+    return 0;
+}
+
+int TeleportAuthenticator::removeTeleport(const std::string &entity_id)
+{
+    return 0;
+}
+
+Entity *TeleportAuthenticator::authenticateTeleport(const std::string &entity_id,
+                                            const std::string &possess_key)
+{
+    return 0;
+}
+
+Persistence * Persistence::m_instance = NULL;
+
+Persistence::Persistence() : m_db(*(Database*)0)
+{
+}
+
+Persistence * Persistence::instance()
+{
+    if (m_instance == NULL) {
+        m_instance = new Persistence();
+    }
+    return m_instance;
+}
+
+void Persistence::registerCharacters(Account & ac,
+                                     const EntityDict & worldObjects)
+{
+}
+
+Account * Persistence::getAccount(const std::string & name)
+{
+    return 0;
+}
+
+void TestWorld::message(const Operation & op, Entity & ent)
+{
+}
+
+Entity * TestWorld::addNewEntity(const std::string &,
+                                 const Atlas::Objects::Entity::RootEntity &)
+{
+    return 0;
+}
+CommServer::CommServer() : m_congested(false)
+{
+}
+
+CommServer::~CommServer()
+{
+}
+
+Idle::Idle(CommServer & svr) : m_idleManager(svr)
+{
+}
+
+Idle::~Idle()
+{
+}
+
+CommSocket::CommSocket(CommServer & svr) : m_commServer(svr) { }
+
+CommSocket::~CommSocket()
+{
+}
+
+int CommSocket::flush()
+{
+    return 0;
+}
+
+ExternalMind::ExternalMind(Entity & e) : Router(e.getId(), e.getIntId()),
+                                         m_external(0), m_entity(e)
+{
+}
+
+ExternalMind::~ExternalMind()
+{
+}
+
+void ExternalMind::operation(const Operation & op, OpVector & res)
+{
+}
+
+const std::string & ExternalMind::connectionId()
+{
+    assert(m_external != 0);
+    return m_external->getId();
+}
+
+void ExternalMind::linkUp(Link * c)
+{
+    m_external = c;
+}
+
+Character::Character(const std::string & id, long intId) :
+           Character_parent(id, intId),
+               m_movement(*(Movement*)0),
+               m_mind(0), m_externalMind(0)
+{
+}
+
+Character::~Character()
+{
+}
+
+void Character::operation(const Operation & op, OpVector &)
+{
+}
+
+void Character::externalOperation(const Operation & op)
+{
+}
+
+
+void Character::ImaginaryOperation(const Operation & op, OpVector &)
+{
+}
+
+void Character::InfoOperation(const Operation & op, OpVector &)
+{
+}
+
+void Character::TickOperation(const Operation & op, OpVector &)
+{
+}
+
+void Character::TalkOperation(const Operation & op, OpVector &)
+{
+}
+
+void Character::NourishOperation(const Operation & op, OpVector &)
+{
+}
+
+void Character::UseOperation(const Operation & op, OpVector &)
+{
+}
+
+void Character::WieldOperation(const Operation & op, OpVector &)
+{
+}
+
+void Character::AttackOperation(const Operation & op, OpVector &)
+{
+}
+
+void Character::ActuateOperation(const Operation & op, OpVector &)
+{
+}
+
+void Character::mindActuateOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindAttackOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindCombineOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindCreateOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindDeleteOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindDivideOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindEatOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindGoalInfoOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindImaginaryOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindLookOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindMoveOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindSetOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindSetupOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindTalkOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindThoughtOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindTickOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindTouchOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindUpdateOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindUseOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindWieldOperation(const Operation &, OpVector &)
+{
+}
+
+void Character::mindOtherOperation(const Operation &, OpVector &)
+{
+}
+
+
+Thing::Thing(const std::string & id, long intId) :
+       Thing_parent(id, intId)
+{
+}
+
+Thing::~Thing()
+{
+}
+
+void Thing::DeleteOperation(const Operation & op, OpVector & res)
+{
+}
+
+void Thing::MoveOperation(const Operation & op, OpVector & res)
+{
+}
+
+void Thing::SetOperation(const Operation & op, OpVector & res)
+{
+}
+
+void Thing::LookOperation(const Operation & op, OpVector & res)
+{
+}
+
+void Thing::CreateOperation(const Operation & op, OpVector & res)
+{
+}
+
+void Thing::UpdateOperation(const Operation & op, OpVector & res)
+{
+}
+
+
+Entity::Entity(const std::string & id, long intId) :
+        LocatedEntity(id, intId), m_motion(0), m_flags(0)
+{
+}
+
+Entity::~Entity()
+{
+}
+
+void Entity::destroy()
+{
+    destroyed.emit();
+}
+
+void Entity::ActuateOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::AppearanceOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::AttackOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::CombineOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::CreateOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::DeleteOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::DisappearanceOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::DivideOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::EatOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::GetOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::InfoOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::ImaginaryOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::LookOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::MoveOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::NourishOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::SetOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::SightOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::SoundOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::TalkOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::TickOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::TouchOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::UpdateOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::UseOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::WieldOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::externalOperation(const Operation & op)
+{
+}
+
+void Entity::operation(const Operation & op, OpVector & res)
+{
+}
+
+void Entity::addToMessage(Atlas::Message::MapType & omap) const
+{
+}
+
+void Entity::addToEntity(const Atlas::Objects::Entity::RootEntity & ent) const
+{
+}
+
+PropertyBase * Entity::setAttr(const std::string & name,
+                               const Atlas::Message::Element & attr)
+{
+    return 0;
+}
+
+const PropertyBase * Entity::getProperty(const std::string & name) const
+{
+    return 0;
+}
+
+PropertyBase * Entity::setProperty(const std::string & name,
+                                   PropertyBase * prop)
+{
+    return m_properties[name] = prop;
+}
+
+void Entity::onContainered()
+{
+}
+
+void Entity::onUpdated()
+{
+}
+
+LocatedEntity::LocatedEntity(const std::string & id, long intId) :
+               Router(id, intId),
+               m_refCount(0), m_seq(0),
+               m_script(0), m_type(0), m_contains(0)
+{
+}
+
+LocatedEntity::~LocatedEntity()
+{
+}
+
+bool LocatedEntity::hasAttr(const std::string & name) const
+{
+    return false;
+}
+
+int LocatedEntity::getAttr(const std::string & name,
+                           Atlas::Message::Element & attr) const
+{
+    return -1;
+}
+
+int LocatedEntity::getAttrType(const std::string & name,
+                               Atlas::Message::Element & attr,
+                               int type) const
+{
+    return -1;
+}
+
+PropertyBase * LocatedEntity::setAttr(const std::string & name,
+                                      const Atlas::Message::Element & attr)
+{
+    return 0;
+}
+
+const PropertyBase * LocatedEntity::getProperty(const std::string & name) const
+{
+    return 0;
+}
+
+void LocatedEntity::onContainered()
+{
+}
+
+void LocatedEntity::onUpdated()
+{
+}
+
+PropertyManager * PropertyManager::m_instance = 0;
+
+VariableBase::~VariableBase()
+{
+}
+
+template <typename T>
+Variable<T>::Variable(const T & variable) : m_variable(variable)
+{
+}
+
+template <typename T>
+Variable<T>::~Variable()
+{
+}
+
+template <typename T>
+void Variable<T>::send(std::ostream & o)
+{
+}
+
+template class Variable<int>;
+template class Variable<std::string>;
+template class Variable<const char *>;
+
+Monitors * Monitors::m_instance = NULL;
+
+Monitors::Monitors()
+{
+}
+
+Monitors::~Monitors()
+{
+}
+
+Monitors * Monitors::instance()
+{
+    if (m_instance == NULL) {
+        m_instance = new Monitors();
+    }
+    return m_instance;
+}
+
+void Monitors::insert(const std::string & key, const Element & val)
+{
+}
+
+void Monitors::watch(const::std::string & name, VariableBase * monitor)
+{
+}
+
+Location::Location() : m_loc(0)
+{
+}
+
+long integerId(const std::string & id)
+{
+    long intId = strtol(id.c_str(), 0, 10);
+    if (intId == 0 && id != "0") {
+        intId = -1L;
+    }
+
+    return intId;
+}
+
+
+
+void encrypt_password(const std::string & pwd, std::string & hash)
+{
+}
+
+int check_password(const std::string & pwd, const std::string & hash)
+{
+    return 0;
+}
+
+bool_config_register::bool_config_register(bool & var,
+                                           const char * section,
+                                           const char * setting,
+                                           const char * help)
+{
+}
