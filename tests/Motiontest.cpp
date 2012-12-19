8563f100bc6990f3c76f2f6b3ac51764d374d358
Alistair Riddoch <alriddoch@googlemail.com>
2012-12-19 22:51:37 +0000

    Stub out Motiontest

--- tests/Motiontest.cpp
+++ tests/Motiontest.cpp
@@ -365,3 +365,300 @@ int main()
 
     t.run();
 }
+
+// stubs
+
+#include "common/const.h"
+#include "common/log.h"
+
+Entity::Entity(const std::string & id, long intId) :
+        LocatedEntity(id, intId), m_motion(0), m_flags(0)
+{
+}
+
+Entity::~Entity()
+{
+}
+
+void Entity::destroy()
+{
+    destroyed.emit();
+}
+
+void Entity::ActuateOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::AppearanceOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::AttackOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::CombineOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::CreateOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::DeleteOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::DisappearanceOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::DivideOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::EatOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::GetOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::InfoOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::ImaginaryOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::LookOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::MoveOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::NourishOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::SetOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::SightOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::SoundOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::TalkOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::TickOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::TouchOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::UpdateOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::UseOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::WieldOperation(const Operation &, OpVector &)
+{
+}
+
+void Entity::externalOperation(const Operation & op, Link &)
+{
+}
+
+void Entity::operation(const Operation & op, OpVector & res)
+{
+}
+
+void Entity::addToMessage(Atlas::Message::MapType & omap) const
+{
+}
+
+void Entity::addToEntity(const Atlas::Objects::Entity::RootEntity & ent) const
+{
+}
+
+PropertyBase * Entity::setAttr(const std::string & name,
+                               const Atlas::Message::Element & attr)
+{
+    return 0;
+}
+
+const PropertyBase * Entity::getProperty(const std::string & name) const
+{
+    return 0;
+}
+
+PropertyBase * Entity::modProperty(const std::string & name)
+{
+    return 0;
+}
+
+void Entity::onContainered()
+{
+}
+
+void Entity::onUpdated()
+{
+}
+
+Domain * Entity::getMovementDomain()
+{
+    return 0;
+}
+
+LocatedEntity::LocatedEntity(const std::string & id, long intId) :
+               Router(id, intId),
+               m_refCount(0), m_seq(0),
+               m_script(0), m_type(0), m_contains(0)
+{
+}
+
+LocatedEntity::~LocatedEntity()
+{
+}
+
+bool LocatedEntity::hasAttr(const std::string & name) const
+{
+    return false;
+}
+
+int LocatedEntity::getAttr(const std::string & name,
+                           Atlas::Message::Element & attr) const
+{
+    return -1;
+}
+
+int LocatedEntity::getAttrType(const std::string & name,
+                               Atlas::Message::Element & attr,
+                               int type) const
+{
+    return -1;
+}
+
+PropertyBase * LocatedEntity::setAttr(const std::string & name,
+                                      const Atlas::Message::Element & attr)
+{
+    return 0;
+}
+
+const PropertyBase * LocatedEntity::getProperty(const std::string & name) const
+{
+    return 0;
+}
+
+void LocatedEntity::onContainered()
+{
+}
+
+void LocatedEntity::onUpdated()
+{
+}
+
+void LocatedEntity::makeContainer()
+{
+    if (m_contains == 0) {
+        m_contains = new LocatedEntitySet;
+    }
+}
+
+void LocatedEntity::changeContainer(LocatedEntity * new_loc)
+{
+    assert(m_location.m_loc != 0);
+    assert(m_location.m_loc->m_contains != 0);
+    m_location.m_loc->m_contains->erase(this);
+    if (m_location.m_loc->m_contains->empty()) {
+        m_location.m_loc->onUpdated();
+    }
+    new_loc->makeContainer();
+    bool was_empty = new_loc->m_contains->empty();
+    new_loc->m_contains->insert(this);
+    if (was_empty) {
+        new_loc->onUpdated();
+    }
+    assert(m_location.m_loc->checkRef() > 0);
+    m_location.m_loc->decRef();
+    m_location.m_loc = new_loc;
+    m_location.m_loc->incRef();
+    assert(m_location.m_loc->checkRef() > 0);
+
+    onContainered();
+}
+
+void LocatedEntity::merge(const Atlas::Message::MapType & ent)
+{
+}
+
+Router::Router(const std::string & id, long intId) : m_id(id),
+                                                             m_intId(intId)
+{
+}
+
+Router::~Router()
+{
+}
+
+void Router::addToMessage(Atlas::Message::MapType & omap) const
+{
+}
+
+void Router::addToEntity(const Atlas::Objects::Entity::RootEntity & ent) const
+{
+}
+
+void Router::error(const Operation & op,
+                   const std::string & errstring,
+                   OpVector & res,
+                   const std::string & to) const
+{
+}
+
+Location::Location() :
+    m_simple(true), m_solid(true),
+    m_boxSize(consts::minBoxSize),
+    m_squareBoxSize(consts::minSqrBoxSize),
+    m_loc(0)
+{
+}
+
+Location::Location(LocatedEntity * rf, const Point3D & pos) :
+    m_simple(true), m_solid(true),
+    m_boxSize(consts::minBoxSize),
+    m_squareBoxSize(consts::minSqrBoxSize)
+{
+}
+
+void Location::addToEntity(const Atlas::Objects::Entity::RootEntity & ent) const
+{
+}
+
+TypeNode::TypeNode(const std::string & name) : m_name(name), m_parent(0)
+{
+}
+
+TypeNode::~TypeNode()
+{
+}
+
+void log(LogLevel lvl, const std::string & msg)
+{
+}
