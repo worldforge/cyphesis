febc72c555cda152e4f77f1dcfdc244ea88eea13
Alistair Riddoch <alriddoch@googlemail.com>
2013-01-21 19:10:46 +0000

    Add stub integration test for detecting a bug

---
+++ tests/StatisticsPropertyintegration.cpp
@@ -0,0 +1,368 @@
+// Cyphesis Online RPG Server and AI Engine
+// Copyright (C) 2012 Alistair Riddoch
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+// $Id$
+
+#ifdef NDEBUG
+#undef NDEBUG
+#endif
+#ifndef DEBUG
+#define DEBUG
+#endif
+
+#include "TestBase.h"
+
+#include "rulesets/Entity.h"
+
+// Check what happens when two instance of a type both instantiate
+// this property when there is a script. I think you'll get a segfault.
+class StatisicsPropertyintegration : public Cyphesis::TestBase
+{
+  public:
+    StatisicsPropertyintegration();
+
+    void setup();
+    void teardown();
+
+    void test_sequence();
+};
+
+StatisicsPropertyintegration::StatisicsPropertyintegration()
+{
+    ADD_TEST(StatisicsPropertyintegration::test_sequence);
+}
+
+void StatisicsPropertyintegration::setup()
+{
+}
+
+void StatisicsPropertyintegration::teardown()
+{
+}
+
+void StatisicsPropertyintegration::test_sequence()
+{
+}
+
+int main()
+{
+    StatisicsPropertyintegration t;
+
+    return t.run();
+}
+
+// stubs
+
+#include "Property_stub_impl.h"
+
+#include "rulesets/AtlasProperties.h"
+#include "rulesets/Domain.h"
+#include "rulesets/Script.h"
+
+#include "common/BaseWorld.h"
+#include "common/log.h"
+#include "common/PropertyManager.h"
+#include "common/TypeNode.h"
+
+namespace Atlas { namespace Objects { namespace Operation {
+int ACTUATE_NO = -1;
+int ATTACK_NO = -1;
+int EAT_NO = -1;
+int NOURISH_NO = -1;
+int SETUP_NO = -1;
+int TICK_NO = -1;
+int UPDATE_NO = -1;
+} } }
+
+Domain * Domain::m_instance = new Domain();
+
+Domain::Domain() : m_refCount(0)
+{
+}
+
+Domain::~Domain()
+{
+}
+
+float Domain::constrainHeight(LocatedEntity * parent,
+                              const Point3D & pos,
+                              const std::string & mode)
+{
+    return 0.f;
+}
+
+void Domain::tick(double t)
+{
+}
+
+void addToEntity(const Point3D & p, std::vector<double> & vd)
+{
+    vd.resize(3);
+    vd[0] = p[0];
+    vd[1] = p[1];
+    vd[2] = p[2];
+}
+
+Router::Router(const std::string & id, long intId) : m_id(id),
+                                                             m_intId(intId)
+{
+}
+
+Router::~Router()
+{
+}
+
+void Router::addToMessage(Atlas::Message::MapType & omap) const
+{
+}
+
+void Router::addToEntity(const Atlas::Objects::Entity::RootEntity & ent) const
+{
+}
+
+BaseWorld * BaseWorld::m_instance = 0;
+
+BaseWorld::BaseWorld(LocatedEntity & gw) : m_gameWorld(gw)
+{
+    m_instance = this;
+}
+
+BaseWorld::~BaseWorld()
+{
+    m_instance = 0;
+}
+
+LocatedEntity * BaseWorld::getEntity(const std::string & id) const
+{
+    return 0;
+}
+
+LocatedEntity * BaseWorld::getEntity(long id) const
+{
+    return 0;
+}
+
+Script::Script()
+{
+}
+
+/// \brief Script destructor
+Script::~Script()
+{
+}
+
+bool Script::operation(const std::string & opname,
+                       const Atlas::Objects::Operation::RootOperation & op,
+                       OpVector & res)
+{
+   return false;
+}
+
+void Script::hook(const std::string & function, LocatedEntity * entity)
+{
+}
+
+void Location::addToMessage(Atlas::Message::MapType & omap) const
+{
+}
+
+Location::Location() : m_loc(0)
+{
+}
+
+void Location::addToEntity(const Atlas::Objects::Entity::RootEntity & ent) const
+{
+}
+
+TypeNode::TypeNode(const std::string & name) : m_name(name), m_parent(0)
+{
+}
+
+TypeNode::~TypeNode()
+{
+    PropertyDict::const_iterator I = m_defaults.begin();
+    PropertyDict::const_iterator Iend = m_defaults.end();
+    for (; I != Iend; ++I) {
+        delete I->second;
+    }
+}
+
+IdProperty::IdProperty(const std::string & data) : PropertyBase(per_ephem),
+                                                   m_data(data)
+{
+}
+
+int IdProperty::get(Atlas::Message::Element & e) const
+{
+    return 0;
+}
+
+void IdProperty::set(const Atlas::Message::Element & e)
+{
+}
+
+void IdProperty::add(const std::string & key,
+                     Atlas::Message::MapType & ent) const
+{
+}
+
+void IdProperty::add(const std::string & key,
+                     const Atlas::Objects::Entity::RootEntity & ent) const
+{
+}
+
+IdProperty * IdProperty::copy() const
+{
+    return 0;
+}
+
+PropertyBase::PropertyBase(unsigned int flags) : m_flags(flags)
+{
+}
+
+PropertyBase::~PropertyBase()
+{
+}
+
+void PropertyBase::install(LocatedEntity *)
+{
+}
+
+void PropertyBase::apply(LocatedEntity *)
+{
+}
+
+void PropertyBase::add(const std::string & s,
+                       Atlas::Message::MapType & ent) const
+{
+    get(ent[s]);
+}
+
+void PropertyBase::add(const std::string & s,
+                       const Atlas::Objects::Entity::RootEntity & ent) const
+{
+}
+
+HandlerResult PropertyBase::operation(LocatedEntity *,
+                                      const Operation &,
+                                      OpVector &)
+{
+    return OPERATION_IGNORED;
+}
+
+template<>
+void Property<int>::set(const Atlas::Message::Element & e)
+{
+    if (e.isInt()) {
+        this->m_data = e.asInt();
+    }
+}
+
+template<>
+void Property<double>::set(const Atlas::Message::Element & e)
+{
+    if (e.isNum()) {
+        this->m_data = e.asNum();
+    }
+}
+
+template<>
+void Property<std::string>::set(const Atlas::Message::Element & e)
+{
+    if (e.isString()) {
+        this->m_data = e.String();
+    }
+}
+
+template class Property<int>;
+template class Property<double>;
+template class Property<std::string>;
+
+SoftProperty::SoftProperty()
+{
+}
+
+SoftProperty::SoftProperty(const Atlas::Message::Element & data) :
+              PropertyBase(0), m_data(data)
+{
+}
+
+int SoftProperty::get(Atlas::Message::Element & val) const
+{
+    val = m_data;
+    return 0;
+}
+
+void SoftProperty::set(const Atlas::Message::Element & val)
+{
+}
+
+SoftProperty * SoftProperty::copy() const
+{
+    return new SoftProperty(*this);
+}
+
+ContainsProperty::ContainsProperty(LocatedEntitySet & data) :
+      PropertyBase(per_ephem), m_data(data)
+{
+}
+
+int ContainsProperty::get(Atlas::Message::Element & e) const
+{
+    return 0;
+}
+
+void ContainsProperty::set(const Atlas::Message::Element & e)
+{
+}
+
+void ContainsProperty::add(const std::string & s,
+                           const Atlas::Objects::Entity::RootEntity & ent) const
+{
+}
+
+ContainsProperty * ContainsProperty::copy() const
+{
+    return 0;
+}
+
+PropertyManager * PropertyManager::m_instance = 0;
+
+PropertyManager::PropertyManager()
+{
+    assert(m_instance == 0);
+    m_instance = this;
+}
+
+PropertyManager::~PropertyManager()
+{
+   m_instance = 0;
+}
+
+long integerId(const std::string & id)
+{
+    long intId = strtol(id.c_str(), 0, 10);
+    if (intId == 0 && id != "0") {
+        intId = -1L;
+    }
+
+    return intId;
+}
+
+void log(LogLevel lvl, const std::string & msg)
+{
+}
