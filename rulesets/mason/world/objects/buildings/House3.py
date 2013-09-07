eed61fc80f85a7a5cd3420bbb4cbc47e4656c1fc
Erik Ogenvik <erik@ogenvik.org>
2013-09-07 19:11:31 +0200

    Don't create wall entities.
    Trying to do collision with extra invisible child entities is a poor
    way. This should be implemented with proper geometry instead.

--- rulesets/mason/world/objects/buildings/House3.py
+++ rulesets/mason/world/objects/buildings/House3.py
@@ -7,30 +7,6 @@ from physics import Point3D
 
 import server
 
-# bbox = 4,4,2.5
-# bmedian = 3.5,3.5,2.5
-# offset = SW corner = -0.5,-0.5
 
 class House3(server.Thing):
-    def setup_operation(self, op):
-        ret = Oplist()
-        # West wall  with door
-	loc = Location(self, Point3D(0,0,-1))
-        loc.bbox = Vector3D(0.1,2,4)
-        ret.append(Operation("create",Entity(parents=['wall'],location=loc),to=self))
-	loc = Location(self, Point3D(0,4,-1))
-        loc.bbox = Vector3D(0.1,2,4)
-        ret.append(Operation("create",Entity(parents=['wall'],location=loc),to=self))
-        # South wall
-	loc = Location(self, Point3D(0,0,-1))
-        loc.bbox = Vector3D(9,0.1,4)
-        ret.append(Operation("create",Entity(parents=['wall'],location=loc),to=self))
-        # North wall
-	loc = Location(self, Point3D(0,5.9,-1))
-        loc.bbox = Vector3D(9,0.1,4)
-        ret.append(Operation("create",Entity(parents=['wall'],location=loc),to=self))
-        # East wall
-	loc = Location(self, Point3D(8.9,0,-1))
-        loc.bbox = Vector3D(0.1,6,4)
-        ret.append(Operation("create",Entity(parents=['wall'],location=loc),to=self))
-        return ret
+    """Standard house."""
