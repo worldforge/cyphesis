0bc8dccb3dd5df0c99881cf0bc60d95750fe7978
Jesse <j.horne2796@gmail.com>
2013-09-29 23:11:32 -0400

    fixed long lines in Pioneering.py

--- rulesets/mason/world/tasks/Pioneering.py
+++ rulesets/mason/world/tasks/Pioneering.py
@@ -73,15 +73,21 @@ class Pioneering(server.Task):
         # Select which structure to produce depending on the recipe present in inventory
         if rcount == 1 :
             if wcount == 1 and lcount == 0:
-                create=Operation("create", Entity(name = "sledge", type = "sledge", location = chunk_loc), to = self.target())
+                sledge_ent = Entity(name = "sledge", type = "sledge",
+                                    location = chunk_loc)
+                create=Operation("create", sledge_ent, to = self.target())
                 res.append(create)
 
             if wcount == 1 and lcount == 1:
-                create=Operation("create", Entity(name = "board_wall", type = "board_wall", location = chunk_loc), to = self.target())
+                board_wall_ent = Entity(name = "board_wall", type = "board_wall",
+                                        location = chunk_loc)
+                create=Operation("create", board_wall_ent, to = self.target())
                 res.append(create)
 
             if wcount == 0 and lcount == 2:
-                create=Operation("create", Entity(name = "palissade_unit", type = "palissade_unit", location = chunk_loc), to = self.target())
+                palissade_unnit_ent = Entity(name = "palissade_unit",
+                                             type = "palissade_unit", location = chunk_loc)
+                create=Operation("create", palissade_unnit_ent, to = self.target())
                 res.append(create)
 
             #if wcount == 2 and lcount == 1:
@@ -90,16 +96,21 @@ class Pioneering(server.Task):
 
         if rcount == 2 :
             if wcount == 1 and lcount == 0:
-                create=Operation("create", Entity(name = "fence_section", type = "fence_section", location = chunk_loc), to = self.target())
+                fence_section_ent = Entity(name = "fence_section", type = "fence_section",
+                                           location = chunk_loc)
+                create=Operation("create", fence_section_ent, to = self.target())
                 res.append(create)
 
             if wcount == 0 and lcount == 3:
-                create=Operation("create", Entity(name = "palissade_entry", type = "palissade_entry", location = chunk_loc), to = self.target())
+                palissade_entry_ent = Entity(name = "palissade_entry", type = "palissade_entry",
+                                             location = chunk_loc)
+                create=Operation("create", palissade_entry_ent, to = self.target())
                 res.append(create)
 
         if rcount == 3 :
             if wcount == 1 and lcount == 0:
-                create=Operation("create", Entity(name = "fence_gate", type = "fence_gate", location = chunk_loc), to = self.target())
+                fence_gate_ent = Entity(name = "fence_gate", type = "fence_gate", location = chunk_loc)
+                create=Operation("create", fence_gate_ent, to = self.target())
                 res.append(create)
 
 #            if wcount == 3 and lcount == 0:
@@ -107,7 +118,9 @@ class Pioneering(server.Task):
  #               res.append(create)
 
             if wcount == 0 and lcount == 5:
-                create=Operation("create", Entity(name = "palissade_circle", type = "palissade_circle", location = chunk_loc), to = self.target())
+                palissade_circle_ent = Entity(name = "palissade_circle",
+                                              type = "palissade_circle", location = chunk_loc)
+                create=Operation("create", palissade_circle_ent, to = self.target())
                 res.append(create)
 
         # Consume the materials according to the recipe
