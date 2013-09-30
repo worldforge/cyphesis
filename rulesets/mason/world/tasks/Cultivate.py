f2e683e9c956da53d058ed0a4036bd6570ebbf5e
Jesse <j.horne2796@gmail.com>
2013-09-29 22:46:54 -0400

    fixed a line in Cultivate.py to be shorter

--- rulesets/mason/world/tasks/Cultivate.py
+++ rulesets/mason/world/tasks/Cultivate.py
@@ -34,12 +34,13 @@ class Cultivate(server.Task):
             return
 
         #Measure the distance between the entity horizontal edges. Else we won't be able to reach if either entity is too thick.
-        distance_between_entity_edges_squared = square_horizontal_edge_distance(self.character.location, self.target().location) 
+        entity_edges_dis = square_horizontal_edge_distance(self.character.location,
+                                                           self.target().location) 
         
         #Assume that a standard human can reach 1.5 meters, and use this to determine if we're close enough to be able to perform the logging
         standard_human_reach_squared=1.5*1.5
 
-        if distance_between_entity_edges_squared > standard_human_reach_squared:
+        if entity_edges_dis > standard_human_reach_squared:
             self.progress = 0
             self.rate = 0
             return self.next_tick(1.75)
