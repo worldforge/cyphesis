a9e76b79514d713daa5a6cf1e83b5659b3981d86
Jesse <j.horne2796@gmail.com>
2013-09-29 23:28:00 -0400

    fixed long line with Woodenwall.py

--- rulesets/mason/world/tasks/Woodenwall.py
+++ rulesets/mason/world/tasks/Woodenwall.py
@@ -86,7 +86,8 @@ class Woodenwall(server.Task):
             res.append(set)
             count = count - 1
 
-        create=Operation("create", Entity(name = "board_wall", type = "board_wall", location = chunk_loc), to = target)
+        create=Operation("create", Entity(name = "board_wall", type = "board_wall",
+                                          location = chunk_loc), to = target)
         res.append(create)
         self.progress = 1
         self.irrelevant()
