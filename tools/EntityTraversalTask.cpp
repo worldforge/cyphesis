2188d1ed51bc934ada149d7e4367d44f59796a75
Erik Ogenvik <erik@ogenvik.org>
2013-11-24 13:12:59 +0100

    Added a task for traversing the entity tree.

---
+++ tools/EntityTraversalTask.cpp
@@ -0,0 +1,119 @@
+/*
+ Copyright (C) 2013 Erik Ogenvik
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "EntityTraversalTask.h"
+
+#include "common/serialno.h"
+
+#include <Atlas/Objects/Operation.h>
+#include <Atlas/Objects/Entity.h>
+
+using Atlas::Objects::Root;
+using Atlas::Objects::smart_dynamic_cast;
+using Atlas::Objects::Entity::Anonymous;
+using Atlas::Objects::Entity::RootEntity;
+using Atlas::Objects::Operation::Create;
+using Atlas::Objects::Operation::Get;
+using Atlas::Objects::Operation::Look;
+using Atlas::Objects::Operation::Set;
+using Atlas::Message::Element;
+
+EntityTraversalTask::EntityTraversalTask(const std::string& accountId,
+        std::function<bool(const Atlas::Objects::Entity::RootEntity&)>& visitor) :
+        mAccountId(accountId), mVisitor(visitor), mSerial(0)
+{
+}
+
+EntityTraversalTask::~EntityTraversalTask()
+{
+}
+
+void EntityTraversalTask::setup(const std::string & arg, OpVector & res)
+{
+    Anonymous get_arg;
+    get_arg->setId("0");
+    get_arg->setObjtype("obj");
+
+    Get get;
+    get->setArgs1(get_arg);
+    get->setFrom(mAccountId);
+    get->setSerialno(newSerialNo());
+    mSerial = get->getSerialno();
+    res.push_back(get);
+}
+
+void EntityTraversalTask::operation(const Operation & op, OpVector & res)
+{
+    if (!op->isDefaultRefno() && op->getRefno() == mSerial) {
+        if (op->getClassNo() == Atlas::Objects::Operation::INFO_NO) {
+            if (!op->getArgs().empty()) {
+                auto arg = smart_dynamic_cast<RootEntity>(
+                        op->getArgs().front());
+                if (arg.isValid()) {
+                    bool result = mVisitor(arg);
+                    if (result) {
+                        if (!arg->isDefaultContains()
+                                && !arg->getContains().empty()) {
+                            mStack.emplace_back();
+                            mStack.back().children = arg->getContains();
+                            mStack.back().currentChildIterator =
+                                    mStack.back().children.begin();
+                            getEntity(*mStack.back().currentChildIterator, res);
+                            return;
+                        } else {
+                            while (!mStack.empty()) {
+                                StackEntry& stackEntry = mStack.back();
+                                stackEntry.currentChildIterator++;
+                                if (stackEntry.currentChildIterator
+                                        == stackEntry.children.end()) {
+                                    mStack.pop_back();
+                                } else {
+                                    getEntity(*stackEntry.currentChildIterator,
+                                            res);
+                                    return;
+                                }
+                            }
+                            m_complete = true;
+                        }
+                    } else {
+                        m_complete = true;
+                    }
+                }
+            }
+        }
+    }
+}
+
+void EntityTraversalTask::getEntity(const std::string & id, OpVector & res)
+{
+    Anonymous get_arg;
+    get_arg->setId(id);
+    get_arg->setObjtype("obj");
+
+    Get get;
+    get->setArgs1(get_arg);
+    get->setFrom(mAccountId);
+    get->setSerialno(newSerialNo());
+    mSerial = get->getSerialno();
+    res.push_back(get);
+}
+
---
+++ tools/EntityTraversalTask.h
@@ -0,0 +1,105 @@
+/*
+ Copyright (C) 2013 Erik Ogenvik
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#ifndef ENTITYTRAVERSALTASK_H_
+#define ENTITYTRAVERSALTASK_H_
+
+#include "common/ClientTask.h"
+#include <Atlas/Objects/Entity.h>
+
+#include <functional>
+#include <string>
+
+/**
+ * \brief A client task for traversing the entity tree.
+ *
+ * A function is supplied to the task. When active the task will
+ * walk through the entity hierarchy on the server, calling the supplied
+ * function for every entity it receives, until either all entities have been
+ * visited or it's told to stop.
+ */
+class EntityTraversalTask: public ClientTask
+{
+    public:
+
+        /**
+         * \brief Ctor.
+         *
+         * A function must be supplied. The signature of the function is
+         * bool(const RootEntity&)
+         * The supplied entity is a representation of an entity on the server.
+         * If the function returns true if the traversal should continue.
+         * \param accountId An account id.
+         * \param visitor A visitor function.
+         */
+        EntityTraversalTask(const std::string& accountId,
+                std::function<bool(const Atlas::Objects::Entity::RootEntity&)>& visitor);
+        virtual ~EntityTraversalTask();
+
+        /// \brief Set up the task processing user arguments
+        virtual void setup(const std::string & arg, OpVector &);
+        /// \brief Handle an operation from the server
+        virtual void operation(const Operation &, OpVector &);
+
+    private:
+
+        /**
+         * \brief Represents one entry on the traversal stack.
+         */
+        struct StackEntry
+        {
+                /**
+                 * @brief The ids of the children of the rule.
+                 */
+                std::list<std::string> children;
+                /**
+                 * @brief The current children iterator. This is an iterator of the "children" field.
+                 */
+                std::list<std::string>::const_iterator currentChildIterator;
+        };
+
+        /**
+         * \brief A logged in account id.
+         */
+        const std::string mAccountId;
+
+        /**
+         * \brief A visitor function applied to each entity.
+         */
+        std::function<bool(const Atlas::Objects::Entity::RootEntity&)> mVisitor;
+
+        /**
+         * \brief The last serial number used.
+         */
+        long int mSerial;
+
+        /**
+         * \brief A stack keeping track of the traversal.
+         */
+        std::list<StackEntry> mStack;
+
+        /**
+         * \brief Requests a new entity from the server.
+         * \param id The id of the entity.
+         * \param res
+         */
+        void getEntity(const std::string & id, OpVector & res);
+
+};
+
+#endif /* ENTITYTRAVERSALTASK_H_ */
