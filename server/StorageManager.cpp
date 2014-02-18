0f9ecdc74ef86008532b0617b0d64b3612e73842
Erik Ogenvik <erik@ogenvik.org>
2014-02-18 20:38:23 +0100

    Improve property restoring code.
    Add extra checks to make sure that pre-existing properties aren't
    installed or applied unless the data has changed.

--- server/StorageManager.cpp
+++ server/StorageManager.cpp
@@ -185,28 +185,51 @@ void StorageManager::restoreProperties(LocatedEntity * ent)
         }
         MapType prop_data;
         db->decodeMessage(val_string, prop_data);
-        MapType::iterator J = prop_data.find("val");
+        MapType::const_iterator J = prop_data.find("val");
         if (J == prop_data.end()) {
             log(ERROR, compose("No property value data for %1:%2",
                                ent->getId(), name));
             continue;
         }
-        Element & val = J->second;
-        PropertyBase * prop = ent->modProperty(name);
-        if (prop == 0) {
-            prop = pm->addProperty(name, val.getType());
-            ent->setProperty(name, prop);
-        }
-        prop->set(val);
-        prop->setFlags(per_clean | per_seen);
         const TypeNode * type = ent->getType();
         assert(type != 0);
-        if (type->defaults().find(name) == type->defaults().end()) {
-            prop->install(ent, name);
+        const Element & val = J->second;
+        PropertyBase * prop = ent->modProperty(name);
+        Element existing_val;
+        if (prop == 0) {
+            prop = pm->addProperty(name, val.getType());
+            //If the property didn't exist on the entity, check if the property exists in Type defaults,
+            //and if so if it's the same value (and if that's true skip it)
+            auto defaultsI = type->defaults().find(name);
+            if (defaultsI != type->defaults().end()) {
+                if (defaultsI->second->get(existing_val) == 0) {
+                    if (existing_val == val) {
+                        //Since the property didn't exist on the entity and was created in this method
+                        //we must delete it if we're not using it.
+                        delete prop;
+                        continue;
+                    }
+                }
+            } else {
+                //The property didn't exist neither as an entity property nor a Type entity; we should install it.
+                prop->install(ent, name);
+            }
+            //This transfers ownership of the property to the entity.
+            ent->setProperty(name, prop);
+        } else {
+            //If the property already existed, and had the same data, just skip it.
+            if (prop->get(existing_val) == 0) {
+                if (existing_val == val) {
+                    continue;
+                }
+            }
         }
+
+        //If we get to here the property either doesn't exists, or have a different value than the default or existing property.
+        prop->set(val);
+        prop->setFlags(per_clean | per_seen);
         prop->apply(ent);
     }
-    // Iterate over res and create the property values.
 }
 
 void StorageManager::restoreThoughts(LocatedEntity * ent)
