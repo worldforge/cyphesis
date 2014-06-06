551d520ee311209541a69944c3fbb8fdf5107caf
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2014-06-06 00:26:01 -0400

    Reworked filtering implementation
    Tokens are now parsed deeper into structures depending on
    the case we encounter. After being initialized, these
    structures must be able to test a condition for any
    entity provided.

--- rulesets/Filter.h
+++ rulesets/Filter.h
@@ -6,6 +6,9 @@
 #include <boost/tuple/tuple.hpp>
 #include "MemMap.h"
 
+#include <Atlas/Objects/Operation.h>
+#include <Atlas/Objects/Anonymous.h>
+
 #include <boost/spirit/include/qi.hpp>
 #include <boost/fusion/include/adapt_struct.hpp>
 #include <boost/fusion/include/io.hpp>
@@ -26,7 +29,6 @@ namespace parser
 
     struct condition
     {
-        std::string target;
         std::string attribute;
         std::string comp_operator;
         std::string value_str;
@@ -35,7 +37,6 @@ namespace parser
 
 BOOST_FUSION_ADAPT_STRUCT(
     parser::condition,
-    (std::string, target)
     (std::string, attribute)
     (std::string, comp_operator)
     (std::string, value_str)
@@ -75,7 +76,7 @@ namespace parser
             value_g %= +(char_ - logical_operator_g);
             // ">>" operator stands for "followed by".  a >> b matches if b follows a.
             token_g %=
-                    target_g >> "." >> attribute_g >> comp_operator_g >> value_g;
+                    attribute_g >> comp_operator_g >> value_g;
             // a%b matches a list of a separated by occurances of b and has attribute of vector<a>.
             start %= token_g % "&";
         }
@@ -89,9 +90,131 @@ namespace parser
         qi::rule<Iterator, condition(), ascii::space_type> token_g;
         qi::rule<Iterator, std::list<condition>(), ascii::space_type> start;
     };
+    //Additional rules to determine and parse the subject
+
+    //grammar for Type of entity
+    //This rule will match if we use "entity.type" token
+    qi::rule<std::string::iterator, ascii::space_type> type_subject_g =
+            (qi::lit("Entity") | qi::lit("entity")) >> (qi::lit("type") | qi::lit("Type"));
+
+    //grammar for Attribute of an entity
+    //This rule will match if we use "entity.attribute" token
+    qi::rule<std::string::iterator, std::string(), ascii::space_type> attribute_subject_g =
+            (qi::lit("Entity") | "entity") >> +(qi::char_ - "=" - "<" - ">" - "!");
 }
 
-class Filter{
+//Float comparers
+bool eqComparer(float val1, float val2)
+{
+    return val1 == val2;
+}
+bool neqComparer(float val1, float val2)
+{
+    return val1 != val2;
+}
+bool gComparer(float val1, float val2)
+{
+    return val1 > val2;
+}
+bool geComparer(float val1, float val2)
+{
+    return val1 >= val2;
+}
+bool lComparer(float val1, float val2)
+{
+    return val1 < val2;
+}
+bool leComparer(float val1, float val2)
+{
+    return val1 <= val2;
+}
+
+//Attribute comparers
+class AttributeComparerWrapper{
+    public:
+        virtual bool compare(LocatedEntity* entity);
+        virtual ~AttributeComparerWrapper() {}
+};
+class StringAttributeComparer : public AttributeComparerWrapper {
+    public:
+        StringAttributeComparer(const std::string &attribute_name,
+                                const std::string &value_str) :
+                m_value(value_str), m_attributeName(attribute_name)
+        {
+        }
+        ;
+        //TODO: Implement compare method
+        bool compare(LocatedEntity* entity);
+    private:
+        std::string m_value;
+        std::string m_attributeName;
+};
+//A class used to compare numeric attributes
+class NumericAttributeComparer : public AttributeComparerWrapper {
+    public:
+        NumericAttributeComparer(const std::string &attribute_name,
+                                 const std::string &value_str,
+                                 const std::string &comp_operator);
+        bool compare(LocatedEntity* entity);
+    private:
+        //a pointer to the comparer method with the right comparison operator
+        bool (*comparer_method)(float, float);
+        std::string m_attributeName;
+        float m_value;
+        Atlas::Message::Element m_atlasAttr;
+
+};
+
+class ListAttributeComparer: public AttributeComparerWrapper{
+
+};
+
+
+//Classes to handle various cases of subjects.
+class AbstractCase{
+    public:
+        virtual bool testCase(LocatedEntity* entity);
+        virtual ~AbstractCase() {}
+};
+// a class to handle "entity.attribute" query
+class EntityAttributeCase: public AbstractCase{
+    public:
+        EntityAttributeCase(const std::string &attribute, const std::string &value, const std::string &comp_operator);
+        bool testCase(LocatedEntity* entity);
+    private:
+        AttributeComparerWrapper* m_comparer;
+        std::string m_attributeName;
+        std::string m_valueStr;
+
+};
+
+//A class to handle "entity.type" query
+class EntityTypeCase: public AbstractCase{
+    public:
+        EntityTypeCase(const std::string &value) :
+                m_valueStr(value)
+        {
+        }
+        ;
+        bool testCase(LocatedEntity* entity);
+    private:
+        std::string m_valueStr;
+};
+
+
+//This class is meant to further parse tokens into appropriate structures.
+class ParsedCondition{
+    public:
+        ParsedCondition(const parser::condition &all_conditions);
+
+        bool isTrue(LocatedEntity* entity);
+    private:
+        AbstractCase* m_case;
+};
+
+
+///Main Filtering class
+class Filter {
     public:
         ///\brief Initialize a filter with a given query
         ///@param what query to be used for filtering
@@ -104,28 +227,24 @@ class Filter{
         void search(MemEntityDict all_entities, EntityVector &res);
 
         //\brief Return entities that pass all conditions.
-        void attributeSearchAnd(const MemEntityDict &all_entities, EntityVector& res);
+        void attributeSearchAnd(const MemEntityDict &all_entities,
+                                EntityVector& res);
         //\brief Return Entities that pass at least one condition
-        void attributeSearchOr(const MemEntityDict &all_entities, EntityVector& res);
+        void attributeSearchOr(const MemEntityDict &all_entities,
+                               EntityVector& res);
     private:
         std::list<parser::condition> m_conditions;
+        std::list<ParsedCondition> m_parsedConditions;
         void recordCondition(const std::string &token,
                              const std::string &comp_operator,
                              int delimiter_index);
         bool testAttributeMatch(LocatedEntity *entity,
-                            const std::string &attribute_name,
-                            const std::string &attribute_value,
-                            const std::string &comp_operator);
-
-        template <typename T>
-        bool compare(T val1, T val2, std::string comp_operator){
-            if (comp_operator== "=") return val1 == val2;
-            if (comp_operator== "!=") return val1 != val2;
-            if (comp_operator== ">") return val1 > val2;
-            if (comp_operator== "<") return val1 < val2;
-            if (comp_operator== ">=") return val1 >= val2;
-            if (comp_operator== "<=") return val1 <= val2;
-        }
+                                const std::string &attribute_name,
+                                const std::string &attribute_value,
+                                const std::string &comp_operator);
 
 };
+
+
+
 #endif
