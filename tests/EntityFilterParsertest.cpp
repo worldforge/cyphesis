9cd5bc973030f655c2ad1affcdcbe4e0a91d79c4
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-05-05 10:15:13 -0700

    Added parser and provider test source files

---
+++ tests/EntityFilterParsertest.cpp
@@ -0,0 +1,38 @@
+#ifdef NDEBUG
+#undef NDEBUG
+#endif
+#ifndef DEBUG
+#define DEBUG
+#endif
+
+#include "TestBase.h"
+
+
+
+class ParserTest : public Cyphesis::TestBase
+{
+  private:
+
+  public:
+        ParserTest();
+
+    void setup();
+    void teardown();
+
+};
+
+ParserTest::ParserTest()
+{
+
+}
+
+void ParserTest::setup()
+{
+
+}
+
+void ParserTest::teardown()
+{
+
+}
+
---
+++ tests/EntityFilterProviderstest.cpp
@@ -0,0 +1,38 @@
+#ifdef NDEBUG
+#undef NDEBUG
+#endif
+#ifndef DEBUG
+#define DEBUG
+#endif
+
+#include "TestBase.h"
+
+
+
+class ProvidersTest : public Cyphesis::TestBase
+{
+  private:
+
+  public:
+    ProvidersTest();
+
+    void setup();
+    void teardown();
+
+};
+
+ProvidersTest::ProvidersTest()
+{
+
+}
+
+void ProvidersTest::setup()
+{
+
+}
+
+void ProvidersTest::teardown()
+{
+
+}
+
