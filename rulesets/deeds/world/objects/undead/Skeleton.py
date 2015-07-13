39ef952b386903030a65b03c98b5de9427a8c64d
Erik Ogenvik <erik@ogenvik.org>
2015-07-13 22:16:50 +0200

    Don't change status too early.
    Doing it too early will trigger a deletion of the skeleton, which makes
    the bones creation ops fail since the entity has been deleted.

--- rulesets/deeds/world/objects/undead/Skeleton.py
+++ rulesets/deeds/world/objects/undead/Skeleton.py
@@ -12,12 +12,11 @@ class Skeleton(server.Thing):
     def touch_operation(self, op):
         retops = Oplist()
         if self.status<0: return
-        self.status=-1
         newloc=self.location.copy()
         newloc.velocity=Vector3D()
         retops = retops + Operation("move", Entity(self.id, location=newloc.copy(), mode="collapsed"), to=self)
         for item in ['skull', 'ribcage', 'femur', 'pelvis', 'tibia']:
             newloc.coordinates = newloc.coordinates + Vector3D(uniform(-1,1), uniform(-1,1), uniform(-1,1))
             retops = retops + Operation("create", Entity(name=item,parents=[item],location=newloc.copy()), to=self)
-        retops = retops + Operation("set", Entity(self.id, status=self.status), to=self)
+        retops = retops + Operation("set", Entity(self.id, status=-1), to=self)
         return retops
