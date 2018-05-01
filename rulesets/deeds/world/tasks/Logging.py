49b0209b3f460a7d5b3e3fd71ccdebf327dfbe98
Erik Ogenvik <erik@ogenvik.org>
2018-05-01 14:22:37 +0200

    Fixed some issues with logging.

--- rulesets/deeds/world/tasks/Logging.py
+++ rulesets/deeds/world/tasks/Logging.py
@@ -36,11 +36,12 @@ class Logging(server.Task):
         current_status = self.target().status
 
         #Measure the distance between the entity horizontal edges. Else we won't be able to reach if either entity is too thick.
-        distance_between_entity_edges_squared = square_horizontal_edge_distance(self.character.location, self.target().location) 
-        
+        distance_between_entity_edges_squared = square_horizontal_edge_distance(self.character.location, self.target().location)
+
         #Assume that a standard human can reach 1.5 meters, and use this to determine if we're close enough to be able to perform the logging
         standard_human_reach_squared=1.5*1.5
 
+        #TODO: replace this with asking the domain about the entities' reach
         if distance_between_entity_edges_squared > standard_human_reach_squared:
             self.progress = 1 - current_status
             self.rate = 0
@@ -50,30 +51,21 @@ class Logging(server.Task):
         if current_status > 0.11:
             set=Operation("set", Entity(self.target().id, status=current_status-0.1), to=self.target())
             res.append(set)
-            # print "CHOP",current_status
 
-            normal=Vector3D(0, 1, 0)
-            # print "LOC.ori ", self.target().location.orientation
-            # calculate how tilted the tree is already
-            if self.target().location.orientation.is_valid():
-                normal.rotate(self.target().location.orientation)
-            # print "Normal ", normal, normal.dot(Vector3D(0,0,1))
-            # if the tree is standing, and it's already half cut down, rotate
-            # it to be horizontal, away from the character
-            if normal.dot(Vector3D(0, 1, 0)) > 0.8 and current_status < 0.5:
-                # print "Fall down"
-                # determine the axis of rotation by cross product of the vector
-                # from character to tree, and vertically upward vector
+            if current_status <= 0.5 and self.target().mode != 'free':
+                print "Timber!"
+
+                #Make it fall initially 15 degrees away from the logger entity.
                 axis = distance_to(self.character.location,
                                    self.target().location).cross(Vector3D(0, 1, 0))
                 # the axis must be a unit vector
                 try:
                     axis = axis.unit_vector()
                 except ZeroDivisionError:
-                    axis = Vector3D(1,0,0)
+                    axis = Vector3D(1, 0, 0)
                 # print "axis ", axis
-                # create a rotation of 90 degrees around this axis
-                orient = Quaternion(axis, math.pi / -2.0)
+                # create a rotation of 15 degrees around this axis
+                orient = Quaternion(axis, math.pi / -12.0)
 
                 # if the tree is rotated, apply this too
                 if self.target().location.orientation.is_valid():
@@ -82,9 +74,10 @@ class Logging(server.Task):
                 move_location = self.target().location.copy()
                 move_location.orientation = orient
 
-                move = Operation("move", Entity(self.target().id, mode='felled',
+                move = Operation("move", Entity(self.target().id, mode='free',
                                                 location=move_location),
-                                 to = self.target())
+                                                to = self.target())
+
                 res.append(move)
         else:
             # print "become log"
@@ -95,15 +88,16 @@ class Logging(server.Task):
             create_loc.orientation = self.target().location.orientation
             create = Operation("create",
                                Entity(parent="lumber",
-                                      mass = self.target().mass, 
+                                      mass = self.target().mass,
                                       location = create_loc,
-                                      bbox = self.target().bbox),
+                                      #bbox = self.target().bbox,
+                                      mode = "free"),
                                to = self.target())
             res.append(create)
-                                            
+
         self.progress = 1 - current_status
         self.rate = 0.1 / 1.75
-        
+
         res.append(self.next_tick(1.75))
 
         return res
