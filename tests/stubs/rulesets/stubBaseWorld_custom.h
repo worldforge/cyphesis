a5b0afd34337ee6bc75979b5ee9759d2f48fe78a
Erik Ogenvik <erik@ogenvik.org>
2018-07-25 21:41:23 +0200

    Fixed stubs.

--- tests/stubs/rulesets/stubBaseWorld_custom.h
+++ tests/stubs/rulesets/stubBaseWorld_custom.h
@@ -15,28 +15,14 @@ BaseWorld::BaseWorld()
 #define STUB_BaseWorld_getEntity
 
 LocatedEntity *BaseWorld::getEntity(const std::string &id) const {
-    long intId = strtol(id.c_str(), 0, 10);
-    if (intId == 0 && id != "0") {
-        intId = -1L;
-    }
-    EntityDict::const_iterator I = m_eobjects.find(intId);
-    if (I != m_eobjects.end()) {
-        assert(I->second != 0);
-        return I->second;
-    } else {
-        return 0;
-    }
+    return getEntity(strtol(id.c_str(), 0, 10));
 }
 
-#endif //STUB_BaseWorld_getEntity
-
-#ifndef STUB_BaseWorld_getEntity
-#define STUB_BaseWorld_getEntity
 LocatedEntity* BaseWorld::getEntity(long id) const
 {
-    EntityDict::const_iterator I = m_eobjects.find(id);
+    auto I = m_eobjects.find(id);
     if (I != m_eobjects.end()) {
-        assert(I->second != 0);
+        assert(I->second);
         return I->second;
     } else {
         return 0;
