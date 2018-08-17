914826fcd710be776603dba71ab44e364358ff32
Erik Ogenvik <erik@ogenvik.org>
2018-08-17 22:21:56 +0200

    Don't block updates.

--- rulesets/deeds/scripts/world/traits/StatusDeletable.py
+++ rulesets/deeds/scripts/world/traits/StatusDeletable.py
@@ -6,6 +6,6 @@ class StatusDeletable(server.Thing):
 
     def update_operation(self, op):
         if self.props.status and self.props.status <= 0:
-            return (server.OPERATION_BLOCKED, Operation("delete", Entity(self.id), to=self))
+            return (server.OPERATION_HANDLED, Operation("delete", Entity(self.id), to=self))
 
-        return (server.OPERATION_BLOCKED)
+        return (server.OPERATION_HANDLED)
