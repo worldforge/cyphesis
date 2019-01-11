a47a016269b67c6a6b6364fc8be0ede7d1f93644
Erik Ogenvik <erik@ogenvik.org>
2019-01-11 21:56:55 +0100

    Remove unused ProxyMind.

--- tests/stubs/rules/simulation/stubProxyMind_custom.h
+++
@@ -1,9 +0,0 @@
-//Add custom implementations of stubbed functions here; this file won't be rewritten when re-generating stubs.
-#ifndef STUB_ProxyMind_ProxyMind
-#define STUB_ProxyMind_ProxyMind
-ProxyMind::ProxyMind(const std::string & id, long intId, LocatedEntity& ownerEntity)
-    : m_ownerEntity(ownerEntity)
-{
-
-}
-#endif //STUB_ProxyMind_ProxyMind
