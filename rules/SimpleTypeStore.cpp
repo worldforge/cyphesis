f037683ee1cac4ce30102a275a99a18536a2203b
Erik Ogenvik <erik@ogenvik.org>
2019-04-04 16:27:31 +0200

    Handle inherited properties.

--- rules/SimpleTypeStore.cpp
+++ rules/SimpleTypeStore.cpp
@@ -39,14 +39,27 @@ const TypeNode* SimpleTypeStore::getType(const std::string& parent) const
 TypeNode* SimpleTypeStore::addChild(const Atlas::Objects::Root& obj)
 {
     assert(obj.isValid());
+    TypeNode* parentNode = nullptr;
+
     const std::string& child = obj->getId();
     std::string parent;
     if (!obj->isDefaultParent()) {
         parent = obj->getParent();
     }
+
+    if (!parent.empty()) {
+        auto I = m_types.find(parent);
+        if (I == m_types.end()) {
+            log(ERROR, String::compose("Installing %1 \"%2\" "
+                                       "which has unknown parent \"%3\".",
+                                       obj->getObjtype(), child, parent));;
+            return nullptr;
+        }
+        parentNode = I->second.get();
+    }
+
     auto I = m_types.find(child);
-    auto Iend = m_types.end();
-    if (I != Iend) {
+    if (I != m_types.end()) {
 
         const TypeNode* existingParent = I->second->parent();
         log(ERROR, String::compose("Installing %1 \"%2\"(parent \"%3\") "
@@ -58,28 +71,29 @@ TypeNode* SimpleTypeStore::addChild(const Atlas::Objects::Root& obj)
     }
     auto type = std::make_unique<TypeNode>(child, obj);
 
+    //First add all properties from parent type.
+    if (parentNode) {
+        for (auto& entry : parentNode->defaults()) {
+            type->injectProperty(entry.first, entry.second->copy());
+        }
+    }
+
     readAttributesIntoType(*type, obj);
 
-    if (!parent.empty()) {
-        I = m_types.find(parent);
-        if (I == Iend) {
-            log(ERROR, String::compose("Installing %1 \"%2\" "
-                                       "which has unknown parent \"%3\".",
-                                       obj->getObjtype(), child, parent));;
-            return nullptr;
-        }
+    if (parentNode) {
+
         Atlas::Message::Element children(Atlas::Message::ListType(1, child));
 
-        auto description = I->second->description(Visibility::PRIVATE);
+        auto description = parentNode->description(Visibility::PRIVATE);
 
         if (description->copyAttr("children", children) == 0) {
             assert(children.isList());
             children.asList().push_back(child);
         }
         description->setAttr("children", children);
-        I->second->setDescription(description);
+        parentNode->setDescription(description);
 
-        type->setParent(I->second.get());
+        type->setParent(parentNode);
     }
 
 
