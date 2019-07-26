f87e5818eda23b63f0b249feeb4798d66873fa8f
Erik Ogenvik <erik@ogenvik.org>
2019-07-26 17:04:57 +0200

    Make "__decays" a private prop.

--- rulesets/deeds/scripts/world/traits/DecaysInto.py
+++ rulesets/deeds/scripts/world/traits/DecaysInto.py
@@ -6,9 +6,9 @@ import server
 class DecaysInto(server.Thing):
 
     def delete_operation(self, op):
-        decays_prop = self.props._decays
+        decays_prop = self.props["__decays"]
         if not decays_prop:
-            print("DecaysInto placed on entity without '_decays' prop")
+            print("DecaysInto placed on entity without '__decays' prop")
             return server.OPERATION_IGNORED
         op_list = Oplist()
         # Only spawn if there's a parent
