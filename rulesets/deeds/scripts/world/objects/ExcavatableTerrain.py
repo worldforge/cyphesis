18321dbb26dd904d530b93596b8ca575b25796ea
Erik Ogenvik <erik@ogenvik.org>
2019-08-04 20:51:27 +0200

    Implement excavatable terrain.
    Replacing the "Delve" task.

---
+++ rulesets/deeds/scripts/world/objects/ExcavatableTerrain.py
@@ -0,0 +1,83 @@
+# This file is distributed under the terms of the GNU General Public license.
+# Copyright (C) 2006 Al Riddoch (See the file COPYING for details).
+
+from atlas import Operation, Entity, Oplist
+from rules import Location
+import random
+import server
+
+
+class ExcavatableTerrain(server.Thing):
+    """
+    Applied on terrain which can be excavated.
+    """
+
+    materials = {'rock': 'granite_a', 'snow': 'ice'}
+
+    def excavate_operation(self, op):
+
+        res = Oplist()
+        arg = op[0]
+        if not arg:
+            return server.OPERATION_IGNORED
+
+        if not arg.pos:
+            print('No pos supplied')
+            return server.OPERATION_IGNORED
+
+        terrain_prop = self.props.terrain
+        if not terrain_prop:
+            print('No terrain prop on excavatable terrain entity')
+            return server.OPERATION_IGNORED
+
+        chunk_loc = Location(self, arg.pos)
+
+        surface = terrain_prop.get_surface_name(arg.pos[0], arg.pos[2])
+        if surface not in ExcavatableTerrain.materials:
+            print("Not rock")
+            return
+        material = ExcavatableTerrain.materials[surface]
+
+        mods = terrain_prop.find_mods(arg.pos[0], arg.pos[2])
+        if len(mods) == 0:
+            # There is no terrain mod where we are digging,
+            # create a quarry
+
+            modmap = {
+                'heightoffset': -1,
+                'shape': {
+                    'points': [[-1.0, -1.0],
+                               [-1.0, 1.0],
+                               [1.0, 1.0],
+                               [1.0, -1.0]],
+                    'type': 'polygon'
+                },
+                'type': 'levelmod'
+            }
+            quarry_create = Operation("create",
+                                      Entity(name="quarry",
+                                             parent="path",
+                                             location=chunk_loc,
+                                             terrainmod=modmap),
+                                      to=self)
+            res.append(quarry_create)
+        else:
+            print(mods)
+            for mod in mods:
+                if not hasattr(mod, 'name') or mod.name != 'quarry':
+                    print("%s is no good" % mod.id)
+                    continue
+                print("%s looks good" % mod.id)
+                print(mod.terrainmod)
+                mod.terrainmod.heightoffset -= 1.0
+                # We have modified the attribute in place, so must send an update op to propagate
+                res.append(Operation("update", to=mod.id))
+                break
+
+        create = Operation("create",
+                           Entity(name=material,
+                                  parent=material,
+                                  location=chunk_loc), to=self)
+        res.append(create)
+
+        return res
--- rulesets/deeds/scripts/world/traits/Nourishable.py
+++ rulesets/deeds/scripts/world/traits/Nourishable.py
@@ -8,11 +8,10 @@ class Nourishable(server.Thing):
     def nourish_operation(self, op):
         # Get the mass of the contained arg, convert it to nutrient through _modifier_eat* properties, and increase the "_nutrients" property.
         # Check any limits on the amount of nutrient we can contain in our stomach/reserves through the _nutrients_max prop too.
-        print('nourish')
         if len(op) > 0:
             arg = op[0]
             if hasattr(arg, 'mass'):
-                print('mass {}'.format(arg.mass))
+                # print('mass {}'.format(arg.mass))
                 # Check if we can convert to nutrient through the _modifier_eat property.
                 # We also check if there are specific values for herbivores and omnivores (_modifier_consume_type_meat and _modifier_consume_type_plant)
                 consume_factor = 0
@@ -22,7 +21,7 @@ class Nourishable(server.Thing):
                     if self.props["_modifier_consume_type_" + arg.consume_type]:
                         consume_factor = self.props["_modifier_consume_type_" + arg.consume_type]
 
-                print("consume factor {}".format(consume_factor))
+                # print("consume factor {}".format(consume_factor))
                 if consume_factor != 0:
                     nutrient = 0
                     if self.props._nutrients:
