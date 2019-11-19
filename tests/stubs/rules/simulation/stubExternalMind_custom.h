f1de67510e3490bc25da864db1df36958c7ffd6e
Erik Ogenvik <erik@ogenvik.org>
2019-11-19 21:16:27 +0100

    Work with older compilers.

--- tests/stubs/rules/simulation/stubExternalMind_custom.h
+++ tests/stubs/rules/simulation/stubExternalMind_custom.h
@@ -1,4 +1,5 @@
 //Add custom implementations of stubbed functions here; this file won't be rewritten when re-generating stubs.
+#include "rules/LocatedEntity.h"
 #ifndef STUB_ExternalMind_ExternalMind
 #define STUB_ExternalMind_ExternalMind
 ExternalMind::ExternalMind(const std::string& strId, long id, Ref<LocatedEntity> entity)
