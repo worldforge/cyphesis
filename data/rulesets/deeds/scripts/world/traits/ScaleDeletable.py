ce089cb584658db7175226a71afb46e88d0e9b07
Erik Ogenvik <erik@ogenvik.org>
2019-12-05 00:07:51 +0100

    Use property update mechanism.

--- data/rulesets/deeds/scripts/world/traits/ScaleDeletable.py
+++ data/rulesets/deeds/scripts/world/traits/ScaleDeletable.py
@@ -5,9 +5,7 @@ from atlas import Operation, Entity
 # When scale is set to <= 0 the entity is deleted
 class ScaleDeletable(server.Thing):
 
-    def update_operation(self, op):
+    def status_property_update(self):
         scale_prop = self.props.scale
         if scale_prop and (scale_prop[0] <= 0 or scale_prop[1] <= 0 or scale_prop[2] <= 0):
-            return server.OPERATION_HANDLED, Operation("delete", Entity(self.id), to=self)
-
-        return server.OPERATION_HANDLED
+            return Operation("delete", Entity(self.id), to=self)
--- data/rulesets/deeds/scripts/world/traits/StatusDeletable.py
+++ data/rulesets/deeds/scripts/world/traits/StatusDeletable.py
@@ -5,8 +5,6 @@ from atlas import Operation, Entity
 # When status is set to <= 0 the entity is deleted
 class StatusDeletable(server.Thing):
 
-    def update_operation(self, op):
-        if self.has_prop_float('status') and self.get_prop_float('status') <= 0:
-            return server.OPERATION_HANDLED, Operation("delete", Entity(self.id), to=self)
-
-        return server.OPERATION_HANDLED
+    def status_property_update(self):
+        if self.get_prop_float('status') <= 0:
+            return Operation("delete", Entity(self.id), to=self)
