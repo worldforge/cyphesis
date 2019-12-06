8a5d773bef82b92316ebb34de460f6ed1ae06ad1
Erik Ogenvik <erik@ogenvik.org>
2019-12-06 11:49:08 +0100

    Fixed error where script property wasn't cloned properly.

--- src/rules/python/ScriptsProperty.cpp
+++ src/rules/python/ScriptsProperty.cpp
@@ -17,16 +17,17 @@ using String::compose;
 
 std::map<std::pair<std::string, std::string>, std::shared_ptr<ScriptKit<LocatedEntity>>> ScriptsProperty::sScriptFactories;
 
-void ScriptsProperty::set(const Atlas::Message::Element &element) {
+void ScriptsProperty::set(const Atlas::Message::Element& element)
+{
     Property::set(element);
 
     m_scripts.clear();
-    for (auto &entry : m_data) {
+    for (auto& entry : m_data) {
         if (entry.isMap()) {
             std::string script_package;
             std::string script_class;
             if (Script::getScriptDetails(entry.Map(), "<none>", "Entity",
-                                              script_package, script_class) == 0) {
+                                         script_package, script_class) == 0) {
                 auto key = std::make_pair(script_package, script_class);
                 auto I = sScriptFactories.find(key);
                 if (I != sScriptFactories.end()) {
@@ -46,27 +47,36 @@ void ScriptsProperty::set(const Atlas::Message::Element &element) {
     }
 }
 
-void ScriptsProperty::apply(LocatedEntity *entity) {
+void ScriptsProperty::apply(LocatedEntity* entity)
+{
 
     applyScripts(entity);
 }
 
-void ScriptsProperty::applyScripts(LocatedEntity *entity) const {
+void ScriptsProperty::applyScripts(LocatedEntity* entity) const
+{
 
     entity->m_scripts.clear();
-    for (auto &scriptFactory : m_scripts) {
+    for (auto& scriptFactory : m_scripts) {
         scriptFactory->addScript(entity);
     }
 }
 
-void ScriptsProperty::reloadAllScriptFactories() {
+void ScriptsProperty::reloadAllScriptFactories()
+{
     for (auto& entry : sScriptFactories) {
         entry.second->refreshClass();
     }
 }
 
-void ScriptsProperty::remove(LocatedEntity *entity, const std::string & name) {
+void ScriptsProperty::remove(LocatedEntity* entity, const std::string& name)
+{
     entity->m_scripts.clear();
+}
+
+ScriptsProperty* ScriptsProperty::copy() const
+{
+    return new ScriptsProperty(*this);
 }
 
 
--- src/rules/python/ScriptsProperty.h
+++ src/rules/python/ScriptsProperty.h
@@ -14,34 +14,37 @@
 /**
  * Specifies a list of scripts to be used by the entity.
  */
-class ScriptsProperty : public Property<Atlas::Message::ListType> {
-public:
+class ScriptsProperty : public Property<Atlas::Message::ListType>
+{
+    public:
 
-    /**
-     * Cache of script factories.
-     */
-    static std::map<std::pair<std::string, std::string>, std::shared_ptr<ScriptKit<LocatedEntity>>> sScriptFactories;
+        /**
+         * Cache of script factories.
+         */
+        static std::map<std::pair<std::string, std::string>, std::shared_ptr<ScriptKit<LocatedEntity>>> sScriptFactories;
 
-    /**
-     * Reloads all script factories (i.e. reload the scripts from disk).
-     */
-    static void reloadAllScriptFactories();
+        /**
+         * Reloads all script factories (i.e. reload the scripts from disk).
+         */
+        static void reloadAllScriptFactories();
 
-    void set(const Atlas::Message::Element &) override;
+        void set(const Atlas::Message::Element&) override;
 
-    void apply(LocatedEntity *) override;
+        void apply(LocatedEntity*) override;
 
-    void remove(LocatedEntity *, const std::string & name) override;
+        void remove(LocatedEntity*, const std::string& name) override;
 
-    /**
-     * Applies the scripts to the entity.
-     */
-    void applyScripts(LocatedEntity * ) const;
+        /**
+         * Applies the scripts to the entity.
+         */
+        void applyScripts(LocatedEntity*) const;
+
+        ScriptsProperty* copy() const override;
 
 
-protected:
+    protected:
 
-    std::vector<std::shared_ptr<ScriptKit<LocatedEntity>>> m_scripts;
+        std::vector<std::shared_ptr<ScriptKit<LocatedEntity>>> m_scripts;
 
 };
 
