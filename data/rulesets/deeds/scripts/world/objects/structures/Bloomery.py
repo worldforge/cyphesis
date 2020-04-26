41b0da71abdeeea23b89bada6e0842e6628e229f
Erik Ogenvik <erik@ogenvik.org>
2020-04-26 22:07:59 +0200

    Allow refining hematite into iron ingots.
    By using a bloomery and charcoal.

---
+++ data/rulesets/deeds/scripts/world/objects/structures/Bloomery.py
@@ -0,0 +1,45 @@
+import entity_filter
+import server
+from atlas import Operation, Entity, Oplist
+
+from world.StoppableTask import StoppableTask
+
+charcoal_filter = entity_filter.Filter("entity instance_of types.charcoal")
+hematite_filter = entity_filter.Filter("entity instance_of types.hematite")
+
+
+def craft(instance):
+    task = Smelt(instance, duration=5, tick_interval=1, name="Smelting")
+
+    instance.actor.send_world(Operation("sight", instance.op))
+
+    return server.OPERATION_BLOCKED, instance.actor.start_task("smelt", task)
+
+
+class Smelt(StoppableTask):
+    """ Smelting iron from iron ore """
+
+    def setup(self, task_id):
+        """ Setup code, could do something """
+        pass
+
+    def tick(self):
+        (valid, err) = self.usage.is_valid()
+        if not valid:
+            return self.irrelevant(err)
+
+    def completed(self):
+        res = Oplist()
+        # Find one charcoal and one hematite amongst the children and delete them, and create one iron ingot.
+        hematites = self.usage.tool.find_in_contains(hematite_filter)
+        charcoals = self.usage.tool.find_in_contains(charcoal_filter)
+        if len(hematites) and len(charcoals):
+            res.append(Operation("delete", Entity(hematites[0].id), to=hematites[0]))
+            res.append(Operation("delete", Entity(charcoals[0].id), to=charcoals[0]))
+
+            new_entity = Entity(parent="iron_ingot",
+                                loc=self.usage.tool.id)
+            res.append(Operation("create", new_entity, to=self.usage.tool.id))
+            res.append(Operation("imaginary", Entity(description="You successfully refine the ore."),
+                                 to=self.usage.actor.id, from_=self.usage.actor.id))
+            return res
