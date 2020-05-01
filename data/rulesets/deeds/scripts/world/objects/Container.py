6a677c14dca2468d708a4fa32e9480eccaf2a9c3
Erik Ogenvik <erik@ogenvik.org>
2020-05-01 16:40:25 +0200

    Initial work on allowing easy inspection of containers.

--- data/rulesets/deeds/scripts/world/objects/Container.py
+++ data/rulesets/deeds/scripts/world/objects/Container.py
@@ -1,9 +1,12 @@
 # This file is distributed under the terms of the GNU General Public license.
 # Copyright (C) 2019 Erik Ogenvik (See the file COPYING for details).
 
-from world.utils import Usage
-
 import server
+from atlas import Operation
+
+
+def view(instance):
+    return server.OPERATION_BLOCKED, Operation("look", to=instance.tool, from_=instance.actor),
 
 
 def open_container(instance):
