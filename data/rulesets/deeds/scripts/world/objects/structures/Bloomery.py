9214e14fd92328176f4f495503c86785a2992852
Erik Ogenvik <erik@ogenvik.org>
2020-05-23 23:56:57 +0200

    Allow for smelting of copper too.

--- data/rulesets/deeds/scripts/world/objects/structures/Bloomery.py
+++ data/rulesets/deeds/scripts/world/objects/structures/Bloomery.py
@@ -7,7 +7,8 @@ from world.StoppableTask import StoppableTask
 # Allow both charcoal and lumber to feed the fire. Charcoal is preferable since it weights less
 # (in the future we could also only allow charcoal, on the basis that it allows higher temperatures).
 charcoal_filter = entity_filter.Filter("entity instance_of types.charcoal or entity instance_of types.lumber")
-hematite_filter = entity_filter.Filter("entity instance_of types.hematite")
+ores = {"iron_ingot": entity_filter.Filter("entity instance_of types.hematite"),
+        "copper_ingot": entity_filter.Filter("entity instance_of types.copper_ore")}
 
 
 def craft(instance):
@@ -32,16 +33,19 @@ class Smelt(StoppableTask):
 
     def completed(self):
         res = Oplist()
-        # Find one charcoal and one hematite amongst the children and delete them, and create one iron ingot.
-        hematites = self.usage.tool.find_in_contains(hematite_filter)
+        # Find one charcoal and one ore amongst the children and delete them, and create one iron ingot.
         charcoals = self.usage.tool.find_in_contains(charcoal_filter)
-        if len(hematites) and len(charcoals):
-            res.append(Operation("delete", Entity(hematites[0].id), to=hematites[0]))
-            res.append(Operation("delete", Entity(charcoals[0].id), to=charcoals[0]))
+        if len(charcoals):
+            # We have charcoal, lets see if we also have something to smelt. We'll go through the map of ores to find a match.
+            for key, value in ores.items():
+                inputs = self.usage.tool.find_in_contains(value)
+                if len(inputs) > 0:
+                    res.append(Operation("delete", Entity(inputs[0].id), to=inputs[0]))
+                    res.append(Operation("delete", Entity(charcoals[0].id), to=charcoals[0]))
 
-            new_entity = Entity(parent="iron_ingot",
-                                loc=self.usage.tool.id)
-            res.append(Operation("create", new_entity, to=self.usage.tool.id))
-            res.append(Operation("imaginary", Entity(description="You successfully refine the ore."),
-                                 to=self.usage.actor.id, from_=self.usage.actor.id))
-            return res
+                    new_entity = Entity(parent=key, loc=self.usage.tool.id)
+                    res.append(Operation("create", new_entity, to=self.usage.tool.id))
+                    res.append(Operation("imaginary", Entity(description="You successfully refine the ore."),
+                                         to=self.usage.actor.id, from_=self.usage.actor.id))
+
+        return res
