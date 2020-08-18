f91416e3abc14ff0d930f000cfdbd12533b96dc5
Erik Ogenvik <erik@ogenvik.org>
2020-08-18 23:31:18 +0200

    Listen to correct property.

--- data/rulesets/deeds/scripts/world/traits/ScaleDeletable.py
+++ data/rulesets/deeds/scripts/world/traits/ScaleDeletable.py
@@ -5,7 +5,7 @@ from atlas import Operation, Entity
 # When scale is set to <= 0 the entity is deleted
 class ScaleDeletable(server.Thing):
 
-    def status_property_update(self):
+    def scale_property_update(self):
         scale_prop = self.props.scale
         if scale_prop and (scale_prop[0] <= 0 or scale_prop[1] <= 0 or scale_prop[2] <= 0):
             return Operation("delete", Entity(self.id), to=self)
