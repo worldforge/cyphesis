b56cf25bd95ad9c457dd8e09e4f51845d6c7427c
Erik Ogenvik <erik@ogenvik.org>
2022-04-15 14:06:33 +0200

    Don't use transition headers.

--- src/rules/entityfilter/ParserDefinitions.h
+++ src/rules/entityfilter/ParserDefinitions.h
@@ -8,7 +8,7 @@
 
 #include <boost/spirit/include/qi.hpp>
 #include <boost/spirit/include/qi_real.hpp>
-#include <boost/spirit/include/phoenix.hpp>
+#include <boost/phoenix/phoenix.hpp>
 #include <boost/fusion/include/adapt_struct.hpp>
 
 namespace EntityFilter {
